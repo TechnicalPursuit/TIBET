5862a4cbd86da85f3ce1b1aa3bac488173542eae
idearat <ss@technicalpursuit.com>
2014-01-13 23:11:04 -0700

    Add better parsing so we can pass port number etc.

--- dna/default/server.js
+++ dna/default/server.js
@@ -1,6 +1,26 @@
+/**
+ * @overview A baseline web server leveraging Connect.
+ * @author Scott Shattuck (ss)
+ * @copyright Copyright (C) 1999-2014 Technical Pursuit Inc. (TPI) All Rights
+ *     Reserved. Patents Pending, Technical Pursuit Inc. Licensed under the
+ *     OSI-approved Reciprocal Public License (RPL) Version 1.5. See the RPL
+ *     for your rights and responsibilities. Contact TPI to purchase optional
+ *     open source waivers to keep your derivative work source code private.
+ */
+
+(function() {
+
 var Connect = require('connect');
+var opt = require('optimist');
+var argv = opt.argv;
+var port = argv.port ||
+    process.env.npm_package_config_port ||
+    process.env.PORT ||
+    1407;
 
 Connect.createServer(
-  Connect.logger(), // Log responses to the terminal using Common Log Format.
-  Connect.static(__dirname) // Serve all static files in the current dir.
-).listen(process.env.PORT || 5000);
+  Connect.logger(),
+  Connect.static(__dirname)
+).listen(port);
+
+}());
