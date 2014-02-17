86e8c0fe0d2549cb4541a92f010ff3b25ba7ca2b
idearat <ss@technicalpursuit.com>
2014-02-17 11:45:06 -0700

    Update core dan template to match latest boot code requirements.

--- dna/default/TIBET-INF/cfg/environment.js
+++
@@ -1,9 +0,0 @@
-/**
- * @overview A common environment file. Entries you place here apply to
- *     all environments: production, test, and development. You should place
- *     environment-specific values in a file named for the desired enviroment
- *     (.e.g. development.js for development).
- */
-
-// Ensure we use the tibetdir approach to computing root paths.
-TP.sys.setcfg('boot.rootcomp', 'tibetdir');
