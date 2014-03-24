30288c53dc01ec94e8904d239f65427d6225fafa
idearat <ss@technicalpursuit.com>
2014-03-23 21:12:13 -0600

    Add sample custom command.

---
+++ dna/default/TIBET-INF/cmd/sample.js
@@ -0,0 +1,61 @@
+/**
+ * @overview Sample custom tibet command.
+ */
+
+;(function(root) {
+
+// TODO: this should work via 'tibet/src/cli/_cli' but won't until 3x and 5x
+// merge. Meanwhile 'tibet init' has to link both tibet3 and tibet :(.
+var CLI = require('tibet/src/cli/_cli');
+
+//  ---
+//  Type Construction
+//  ---
+
+var parent = require('tibet/src/cli/_cmd');
+
+var Cmd = function(){};
+Cmd.prototype = new parent();
+
+
+//  ---
+//  Instance Attributes
+//  ---
+
+/**
+ * The command execution context. Clone can only be done outside of a project.
+ * @type {Cmd.CONTEXTS}
+ */
+Cmd.prototype.CONTEXT = CLI.CONTEXTS.BOTH;
+
+
+/**
+ * The command usage string.
+ * @type {string}
+ */
+Cmd.prototype.USAGE = 'tibet sample [args]';
+
+
+//  ---
+//  Instance Methods
+//  ---
+
+Cmd.prototype.process = function(args) {
+    this.log('sample command');
+};
+
+
+//  ---
+//  Export
+//  ---
+
+if (typeof exports !== 'undefined') {
+    if (typeof module !== 'undefined' && module.exports) {
+        exports = module.exports = Cmd;
+    }
+    exports.Cmd = Cmd;
+} else {
+    root.Cmd = Cmd;
+}
+
+}(this));
