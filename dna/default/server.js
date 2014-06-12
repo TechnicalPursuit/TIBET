0bbcc1978f5db72fd2562745bd5e2f6e71d68432
William J. Edney <bedney@technicalpursuit.com>
2014-06-12 15:18:42 -0500

    Added file watch capability to baseline server.

--- dna/default/server.js
+++ dna/default/server.js
@@ -13,13 +13,113 @@
 var Connect = require('connect');
 var gzipStatic = require('connect-gzip-static');
 var minimist = require('minimist');
+var watch = require('watch');
+var path = require('path');
 var argv = minimist(process.argv.slice(2));
 var port = argv.port ||
     process.env.npm_package_config_port ||
     process.env.PORT ||
     1407;
 
+//  ------------------------------------------------------------------------
+//  FILE WATCH CAPABILITY
+//  ------------------------------------------------------------------------
+
+var pathToWatch = path.resolve(__dirname, './src');
+var changedFileName = '';
+
+var fileWatcher = function (req, res, next) {
+    var writeSSEHead,
+        writeSSEData;
+
+    writeSSEHead = function (req, res, cb) {
+        res.writeHead(
+            200,
+            {
+                'Content-Type': 'text/event-stream',
+                'Cache-Control': 'no-cache',
+                'Connection': 'keep-alive'
+            });
+
+        res.write('retry: 1000\n');
+
+        if (cb) {
+            return cb(req, res);
+        }
+    };
+
+    writeSSEData = function (req, res, eventName, eventData, cb) {
+        var id;
+
+        id = (new Date()).toLocaleTimeString();
+
+        res.write('id: ' + id + '\n');
+        res.write('event: ' + eventName + '\n');
+        res.write('data: ' + JSON.stringify(eventData) + '\n\n');
+
+        if (cb) {
+            return cb(req, res);
+        }
+    };
+
+    if (req.headers.accept &&
+        req.headers.accept === 'text/event-stream' &&
+        req.url.match('/fileWatcher')) {
+
+        var eventName;
+
+        if (changedFileName !== '') {
+            eventName = 'fileChanged';
+        } else {
+            eventName = '';
+        }
+
+        //  Write the SSE HEAD and then in that method's callback, write the SSE
+        //  data.
+        writeSSEHead(
+            req, res,
+            function(req, res) {
+                writeSSEData(
+                    req, res, eventName, changedFileName,
+                    function(req, res) {
+
+                        if (changedFileName !== '') {
+                            changedFileName = '';
+                        }
+
+                        res.end();
+                    });
+        });
+    }
+
+    next();
+};
+
+//  This requires the following npm module:
+//  npm install watch
+
+watch.watchTree(
+    pathToWatch,
+    function (fileName, curr, prev) {
+        if (typeof fileName === 'object' && prev === null && curr === null) {
+            // Finished walking the tree
+        } else if (prev === null) {
+            // fileName is a new file
+        } else if (curr.nlink === 0) {
+            // fileName was removed
+        } else {
+            //console.log('the file changed: ' + fileName);
+            // fileName was changed
+            changedFileName = fileName;
+        }
+    });
+
+//  ------------------------------------------------------------------------
+//  SERVER EXECUTION
+//  ------------------------------------------------------------------------
+
 Connect.createServer(
+    fileWatcher,
   Connect.logger(),
   gzipStatic(__dirname)
 ).listen(port);
