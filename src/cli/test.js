0c664856d4212fddc250a9055b9b3c809d230db2
idearat <ss@technicalpursuit.com>
2014-06-18 18:00:48 -0600

    Initial rough cut.  Runs but doesn't process inbound arguments.

--- src/cli/test.js
+++ src/cli/test.js
@@ -13,7 +13,6 @@
 'use strict';
 
 var CLI = require('./_cli');
-var beautify = require('js-beautify').js_beautify;
 
 
 //  ---
@@ -30,24 +29,42 @@ Cmd.prototype = new Parent();
 //  Type Attributes
 //  ---
 
+/**
+ * The default path to the TIBET-specific phantomjs test runner.
+ * @type {String}
+ */
+Cmd.DEFAULT_RUNNER = './tst/phantomtsh.js';
+
+
 //  ---
 //  Instance Attributes
 //  ---
 
 /**
  * The command help string.
- * @type {string}
+ * @type {String}
  */
 Cmd.prototype.HELP =
     'Runs unit and functional tests for the library and application.';
 
 
 /**
+ * Command argument parsing options.
+ * @type {Object}
+ */
+Cmd.prototype.PARSE_OPTIONS = CLI.blend(
+    {
+        boolean: ['ignore_only', 'ignore_defer'],
+        string: ['target'],
+        default: {}
+    },
+    Parent.prototype.PARSE_OPTIONS);
+
+/**
  * The command usage string.
- * @type {string}
+ * @type {String}
  */
 Cmd.prototype.USAGE = 'tibet test [options]';
-
 
 //  ---
 //  Instance Methods
@@ -57,13 +74,63 @@ Cmd.prototype.USAGE = 'tibet test [options]';
  * Perform the actual command processing logic.
  */
 Cmd.prototype.execute = function() {
-    this.info('testing...');
 
-// TODO: make this work :)
+    var sh;         // The shelljs module.
+    var child;      // The child_process module.
+    var process;    // The spawned child process.
+    var path;       // The path module.
+    var testpath;   // Path to the TIBET test runner script.
+    var cmd;        // Local binding variable.
 
-    return;
+    path = require('path');
+    sh = require('shelljs');
+    child = require('child_process');
+
+    // Run phantomjs with a manufactured command line which reflects the :test
+    // command we'd run if we were in the TIBET Shell environment.
+
+    testpath = CLI.expandPath(
+        path.join('~/node_modules/tibet3/', Cmd.DEFAULT_RUNNER));
+    if (!sh.test('-e', testpath)) {
+        this.log('cannot find runner at: ' + testpath);
+        return;
+    }
+
+    cmd = this;
+
+    process = child.spawn('phantomjs', [testpath,
+        '--profile', '~app/tst/phantom',
+        '--script', ':test -ignore_only']);
+
+    process.stdout.on('data', function(data) {
+        var msg = data.slice(0, -1);
+        cmd.log(msg);
+    });
+
+    process.stderr.on('data', function(data) {
+        var msg = data.slice(0, -1);
+
+        // Somebody down below likes to write error output with empty lines.
+        if (msg.trim().length === 0) {
+            return;
+        }
+
+        // A lot of errors will include what appears to be a common 'header'
+        // output message from events.js:72 etc. which provides no useful
+        // data but clogs up the output. Filter those messages.
+        if (/throw er;/.test(msg)) {
+            return;
+        }
+
+        cmd.error(msg);
+    });
+
+    process.on('close', function(code) {
+        if (code !== 0) {
+            cmd.error('Testing stopped with status: ' + code);
+        }
+    });
 };
-
 
 module.exports = Cmd;
 
