d718cf3f7f9b60ed5156189bcd6be209d6263516
idearat <ss@technicalpursuit.com>
2014-07-12 13:35:11 -0600

    Add user prompting for couch url and database name.

--- dna/couchapp/makefile.js
+++ dna/couchapp/makefile.js
@@ -14,6 +14,40 @@ var sh = require('shelljs');
 // Uncomment to include TIBET's make helper routines for rollups.
 // var helpers = require('tibet/src/tibet/cli/_make_helpers');
 
+
+/**
+ * A helper function to handle prompting the user for common database
+ * parameters. The make parameter should be provided to grant access to the
+ * currently running task object.
+ * @param {Object} make The make command instance.
+ * @return {Object} An object containing db_url and db_name keys.
+ */
+var getDatabaseParameters = function(make) {
+    var db_url;
+    var db_name;
+    var result;
+
+    if (!make) {
+        throw new Error('Invalid call to helper function. No task provided.');
+    }
+
+    db_url = 'http://0.0.0.0:5984';
+    db_name = make.getProjectName();
+
+    result = make.prompt.question('Database url [' + db_url + '] ? ');
+    if (result && result.length > 0) {
+        db_url = result;
+    }
+    make.log('using database url \'' + db_url + '\'.');
+
+    result = make.prompt.question('Database name [' + db_name + '] ? ');
+    if (result && result.length > 0) {
+        db_name = result;
+    }
+
+    return {db_url: db_url, db_name: db_name};
+};
+
 /**
  * Canonical `targets` object for exporting the various target functions.
  */
@@ -48,17 +82,16 @@ targets.check = function(make) {
  * Create a new CouchDB database.
  */
 targets.createdb = function(make) {
+    var params;
     var db_url;
     var db_name;
     var nano;
 
-    // TODO: pull command line arguments if they're here. We need to support
-    // changing the database url and the database name.
+    params = getDatabaseParameters(make);
+    db_url = params.db_url;
+    db_name = params.db_name;
 
-    db_url = 'http://0.0.0.0:5984';
-    db_name = make.getProjectName();
-
-    make.log('creating CouchDB database: ' + db_name);
+    make.log('creating database: ' + db_url + '/' + db_name);
 
     nano = require('nano')(db_url);
     nano.db.create(db_name,
@@ -77,17 +110,16 @@ targets.createdb = function(make) {
  * Push the current app.js and attachments content to CouchDB.
  */
 targets.pushdb = function(make) {
-    var result;
+    var params;
     var db_url;
     var db_name;
+    var result;
 
-    // TODO: pull command line arguments if they're here. We need to support
-    // changing the database url and the database name.
+    params = getDatabaseParameters(make);
+    db_url = params.db_url;
+    db_name = params.db_name;
 
-    db_url = 'http://0.0.0.0:5984';
-    db_name = make.getProjectName();
-
-    make.log('pushing to CouchDB database: ' + db_name);
+    make.log('pushing to database: ' + db_url + '/' + db_name);
 
     // TODO: copy the tibet.json from the top level into the attachments
     // directory but remove 'attachments' from the app.inf reference and any
@@ -111,26 +143,25 @@ targets.pushdb = function(make) {
  * Remove the current CouchDB database.
  */
 targets.removedb = function(make) {
-    var response;
+    var params;
     var db_url;
     var db_name;
+    var result;
     var nano;
 
-    // TODO: pull command line arguments if they're here. We need to support
-    // changing the database url and the database name.
+    params = getDatabaseParameters(make);
+    db_url = params.db_url;
+    db_name = params.db_name;
 
-    db_url = 'http://0.0.0.0:5984';
-    db_name = make.getProjectName();
-
-    response = make.prompt.question(
-        'Delete the database? Enter \'yes\' to confirm: ');
-    if (!response || response.trim().toLowerCase() !== 'yes') {
+    result = make.prompt.question(
+        'Delete database [' + db_url + '/' + db_name + '] ? Enter \'yes\' to confirm: ');
+    if (!result || result.trim().toLowerCase() !== 'yes') {
         make.log('database removal cancelled.');
         targets.removedb.resolve();
         return;
     }
 
-    make.log('deleting database at ' + db_url + '/' + db_name);
+    make.log('deleting database: ' + db_url + '/' + db_name);
 
     nano = require('nano')(db_url);
     nano.db.destroy(db_name,
