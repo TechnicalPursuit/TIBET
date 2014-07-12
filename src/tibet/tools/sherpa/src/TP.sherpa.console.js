70f5883864366ae717db097b33e710a791b4f40a
William J. Edney <bedney@technicalpursuit.com>
2014-07-12 18:27:19 -0500

    Make sure to scroll the console to the bottom after changing it's maximization.

--- src/tibet/tools/sherpa/src/TP.sherpa.console.js
+++ src/tibet/tools/sherpa/src/TP.sherpa.console.js
@@ -432,6 +432,12 @@ function() {
 
     this.focusInput();
 
+    (function() {
+        this.focusInput();
+        this.scrollToEnd();
+
+    }.bind(this)).afterUnwind();
+
     return this;
 });
 
