eba59c997554fdd78ca3a571289fe185796354a2
William J. Edney <bedney@technicalpursuit.com>
2014-08-25 10:37:00 -0500

    The path parameterized now starts parameter arguments in the 0th place.

--- src/goog/services/search/TPGoogleSearchTypes.js
+++ src/goog/services/search/TPGoogleSearchTypes.js
@@ -51,7 +51,7 @@ TP.goog.GoogleSearchData.Inst.defineAttribute(
 TP.goog.GoogleSearchData.Inst.defineAttribute(
         'resultsFromTo',
         {'value':
-                TP.apc('data.responseData.results.[{{1}}:{{2}}]')});
+                TP.apc('data.responseData.results.[{{0}}:{{1}}]')});
 
 TP.goog.GoogleSearchData.Inst.defineAttribute(
         'estimatedResultCount',
