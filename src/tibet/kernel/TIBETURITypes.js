29e99ffa8e156904a9a9d25d8b77df93e81a290c
William J. Edney <bedney@technicalpursuit.com>
2014-10-15 21:40:06 -0500

    Leverage the shouldCreateContent flag.

--- src/tibet/kernel/TIBETURITypes.js
+++ src/tibet/kernel/TIBETURITypes.js
@@ -3685,6 +3685,7 @@ function(aRequest, aResult, aResource) {
             fragment = fragment;
         } else if (TP.regex.ANY_POINTER.test(fragment)) {
             fragment = TP.apc(fragment, true);
+            fragment.set('shouldMake', this.get('shouldCreateContent'));
         }
 
         if (TP.canInvoke(result, 'set')) {
