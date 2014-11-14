bbb7cace1057f7065cef43146c29866b2bbcd4bc
idearat <ss@technicalpursuit.com>
2014-11-13 21:07:33 -0700

    Add checkup targets to dna templates.

--- dna/noserver/makefile.js
+++ dna/noserver/makefile.js
@@ -36,6 +36,31 @@ targets.build = function(make) {
 };
 
 /**
+ * Run lint and test commands to verify the code is in good shape.
+ */
+targets.checkup = function(make) {
+    var result;
+
+    make.log('checking for lint...');
+
+    result = sh.exec('tibet lint');
+    if (result.code !== 0) {
+        targets.checkup.reject();
+        return;
+    }
+
+    make.log('running unit tests...');
+
+    result = sh.exec('tibet test');
+    if (result.code !== 0) {
+        targets.checkup.reject();
+        return;
+    }
+
+    targets.checkup.resolve();
+};
+
+/**
  */
 targets.clean = function(make) {
     make.log('cleaning...');
