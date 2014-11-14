c4233e03f946967acd2a20771d4419fdedda1648
idearat <ss@technicalpursuit.com>
2014-11-13 22:21:14 -0700

    Convert to be "json" quoted content.

--- src/tibet/kernel/TIBETVersion.js
+++ src/tibet/kernel/TIBETVersion.js
@@ -12,16 +12,16 @@
 
     //  --- latest.js start ---
     release({
-        'describe': '3.0.0-116-g52240583fc-dirty',
-        'major': '5',
-        'minor': '0',
-        'patch': '0',
-        'suffix': 'dev',
-        'ptag': '3.0.0',
-        'commits': '116',
-        'phash': '52240583fc',
-        'time': '1405184580500',
-        'semver': 'v5.0.0-dev.116+g52240583fc.1405184580500'
+        "describe": "3.0.0-116-g52240583fc-dirty",
+        "major": "5",
+        "minor": "0",
+        "patch": "0",
+        "suffix": "dev",
+        "ptag": "3.0.0",
+        "commits": "116",
+        "phash": "52240583fc",
+        "time": "1405184580500",
+        "semver": "v5.0.0-dev.116+g52240583fc.1405184580500"
     });
     //  --- end latest.js ---
 
