1bc74d52d2a6b55bf9ca4fcb957ab011ebce6786
idearat <ss@technicalpursuit.com>
2014-11-14 08:57:06 -0700

    Fix version computation in kernel routine.

--- src/tibet/kernel/TIBETVersion.js
+++ src/tibet/kernel/TIBETVersion.js
@@ -17,11 +17,12 @@
         "minor": "0",
         "patch": "0",
         "suffix": "dev",
+        "increment": "0",
         "ptag": "3.0.0",
         "commits": "116",
         "phash": "52240583fc",
         "time": "1405184580500",
-        "semver": "v5.0.0-dev.116+g52240583fc.1405184580500"
+        "semver": "v5.0.0-dev.0+g52240583fc.116.1405184580500"
     });
     //  --- end latest.js ---
 
