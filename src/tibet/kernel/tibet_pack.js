1580da9a0a02742bc826480980ce17d636518641
idearat <ss@technicalpursuit.com>
2014-11-17 13:49:09 -0700

    Back out tokenizer "patch".

--- src/tibet/kernel/tibet_pack.js
+++ src/tibet/kernel/tibet_pack.js
@@ -509,13 +509,7 @@ TP.$tokenize = function(src, ops, tsh, exp, alias, args) {
                     for (;;) {
                         c = src.charAt(i);
                         if (identBody.test(c) !== true) {
-                            // Allow for identifiers to be dot-separated.
-                            if (tsh && c === '.' && identBody.test(src.charAt(i + 1))) {
-                                break;
-                                void(0);
-                            } else {
-                                break;
-                            }
+                            break;
                         }
 
                         str += c;
