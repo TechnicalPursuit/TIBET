6a09289dc96e084dc88b90475c73a576c5c7eb0d
idearat <ss@technicalpursuit.com>
2014-11-18 17:09:29 -0700

    Fix function literal test regex.

--- src/tibet/kernel/TIBETGlobals.js
+++ src/tibet/kernel/TIBETGlobals.js
@@ -2174,7 +2174,7 @@ TP.regex.NUMBER_CONSTRUCTOR = /function Number\(\)/;
 TP.regex.REGEXP_CONSTRUCTOR = /function RegExp\(\)/;
 TP.regex.STRING_CONSTRUCTOR = /function String\(\)/;
 
-TP.regex.FUNCTION_LITERAL = /^function (.*) \((.*)\) \{.*\}\$/;
+TP.regex.FUNCTION_LITERAL = /^function(?:.*)\((?:.*)\)(?:[ ]*)\{(?:.*)\}$/;
 
 TP.regex.ATTRIBUTE_NAME = /^[_$][a-zA-Z0-9_$]*$|^[A-Z]/;
 
