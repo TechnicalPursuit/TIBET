da0814a4feab669963570a049f4cf372db11f956
William J. Edney <bedney@technicalpursuit.com>
2014-12-13 05:33:00 -0600

    Make sure that the detector is a valid Function.

--- src/tibet/kernel/TIBETNotification.js
+++ src/tibet/kernel/TIBETNotification.js
@@ -4933,7 +4933,7 @@ function(originSet, aSignal, aPayload, aType) {
 
         //  check each one as we pass for any on: remapping. if found we need to
         //  ensure that element is in our origin list for the bubbling phase.
-        if (TP.isValid(detector) && TP.isElement(origin)) {
+        if (TP.isCallable(detector) && TP.isElement(origin)) {
             if (TP.elementGetAttributeNames(origin).detect(detector)) {
                 // Found an on: mapping for this origin...
                 originArray.push(origin);
@@ -5063,7 +5063,7 @@ function(originSet, aSignal, aPayload, aType) {
 
         origin = originArray.at(i);
 
-        if (TP.isElement(origin)) {
+        if (TP.isCallable(detector) && TP.isElement(origin)) {
             if (TP.elementGetAttributeNames(origin).detect(detector)) {
                 // Found an on: mapping for this origin...
                 signame = TP.elementGetAttribute(origin, onstar);
