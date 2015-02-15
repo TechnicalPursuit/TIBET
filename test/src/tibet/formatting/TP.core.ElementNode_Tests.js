7528238c41ae6a1880fb974c7d586f3f5946d780
William J. Edney <bedney@technicalpursuit.com>
2015-02-14 23:35:48 -0600

    Added tests for 'ui:storage' formatting (and 'ui:type' supporting it).

--- test/src/tibet/formatting/TP.core.ElementNode_Tests.js
+++ test/src/tibet/formatting/TP.core.ElementNode_Tests.js
@@ -514,6 +514,412 @@ function() {
     });
 });
 
+//  ------------------------------------------------------------------------
+
+TP.core.ElementNode.Inst.describe('TP.core.ElementNode: ui:storage attribute',
+function() {
+
+    var unloadURI;
+
+    unloadURI = TP.uc(TP.sys.cfg('tibet.blankpage'));
+
+    //  ---
+
+    this.before(
+        function() {
+
+            //  ---
+
+            if (!TP.isType(TP.sys.getTypeByName('TP.test.PetStorageConverter'))) {
+
+                TP.lang.Object.defineSubtype('TP.test.PetStorageConverter');
+                TP.test.PetStorageConverter.Type.defineMethod(
+                                        'fromString',
+                                        function(aValue, params) {
+                                            switch (aValue) {
+                                                case 'foo':
+                                                    return 'woof';
+                                                case 'bar':
+                                                    return 'meow';
+                                                case 'baz':
+                                                    return 'gurgle';
+                                                default:
+                                                    break;
+                                            }
+                                        });
+            }
+
+            //  ---
+
+            if (!TP.isType(TP.sys.getTypeByName('TP.test.ColorStorageConverter'))) {
+
+                TP.lang.Object.defineSubtype('TP.test.ColorStorageConverter');
+                TP.test.ColorStorageConverter.Type.defineMethod(
+                                        'fromArray',
+                                        function(aValue, params) {
+                                            return aValue.collect(
+                                                function(val) {
+                                                    return this.fromString(val);
+                                                }.bind(this));
+                                        });
+                TP.test.ColorStorageConverter.Type.defineMethod(
+                                        'fromString',
+                                        function(aValue, params) {
+                                            switch (aValue) {
+                                                case 'foo':
+                                                    return TP.cc('red');
+                                                case 'bar':
+                                                    return TP.cc('blue');
+                                                case 'baz':
+                                                    return TP.cc('yellow');
+                                                default:
+                                                    break;
+                                            }
+                                        });
+            }
+
+            //  ---
+
+            if (!TP.isType(TP.sys.getTypeByName('TP.test.Splitter'))) {
+
+                TP.lang.Object.defineSubtype('TP.test.Splitter');
+                TP.test.Splitter.Type.defineMethod(
+                                        'fromString',
+                                        function(aValue, params) {
+                                            return aValue.split(',');
+                                        });
+            }
+
+            //  ---
+
+            this.getDriver().showTestGUI();
+        });
+
+    //  ---
+
+    this.after(
+        function() {
+            this.getDriver().showTestLog();
+        });
+
+    //  ---
+
+    this.it('get content - single format - simple substitutions', function(test, options) {
+
+        var loadURI;
+
+        loadURI = TP.uc('~lib_tst/src/tibet/formatting/Formatting5.xhtml');
+
+        test.getDriver().setLocation(loadURI);
+
+        test.then(
+            function() {
+                var textField,
+                    textArea,
+
+                    selectSingle,
+                    optionToSelect,
+
+                    selectMultiple,
+
+                    inputRadio2,
+                    inputRadio3,
+
+                    inputCheckbox2,
+                    inputCheckbox3;
+
+                //  Change the content via 'user' interaction
+
+                textField = TP.byOID('input_text');
+
+                test.getDriver().startSequence().
+                    exec(function() {
+                                textField.clearValue();
+                            }).
+                    sendKeys('jones', textField).
+                    sendEvent(TP.hc('type', 'change'), textField).
+                    perform();
+
+                test.then(
+                    function() {
+                        test.assert.isEqualTo(
+                            textField.get('value'),
+                            'Jones');
+                    });
+
+                //  ---
+
+                textArea = TP.byOID('textarea');
+
+                test.getDriver().startSequence().
+                    exec(function() {
+                                textArea.clearValue();
+                            }).
+                    sendKeys(TP.str(TP.dc()), textArea).
+                    sendEvent(TP.hc('type', 'change'), textArea).
+                    perform();
+
+                test.then(
+                    function() {
+                        test.assert.isEqualTo(
+                            textArea.get('value'),
+                            TP.str(TP.dc().getFullYear()));
+                    });
+
+                //  ---
+
+                selectSingle = TP.byOID('select_single');
+                optionToSelect = selectSingle.getElementArray().at(1);
+
+                test.getDriver().startSequence().
+                    click(optionToSelect).
+                    perform();
+
+                test.then(
+                    function() {
+                        test.assert.isEqualTo(
+                            selectSingle.get('value'),
+                            'meow');
+                    });
+
+                //  ---
+
+                selectMultiple = TP.byOID('select_multiple');
+                optionToSelect = selectMultiple.getElementArray().at(1);
+
+                test.getDriver().startSequence().
+                    click(optionToSelect).
+                    perform();
+
+                test.then(
+                    function() {
+                        test.assert.isEqualTo(
+                            selectMultiple.get('value'),
+                            TP.ac(TP.cc('blue')));
+                    });
+
+                //  ---
+
+                //  NB: We go after elements that do *not* have the 'ui:storage'
+                //  attribute, but because they're in a group with an element
+                //  that does, the value should still be formatted.
+                inputRadio2 = TP.byOID('input_radio_2');
+                test.assert.isElement(inputRadio2);
+
+                test.getDriver().startSequence().
+                    click(inputRadio2).
+                    perform();
+
+                test.then(
+                    function() {
+                        test.assert.isEqualTo(
+                            inputRadio2.getValue(),
+                            'meow');
+                    });
+
+                //  ---
+
+                inputRadio3 = TP.byOID('input_radio_3');
+                test.assert.isElement(inputRadio3);
+
+                test.getDriver().startSequence().
+                    click(inputRadio3).
+                    perform();
+
+                test.then(
+                    function() {
+                        test.assert.isEqualTo(
+                            inputRadio3.getValue(),
+                            'gurgle');
+                    });
+
+                //  ---
+
+                //  NB: We go after elements that do *not* have the 'ui:storage'
+                //  attribute, but because they're in a group with an element
+                //  that does, the value should still be formatted.
+
+                inputCheckbox2 = TP.byOID('input_checkbox_2');
+                test.assert.isElement(inputCheckbox2);
+
+                test.getDriver().startSequence().
+                    click(inputCheckbox2).
+                    perform();
+
+                test.then(
+                    function() {
+                        test.assert.isEqualTo(
+                            inputCheckbox2.getValue(),
+                            TP.ac(TP.cc('blue')));
+                    });
+
+                //  ---
+
+                inputCheckbox3 = TP.byOID('input_checkbox_3');
+                test.assert.isElement(inputCheckbox3);
+
+                test.getDriver().startSequence().
+                    click(inputCheckbox3).
+                    perform();
+
+                test.then(
+                    function() {
+                        test.assert.isEqualTo(
+                            inputCheckbox3.getValue(),
+                            //  both checkbox2 and checkbox3 are on at this
+                            //  point
+                            TP.ac(TP.cc('blue'), TP.cc('yellow')));
+                    });
+
+                //  ---
+
+                //  Unload the current page by setting it to the blank
+                test.getDriver().setLocation(unloadURI);
+
+                //  Unregister the URI to avoid a memory leak
+                loadURI.unregister();
+            },
+            function(error) {
+                test.fail(error, TP.sc('Couldn\'t get resource: ',
+                                            loadURI.getLocation()));
+            });
+    });
+
+    //  ---
+
+    this.it('get content - single format - templating', function(test, options) {
+
+        var loadURI;
+
+        loadURI = TP.uc('~lib_tst/src/tibet/formatting/Formatting6.xhtml');
+
+        test.getDriver().setLocation(loadURI);
+
+        test.then(
+            function() {
+                var textField,
+                    textArea;
+
+                //  Change the content via 'user' interaction
+
+                textField = TP.byOID('input_text');
+
+                test.getDriver().startSequence().
+                    exec(function() {
+                                textField.clearValue();
+                            }).
+                    sendKeys('foo', textField).
+                    sendEvent(TP.hc('type', 'change'), textField).
+                    perform();
+
+                test.then(
+                    function() {
+                        test.assert.isEqualTo(
+                            textField.get('value'),
+                            'It is: foo');
+                    });
+
+                //  ---
+
+                textArea = TP.byOID('textarea');
+
+                test.getDriver().startSequence().
+                    exec(function() {
+                                textArea.clearValue();
+                            }).
+                    sendKeys('bill', textArea).
+                    sendEvent(TP.hc('type', 'change'), textArea).
+                    perform();
+
+                test.then(
+                    function() {
+                        test.assert.isEqualTo(
+                            textArea.get('value'),
+                            'Uppercased: BILL');
+                    });
+
+                //  ---
+
+                //  Unload the current page by setting it to the blank
+                test.getDriver().setLocation(unloadURI);
+
+                //  Unregister the URI to avoid a memory leak
+                loadURI.unregister();
+            },
+            function(error) {
+                test.fail(error, TP.sc('Couldn\'t get resource: ',
+                                            loadURI.getLocation()));
+            });
+    });
+
+    //  ---
+
+    this.it('get content - multiple formats - simple substitutions', function(test, options) {
+
+        var loadURI;
+
+        loadURI = TP.uc('~lib_tst/src/tibet/formatting/Formatting7.xhtml');
+
+        test.getDriver().setLocation(loadURI);
+
+        test.then(
+            function() {
+                var textField,
+                    textArea;
+
+                //  Change the content via 'user' interaction
+
+                textField = TP.byOID('input_text');
+
+                test.getDriver().startSequence().
+                    exec(function() {
+                                textField.clearValue();
+                            }).
+                    sendKeys('Bill', textField).
+                    sendEvent(TP.hc('type', 'change'), textField).
+                    perform();
+
+                test.then(
+                    function() {
+                        test.assert.isEqualTo(
+                            textField.get('value'),
+                            'bill');
+                    });
+
+                //  ---
+
+                textArea = TP.byOID('textarea');
+
+                test.getDriver().startSequence().
+                    exec(function() {
+                                textArea.clearValue();
+                            }).
+                    sendKeys('foo,bar,baz', textArea).
+                    sendEvent(TP.hc('type', 'change'), textArea).
+                    perform();
+
+                test.then(
+                    function() {
+                        test.assert.isEqualTo(
+                            textArea.get('value'),
+                            '&lt;html:li&gt;foo&lt;/html:li&gt;&lt;html:li&gt;bar&lt;/html:li&gt;&lt;html:li&gt;baz&lt;/html:li&gt;');
+                    });
+
+                //  ---
+
+                //  Unload the current page by setting it to the blank
+                test.getDriver().setLocation(unloadURI);
+
+                //  Unregister the URI to avoid a memory leak
+                loadURI.unregister();
+            },
+            function(error) {
+                test.fail(error, TP.sc('Couldn\'t get resource: ',
+                                            loadURI.getLocation()));
+            });
+    });
+});
+
 //  ========================================================================
 //  Run those babies!
 //  ------------------------------------------------------------------------
