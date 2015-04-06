5322f2113977c73eceaa4218a6d5029222aab992
William J. Edney <bedney@technicalpursuit.com>
2015-04-05 21:39:53 -0500

    Added a 'tagUnmarshal' call to <a> elements to rewrite their href to dispatch through TIBET's TP.go2() call.

--- src/xhtml/html_HypertextModuleNodes.js
+++ src/xhtml/html_HypertextModuleNodes.js
@@ -24,5 +24,52 @@ TP.html.Focused.defineSubtype('a');
 TP.html.a.Type.set('uriAttrs', TP.ac('href'));
 
 //  ------------------------------------------------------------------------
+//  Type Methods
+//  ------------------------------------------------------------------------
+
+TP.html.a.Type.defineMethod('tagUnmarshal',
+function(aRequest) {
+
+    /**
+     * @method tagUnmarshal
+     * @summary Unmarshals the receiver's content. This includes resolving XML
+     *     Base URIs and virtual URIs that may occur on the receiver's
+     *     attributes.
+     * @param {TP.sig.Request} aRequest A request containing processing
+     *     parameters and other data.
+     */
+
+    var elem,
+        href;
+
+    //  Make sure that we have a node to work from.
+    if (!TP.isNode(elem = aRequest.at('node'))) {
+        //  TODO: Raise an exception.
+        return;
+    }
+
+    //  Call up to allow any 'href' values to be expanded, etc. if necessary
+    this.callNextMethod();
+
+    if (TP.notEmpty(href = TP.elementGetAttribute(elem, 'href', true))) {
+
+        //  Rewrite the link so that it calls TP.go2() instead of just being a
+        //  standard link. This gives TIBET control so that proper routing and
+        //  history management can occur.
+
+        //  First, just set the 'href' to '#'
+        TP.elementSetAttribute(elem, 'href', '#', true);
+
+        //  Then add an 'onclick' that will trigger TIBET.
+        TP.elementSetAttribute(elem,
+                                'onclick',
+                                'TP.go2(\'' + href + '\', window);',
+                                true);
+    }
+
+    return;
+});
+
+//  ------------------------------------------------------------------------
 //  end
 //  ========================================================================
