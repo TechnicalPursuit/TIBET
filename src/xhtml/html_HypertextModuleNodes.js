cc06383cae9f573661460f5d8ca2de4ad1153f5e
idearat <ss@technicalpursuit.com>
2015-04-27 17:44:48 -0600

    Compute go2 replacements more effectively for links.

--- src/xhtml/html_HypertextModuleNodes.js
+++ src/xhtml/html_HypertextModuleNodes.js
@@ -40,32 +40,72 @@ function(aRequest) {
      */
 
     var elem,
+        target,
+        click,
         href;
 
     //  Make sure that we have a node to work from.
     if (!TP.isNode(elem = aRequest.at('node'))) {
-        //  TODO: Raise an exception.
         return;
     }
 
-    //  Call up to allow any 'href' values to be expanded, etc. if necessary
+    //  Capture the original HREF value so we can undo expansion if necessary.
+    href = TP.elementGetAttribute(elem, 'href', true);
+
+    //  Call up the chain and force expansion and any other default processing.
     this.callNextMethod();
 
-    if (TP.notEmpty(href = TP.elementGetAttribute(elem, 'href', true))) {
-
-        //  Rewrite the link so that it calls TP.go2() instead of just being a
-        //  standard link. This gives TIBET control so that proper routing and
-        //  history management can occur.
-
-        //  First, just set the 'href' to '#'
-        TP.elementSetAttribute(elem, 'href', '#', true);
-
-        //  Then add an 'onclick' that will trigger TIBET.
-        TP.elementSetAttribute(elem,
-                                'onclick',
-                                'TP.go2(\'' + href + '\', window);',
-                                true);
+    //  When there's a different target we're going to stay out of the way.
+    if (TP.notEmpty(target = TP.elementGetAttribute(elem, 'target', true))) {
+        if (target !== '_self') {
+            return;
+        }
     }
+
+    //  If there's already a click handler the developer "wins", even if that
+    //  means they don't get the benefit of TIBET here.
+    if (TP.notEmpty(click = TP.elementGetAttribute(elem, 'onclick', true))) {
+        return;
+    }
+
+    //  Links with an empty HREF will try to reload the page. We really don't
+    //  want that, we want to have them do nothing. We set '#' here and let the
+    //  following check add a return false click handler.
+    if (TP.isEmpty(href)) {
+        TP.elementSetAttribute(elem, 'href', '#');
+    }
+
+    //  Links with an empty anchor will try to reset at the top of page. Don't
+    //  let that happen either, just "dampen them" via onclick.
+    if (href === '#') {
+        TP.elementSetAttribute(elem, 'onclick', 'return false;');
+        return;
+    }
+
+    //  If the link triggers a javascript url (or is javascript: void 0) exit.
+    /* eslint-disable no-script-url */
+    if (href.indexOf('javascript:') === 0) {
+        return;
+    }
+    /* eslint-enable no-script-url */
+
+    //  Rewrite the link so that it calls TP.go2() instead of just being a
+    //  standard link. This gives TIBET control so that proper routing and
+    //  history management can occur.
+
+    //  If the original value wasn't a route then refetch to get the fully
+    //  expanded form so we use that for our go2 call.
+    if (href.indexOf('#') !== 0 && href !== '/') {
+        href = TP.elementGetAttribute(elem, 'href', true);
+    }
+
+    //  First, just set the 'href' to '#' to avoid any traversal.
+    TP.elementSetAttribute(elem, 'href', '#', true);
+
+    //  Then add an 'onclick' that will trigger TIBET.
+    TP.elementSetAttribute(elem, 'onclick',
+        'TP.go2(\'' + href + '\', window); return false;',
+        true);
 
     return;
 });
