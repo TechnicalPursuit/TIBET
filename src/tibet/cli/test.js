2f0d5f1e25bac2d07e004fdb39fa0fd5872917ae
William J. Edney <bedney@technicalpursuit.com>
2015-07-17 08:51:34 -0500

    Updated usage to include --remote-debug-port

--- src/tibet/cli/test.js
+++ src/tibet/cli/test.js
@@ -110,7 +110,7 @@ Cmd.prototype.PARSE_OPTIONS = CLI.blend(
  * The command usage string.
  * @type {String}
  */
-Cmd.prototype.USAGE = 'tibet test [target|suite] [--target <target>] [--suite <suite>] [--cases <casename>] [--ignore-only] [--ignore-skip] [--no-tap]';
+Cmd.prototype.USAGE = 'tibet test [target|suite] [--target <target>] [--suite <suite>] [--cases <casename>] [--ignore-only] [--ignore-skip] [--no-tap] [--remote-debug-port <portnumber>]';
 
 //  ---
 //  Instance Methods
