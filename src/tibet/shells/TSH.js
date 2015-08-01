ce719f8b7596e9944e7cab9f55d07d5dee4829ff
William J. Edney <bedney@technicalpursuit.com>
2015-08-01 14:13:32 -0500

    Updated watching output to be fancier

--- src/tibet/shells/TSH.js
+++ src/tibet/shells/TSH.js
@@ -3935,7 +3935,12 @@ function(aRequest) {
 
     resourceHash = TP.core.URI.get('changedResources');
 
-    aRequest.stdout(TP.str(resourceHash));
+    //  Set cmdAsIs to false to get fancy JSON formatting.
+
+    //  NB: We do the TP.json2js(resourceHash.asJSONSource()) shuffle because
+    //  resourceHash has circular references that freak out the JSON parser.
+    aRequest.stdout(TP.jsoncc(TP.json2js(resourceHash.asJSONSource())),
+                    TP.hc('cmdAsIs', false));
 
     aRequest.complete();
 });
@@ -4011,10 +4016,12 @@ function(aRequest) {
     //  Note here how we go after the stored value - we might have changed it
     //  above.
     if (TP.isTrue(TP.sys.cfg('uri.process_remote_changes'))) {
-        aRequest.stdout('Remote URI change monitoring active for: ' +
-                        watchSources);
+        aRequest.stdout('Remote resource change monitoring active for: ');
+
+        //  Set cmdAsIs to false to get fancy JSON formatting.
+        aRequest.stdout(TP.jsoncc(watchSources), TP.hc('cmdAsIs', false));
     } else {
-        aRequest.stdout('Remote URI change monitoring inactive');
+        aRequest.stdout('Remote resource change monitoring inactive');
     }
 
     return aRequest.complete();
@@ -4031,16 +4038,27 @@ function(aRequest) {
     resourceHash = TP.core.URI.get('changedResources');
 
     handler = function(aSignal) {
-                var str,
-                    req;
-
-                str = TP.sc('Resource change: ') + resourceHash.asJSONSource();
+                var req;
 
                 req = TP.sig.UserOutputRequest.construct(
-                            TP.hc('output', str,
-                                    // TODO: alter this class to get attention
-                                    'cssClass', 'inbound_announce',
+                            TP.hc('output', 'Changed remote resources:',
                                     'cmdAsIs', true,
+                                    'cmdBox', false,
+                                    'cmdRecycle', true));
+
+                req.fire(this);
+
+                //  Set cmdAsIs to false to get fancy JSON formatting.
+
+                //  NB: We do the TP.json2js(resourceHash.asJSONSource())
+                //  shuffle because resourceHash has circular references that
+                //  freak out the JSON parser.
+                req = TP.sig.UserOutputRequest.construct(
+                            TP.hc('output',
+                                    TP.jsoncc(
+                                        TP.json2js(
+                                            resourceHash.asJSONSource())),
+                                    'cmdAsIs', false,
                                     'cmdBox', false,
                                     'cmdRecycle', true));
 
