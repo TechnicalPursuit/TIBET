a9b55dac4c1147b641376c17f9f1cb09cd3d37ed
William J. Edney <bedney@technicalpursuit.com>
2015-08-24 16:35:18 -0500

    Added test for Function request to use 'then()'

---
+++ test/src/tibet/requests/TP.sig.Request_Tests.js
@@ -0,0 +1,73 @@
+//  ========================================================================
+/**
+ * @copyright Copyright (C) 1999 Technical Pursuit Inc. (TPI) All Rights
+ *     Reserved. Patents Pending, Technical Pursuit Inc. Licensed under the
+ *     OSI-approved Reciprocal Public License (RPL) Version 1.5. See the RPL
+ *     for your rights and responsibilities. Contact TPI to purchase optional
+ *     privacy waivers if you must keep your TIBET-based source code private.
+ */
+//  ========================================================================
+
+/*
+ * Tests for request methods.
+ */
+
+//  ------------------------------------------------------------------------
+
+TP.sig.Request.Inst.describe('TP.sig.Request - chaining then() on response',
+function() {
+
+    this.it('Function request - synchronous success', function(test, options) {
+
+        var testFunction,
+
+            testRequest,
+            testResponse,
+
+            testPromise,
+
+            testResult;
+
+        testFunction = function() {
+                            return 'Hi there!';
+                        };
+
+        testRequest = testFunction.asFunctionRequest();
+        testResponse = testRequest.constructResponse();
+
+        //  Note here how we capture the Promise returned by 'then()'ing the
+        //  response and return it below from this test method. This is so that
+        //  it will get hooked up in the internal Promise chain for the test
+        //  harness and the code in 'test.then(...)' below will be executed
+        //  *after* the Promise.
+        testPromise = testResponse.then(
+                function(aResult) {
+                    testResult = 'The message is: ' + aResult;
+                });
+
+        testResult = '';
+        testRequest.fire();
+
+        test.then(
+                function(result) {
+                    test.assert.isEqualTo(
+                            testResult,
+                            'The message is: Hi there!');
+                });
+
+        return testPromise;
+    });
+
+});
+
+//  ========================================================================
+//  Run those babies!
+//  ------------------------------------------------------------------------
+
+/*
+TP.sig.Request.Inst.runTestSuites();
+*/
+
+//  ------------------------------------------------------------------------
+//  end
+//  ========================================================================
