ec49f9b169c66b6ca4ce52de8efb9989b0ed0ea3
idearat <ss@technicalpursuit.com>
2015-10-08 01:16:35 -0600

    Add simple login page and route handler for TDS default.

---
+++ dna/default/routes/login.js
@@ -0,0 +1,18 @@
+var fs,
+    path,
+    router;
+
+fs = require('fs');
+path = require('path');
+router = require('express').Router();
+
+router.get('/login', function(req, res) {
+    var str;
+
+    str = fs.readFileSync(path.join(__dirname, './login.xhtml'), 'utf8');
+
+    res.type('.xhtml');
+    res.send(str);
+});
+
+module.exports = router;
