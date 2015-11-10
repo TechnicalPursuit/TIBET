79cd15d673e53bdd23fe73a6d446d79d5d9e393d
William J. Edney <bedney@technicalpursuit.com>
2015-11-10 12:52:45 -0600

    Comment update

--- src/tibet/kernel/TIBETDataBinding.js
+++ src/tibet/kernel/TIBETDataBinding.js
@@ -1126,10 +1126,7 @@ function(aSignalOrHash) {
      *     the body, while in an XML document all elements including the
      *     documentElement are refreshed.
      * @param {TP.sig.UIRefresh|TP.core.Hash} aSignalOrHash An optional signal
-     *     which triggered this action or a hash. If this is a signal, this
-     *     method will try first to use 'getValue()' to get the value from the
-     *     binding. If there is no value there, or this is a hash, this method
-     *     will look under a key of TP.NEWVAL.
+     *     which triggered this action or a hash.
      *     This signal or hash should include a key of 'deep' and a value
      *     of true to cause a deep refresh that updates all nodes.
      * @returns {TP.core.DocumentNode} The receiver.
