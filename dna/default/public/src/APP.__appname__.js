2f5c777af425bd39636bde65712a69bfa8316104
idearat <ss@technicalpursuit.com>
2015-11-27 18:55:56 -0700

    Add Electron DNA

---
+++ dna/default/public/src/APP.__appname__.js
@@ -0,0 +1,15 @@
+/**
+ * @type {Namespace}
+ * @summary Defines namespace-level objects and functionality for the project.
+ */
+
+/**
+ * Define the JavaScript namespace object which will hold application code.
+ */
+TP.defineNamespace('APP.{{appname}}');
+
+/**
+ * Define the XML namespace and prefix for any tags in the application.
+ */
+TP.w3.Xmlns.registerNSInfo('urn:app:{{appname}}',
+    TP.hc('prefix', '{{appname}}'));
