c6f3c65918d8429fb5037ad2de261d98cdd894ef
William J. Edney <bedney@technicalpursuit.com>
2015-12-31 14:58:51 -0600

    Force editors to refresh and focus after appearing

--- src/tibet/tools/sherpa/src/TP.sherpa.elementeditor.js
+++ src/tibet/tools/sherpa/src/TP.sherpa.elementeditor.js
@@ -58,7 +58,13 @@ function() {
     editorObj.setOption('lineNumbers', true);
     editorObj.setOption('lineWrapping', true);
 
-    editorObj.refresh();
+    //  By forking this, we give the console a chance to focus the input cell
+    //  (which it really wants to do after executing a command) and then we can
+    //  shift the focus back to us.
+    (function() {
+        this.get('editor').refreshEditor();
+        this.get('editor').focus();
+    }).bind(this).fork(500);
 
     this.$set('didSetup', true);
 
