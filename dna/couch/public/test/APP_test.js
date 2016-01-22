32a78e922c4006ea6a0afa956827714eafdfdb9f
idearat <ss@technicalpursuit.com>
2016-01-21 17:07:10 -0700

    Factor dna app tag test into proper tag-level test file and update cfgs.

--- dna/couch/public/test/APP_test.js
+++ dna/couch/public/test/APP_test.js
@@ -13,10 +13,6 @@ APP.describe('APP suite', function() {
     this.it('Has an application type', function(test, options) {
         test.assert.isType(APP['{{appname}}']['Application']);
     });
-
-    this.it('Has an app tag', function(test, options) {
-        test.assert.isType(APP['{{appname}}']['app']);
-    });
 });
 
 //  ------------------------------------------------------------------------
---
+++ dna/default/public/src/tags/APP.__appname__.app_test.js
@@ -0,0 +1,16 @@
+/*
+ * APP.{{appname}}:app top-level tests.
+ */
+
+//  ------------------------------------------------------------------------
+
+APP.{{appname}}.app.describe('APP.{{appname}}:app suite', function() {
+
+    this.it('Is a templated tag', function(test, options) {
+        test.assert.isKindOf(APP.{{appname}}.app, 'TP.core.TemplatedTag');
+    });
+});
+
+//  ------------------------------------------------------------------------
+//  end
+//  ========================================================================
