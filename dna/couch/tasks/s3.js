84f80fbede3f8c84081b35ad495c370b9e364eb3
William J. Edney <bedney@technicalpursuit.com>
2016-02-11 22:14:45 -0600

    Make this real

--- dna/couch/tasks/s3.js
+++ dna/couch/tasks/s3.js
@@ -12,17 +12,44 @@
     'use strict';
 
     module.exports = function(options) {
-        var logger;
+        var app,
+            logger,
+            TDS,
+
+            AWS,
+            s3Service;
+
+        //  ---
+        //  Config Check
+        //  ---
+
+        app = options.app;
+        if (!app) {
+            throw new Error('No application instance provided.');
+        }
+
+        logger = options.logger;
+        TDS = app.TDS;
 
         //  ---
         //  Requires
         //  ---
 
-        logger = options.logger;
+        AWS = require('aws-sdk');
 
         //  ---
         //  Variables
         //  ---
+
+        s3Service = new AWS.S3(
+            {
+                accessKeyId: 'AKIAI3VQYU7ZLMBFMGCA',
+                secretAccessKey: '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',
+                region: 'us-east-1',
+                params: {
+                    Bucket: 'bedney'
+                }
+            });
 
         //  ---
         //  Task
@@ -32,11 +59,57 @@
          * The actual task execution function which will be invoked by the task
          * runner.
          */
-        return function(json) {
+        return function(job, step, taskOptions) {
 
-            console.log('\n');
-            console.log('processing s3 for: ' +
-                JSON.stringify(json));
+            var params,
+
+                uploadParams;
+
+            logger.debug('\n');
+            logger.debug('processing s3 for: ' +
+                JSON.stringify(step));
+
+            params = step.params;
+
+            uploadParams = {
+                Key: 'stuff',
+                Body: params.body || 'This is some body content'
+            };
+
+            s3Service.upload(
+                uploadParams,
+                function(err, resp) {
+
+                    var nano,
+                        db;
+
+                    if (err) {
+                        logger.error(
+                                'S3 put operation failed: ' +
+                                    TDS.beautify(JSON.stringify(err)));
+                    } else {
+                        logger.debug(
+                                'S3 put operation succeeded: ' +
+                                    TDS.beautify(JSON.stringify(resp)));
+
+                        nano = require('nano')(taskOptions.db_url);
+                        db = nano.use(taskOptions.db_name);
+
+                        step.end = Date.now();
+                        step.state = '$$complete';
+
+                        logger.debug('update prepped: ' + JSON.stringify(step));
+
+                        db.insert(job, function(err2, body) {
+                            if (err2) {
+                                logger.error('job update failed: ' + err2);
+                            }
+
+                            logger.debug('job update succeeded: ' +
+                                TDS.beautify(JSON.stringify(body)));
+                        });
+                    }
+                });
         };
     };
 }());
