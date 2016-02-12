1297bf14aa20b6dffc7c36f720773377a72418ef
William J. Edney <bedney@technicalpursuit.com>
2016-02-12 09:59:07 -0600

    Added new files for new panels in the Sherpa

---
+++ src/tibet/tools/sherpa/src/TP.sherpa.drawer.js
@@ -0,0 +1,33 @@
+//  ========================================================================
+/**
+ * @copyright Copyright (C) 1999 Technical Pursuit Inc. (TPI) All Rights
+ *     Reserved. Patents Pending, Technical Pursuit Inc. Licensed under the
+ *     OSI-approved Reciprocal Public License (RPL) Version 1.5. See the RPL
+ *     for your rights and responsibilities. Contact TPI to purchase optional
+ *     privacy waivers if you must keep your TIBET-based source code private.
+ */
+//  ========================================================================
+
+/**
+ * @type {TP.sherpa.drawer}
+ */
+
+//  ------------------------------------------------------------------------
+
+TP.sherpa.TemplatedTag.defineSubtype('drawer');
+
+//  ------------------------------------------------------------------------
+//  Instance Methods
+//  ------------------------------------------------------------------------
+
+TP.sherpa.drawer.Inst.defineHandler('Toggle',
+function(aSignal) {
+
+    TP.bySystemId(aSignal.getOrigin()).toggle('closed');
+
+    return this;
+});
+
+//  ------------------------------------------------------------------------
+//  end
+//  ========================================================================
---
+++ src/tibet/tools/sherpa/src/TP.sherpa.opener.js
@@ -0,0 +1,44 @@
+//  ========================================================================
+/**
+ * @copyright Copyright (C) 1999 Technical Pursuit Inc. (TPI) All Rights
+ *     Reserved. Patents Pending, Technical Pursuit Inc. Licensed under the
+ *     OSI-approved Reciprocal Public License (RPL) Version 1.5. See the RPL
+ *     for your rights and responsibilities. Contact TPI to purchase optional
+ *     privacy waivers if you must keep your TIBET-based source code private.
+ */
+//  ========================================================================
+
+/**
+ * @type {TP.sherpa.opener}
+ */
+
+//  ------------------------------------------------------------------------
+
+TP.sherpa.Element.defineSubtype('opener');
+
+//  ------------------------------------------------------------------------
+//  Type Methods
+//  ------------------------------------------------------------------------
+
+TP.sherpa.opener.Type.defineMethod('tagCompile',
+function(aRequest) {
+    var elem,
+        newElem;
+
+    //  Get the initiating element.
+    if (!TP.isElement(elem = aRequest.at('node'))) {
+        return;
+    }
+
+    TP.elementSetAttribute(elem, 'on:click', 'Toggle', true);
+
+    newElem = TP.xhtmlnode('<span class="icon"/>');
+
+    TP.nodeAppendChild(elem, newElem, false);
+
+    return elem;
+});
+
+//  ------------------------------------------------------------------------
+//  end
+//  ========================================================================
