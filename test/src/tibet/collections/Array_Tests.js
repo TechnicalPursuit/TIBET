6a890dd6c14bbc2937565c160762d94e0e59190d
idearat <ss@technicalpursuit.com>
2016-03-04 17:52:31 -0700

    Add some preliminary test infrastructure for Array testing.

---
+++ test/src/tibet/collections/Array_Tests.js
@@ -0,0 +1,49 @@
+//  ========================================================================
+/**
+ * @copyright Copyright (C) 1999 Technical Pursuit Inc. (TPI) All Rights
+ *     Reserved. Patents Pending, Technical Pursuit Inc. Licensed under the
+ *     OSI-approved Reciprocal Public License (RPL) Version 1.5. See the RPL
+ *     for your rights and responsibilities. Contact TPI to purchase optional
+ *     privacy waivers if you must keep your TIBET-based source code private.
+ */
+//  ========================================================================
+
+/*
+ * Tests for the Array type.
+ */
+
+//  ------------------------------------------------------------------------
+
+Array.describe('remove',
+function() {
+
+    this.it('removed all instances of primitive value', function(test, options) {
+        var arr;
+
+        arr = [1, 2, 5, 3, 4, 5];
+        arr.remove(5);
+
+        this.assert.isEqualTo(arr.length, 4);
+        this.assert.isEqualTo(arr.join(''), '1234');
+    });
+
+    this.it('removed all instances of object value', function(test, options) {
+        var arr,
+            obja,
+            objb;
+
+        obja = {a: 1};
+        objb = {b: 2};
+
+        arr = [obja, objb, obja, obja, objb, obja];
+        arr.remove(objb, TP.IDENTITY);
+
+        this.assert.isEqualTo(arr.length, 4);
+        this.assert.isEqualTo(JSON.stringify(arr),
+            '[{"a":1},{"a":1},{"a":1},{"a":1}]');
+    });
+});
+
+//  ------------------------------------------------------------------------
+//  end
+//  ========================================================================
