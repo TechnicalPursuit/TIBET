4b3b15364087258a3331a5dc2e914008e85cf719
William J. Edney <bedney@technicalpursuit.com>
2016-05-15 06:40:43 -0500

    Split into files and cleaned up loads of doclint

---
+++ src/tibet/testing/TP.log.TestAppender.js
@@ -0,0 +1,85 @@
+//  ========================================================================
+/**
+ * @copyright Copyright (C) 1999 Technical Pursuit Inc. (TPI) All Rights
+ *     Reserved. Patents Pending, Technical Pursuit Inc. Licensed under the
+ *     OSI-approved Reciprocal Public License (RPL) Version 1.5. See the RPL
+ *     for your rights and responsibilities. Contact TPI to purchase optional
+ *     privacy waivers if you must keep your TIBET-based source code private.
+ */
+//  ------------------------------------------------------------------------
+
+/**
+ * @type {TP.log.TestAppender}
+ * @summary A log appender that will fail the currently executing test if any
+ *     TIBET code that is currently executing in support of the test logs an
+ *     error to the TIBET logging machinery.
+ */
+
+//  ----------------------------------------------------------------------------
+
+TP.log.Appender.defineSubtype('TestAppender');
+
+//  ----------------------------------------------------------------------------
+//  Type Attributes
+//  ----------------------------------------------------------------------------
+
+/**
+ * The default layout type for this appender. Note that we use the console
+ * layout for this type since we want the plain text.
+ * @type {TP.log.Layout}
+ */
+TP.log.TestAppender.Type.$set('defaultLayoutType', 'TP.log.ConsoleLayout');
+
+//  ----------------------------------------------------------------------------
+//  Instance Attribute
+//  ----------------------------------------------------------------------------
+
+TP.log.TestAppender.Inst.defineAttribute('currentTestCase');
+
+//  ----------------------------------------------------------------------------
+//  Instance Methods
+//  ----------------------------------------------------------------------------
+
+TP.log.TestAppender.Inst.defineMethod('append',
+function(anEntry) {
+
+    /**
+     * @method append
+     * @summary Formats the entry data using the receiver's layout and writes
+     *     it to the console using the best console API method possible.
+     * @param {TP.log.Entry} anEntry The log entry to format and append.
+     * @returns {TP.log.Appender} The receiver.
+     */
+
+    var layout,
+        content,
+
+        currentCase;
+
+    //  If this flag is not true, then just bail out here.
+    if (!TP.sys.cfg('test.fail_on_error_log')) {
+        return this;
+    }
+
+    //  If the entry is an Error or more severe, then we want to fail the test
+    //  case.
+    if (anEntry.isError()) {
+
+        layout = this.getLayout();
+        content = layout.layout(anEntry).at('content');
+        currentCase = this.get('currentTestCase');
+
+        //  We need to set the status code back to TP.ACTIVE here - the test
+        //  thinks it has completed, but since this could be being called
+        //  from an asynchronous callback and it failed, then need to
+        //  'reactivate' the test and fail it.
+        currentCase.set('statusCode', TP.ACTIVE);
+        currentCase.fail(content);
+    }
+
+    return this;
+});
+
+//  ------------------------------------------------------------------------
+//  end
+//  ========================================================================
---
+++ src/tibet/testing/TP.test.Root.js
@@ -0,0 +1,268 @@
+//  ========================================================================
+/**
+ * @copyright Copyright (C) 1999 Technical Pursuit Inc. (TPI) All Rights
+ *     Reserved. Patents Pending, Technical Pursuit Inc. Licensed under the
+ *     OSI-approved Reciprocal Public License (RPL) Version 1.5. See the RPL
+ *     for your rights and responsibilities. Contact TPI to purchase optional
+ *     privacy waivers if you must keep your TIBET-based source code private.
+ */
+//  ------------------------------------------------------------------------
+
+/**
+ * @type {TP.test.Root}
+ * @summary Common supertype for other TP.test objects like TP.test.Case and
+ *     TP.test.Suite.
+ */
+
+TP.lang.Object.defineSubtype('test.Root');
+
+//  can't construct concrete instances of this
+TP.test.Root.isAbstract(true);
+
+//  Add support for job control status tracking and querying.
+
+//  NB: For PhantomJS, we resolve the traits right away, due to a crashing bug.
+TP.test.Root.addTraits(TP.core.JobStatus);
+
+TP.test.Root.Inst.resolveTrait('resume', TP.core.JobStatus);
+
+//  ------------------------------------------------------------------------
+//  Instance Attributes
+//  ------------------------------------------------------------------------
+
+/**
+ * Whether the test object is marked exclusive. If so it will be run exclusively
+ * or a warning will be output if multiple 'exclusives' are found.
+ * @type {Boolean}
+ */
+TP.test.Root.Inst.defineAttribute('exclusive');
+
+/**
+ * Whether the test object is marked todo. If so it will be run but any result
+ * is effectively ignored.
+ * @type {Boolean}
+ */
+TP.test.Root.Inst.defineAttribute('ignored');
+
+/**
+ * The millisecond timestamp at completion of the task.
+ * @type {Number}
+ */
+TP.test.Root.Inst.defineAttribute('msend');
+
+/**
+ * The number of milliseconds the object is limited to for run time before
+ * timing out. Default here is 60 seconds but this is reset by Suite and Case.
+ * @type {Number}
+ */
+TP.test.Root.Inst.defineAttribute('mslimit',
+                                    TP.sys.cfg('test.suite_mslimit'));
+
+/**
+ * The millisecond timestamp at start of the task.
+ * @type {Number}
+ */
+TP.test.Root.Inst.defineAttribute('msstart');
+
+/**
+ * Whether the test object is marked skipped. If so it will be listed in output
+ * but not actually run and it will be counted as skipped.
+ * @type {Boolean}
+ */
+TP.test.Root.Inst.defineAttribute('skipped');
+
+//  ------------------------------------------------------------------------
+//  Instance Methods
+//  ------------------------------------------------------------------------
+
+TP.test.Root.Inst.defineMethod('getElapsedTime',
+function() {
+
+    /**
+     * Returns the number of milliseconds of elapsed time for the operation.
+     * @returns {Number}
+     */
+
+    var end,
+        start;
+
+    //  If we haven't started elapsed time is 0.
+    start = this.get('msstart');
+    if (TP.notValid(start)) {
+        return 0;
+    }
+
+    //  If we've finished report based on that time, but otherwise assume we're
+    //  asking for a value 'to this point in time' and use 'now' instead of end.
+    end = this.get('msend');
+    if (TP.notValid(end)) {
+        end = Date.now();
+    }
+
+    return end - start;
+});
+
+//  ------------------------------------------------------------------------
+
+TP.test.Root.Inst.defineMethod('getTimeout',
+function() {
+
+    /**
+     * Returns the millisecond timeout value for the test case.
+     * @returns {Number}
+     */
+
+    return this.$get('mslimit');
+});
+
+//  ------------------------------------------------------------------------
+
+TP.test.Root.Inst.defineMethod('getTimeRemaining',
+function() {
+
+    /**
+     * Returns the number of milliseconds remaining between elapsed time and the
+     * receiver's timeout value.
+     * @returns {Number}
+     */
+
+    return Math.max(0, this.getTimeout() - this.getElapsedTime());
+});
+
+//  ------------------------------------------------------------------------
+
+TP.test.Root.Inst.defineMethod('isExclusive',
+function() {
+
+    /**
+     * Returns true if the receiver is configured to be run exclusively.
+     * @returns {Boolean} True if the receiver is exclusive.
+     */
+
+    return this.$get('exclusive') === true;
+});
+
+//  ------------------------------------------------------------------------
+
+TP.test.Root.Inst.defineMethod('isSkipped',
+function() {
+
+    /**
+     * Returns true if the receiver is configured to be skipped (not run).
+     * @returns {Boolean} True if the receiver is skipped.
+     */
+
+    return this.$get('skipped') === true;
+});
+
+//  ------------------------------------------------------------------------
+
+TP.test.Root.Inst.defineMethod('isTodo',
+function() {
+
+    /**
+     * Returns true if the receiver is configured as a 'todo' test.
+     * @returns {Boolean} True if the receiver is marked as a todo item.
+     */
+
+    return this.$get('ignored') === true;
+});
+
+//  ------------------------------------------------------------------------
+
+TP.test.Root.Inst.defineMethod('only',
+function() {
+
+    /**
+     * Marks the receiver as exclusive, meaning it should be the only item run
+     * in a list of multiple items. When multiple items are marked as being
+     * exclusive only the first of them will be run.
+     * @returns {TP.test.Root} The receiver.
+     */
+
+    this.$set('exclusive', true);
+
+    return this;
+});
+
+//  ------------------------------------------------------------------------
+
+TP.test.Root.Inst.defineMethod('reset',
+function(options) {
+
+    /**
+     * Resets the receiver, putting instance variables back to their original
+     * values so it can be run again.
+     * @param {TP.core.Hash} options A dictionary of test options.
+     * @returns {TP.test.Root} The receiver.
+     */
+
+    this.$set('result', null);
+
+    this.$set('faultCode', null);
+    this.$set('faultText', null);
+    this.$set('faultInfo', null);
+
+    this.$set('statusCode', null);
+    this.$set('statusText', null);
+
+    return this;
+});
+
+//  ------------------------------------------------------------------------
+
+TP.test.Root.Inst.defineMethod('skip',
+function(shouldSkip) {
+
+    /**
+     * Marks the receiver as skipped, meaning it will be listed but not run.
+     * @param {Boolean} shouldSkip Whether or not to skip this test or suite.
+     *     Defaults to true.
+     * @returns {TP.test.Root} The receiver.
+     */
+
+    var skip;
+
+    skip = TP.ifInvalid(shouldSkip, true);
+
+    this.$set('skipped', skip);
+
+    return this;
+});
+
+//  ------------------------------------------------------------------------
+
+TP.test.Root.Inst.defineMethod('timeout',
+function(ms) {
+
+    /**
+     * Defines a millisecond limit on how long the receiver can run before being
+     * timed out (which cause the Case or Suite to fail).
+     * @param {Number} ms The millisecond timeout value.
+     * @returns {TP.test.Root} The receiver.
+     */
+
+    this.$set('mslimit', ms);
+
+    return this;
+});
+
+//  ------------------------------------------------------------------------
+
+TP.test.Root.Inst.defineMethod('todo',
+function() {
+
+    /**
+     * Marks the receiver as todo, meaning it will be run but its result will
+     * always be considered as a non-failure for overall reporting purposes.
+     * @returns {TP.test.Root} The receiver.
+     */
+
+    this.$set('ignored', true);
+
+    return this;
+});
+
+//  ------------------------------------------------------------------------
+//  end
+//  ========================================================================
