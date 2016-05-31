4e33b4349494a8462247a1622ec78802f73f47c3
William J. Edney <bedney@technicalpursuit.com>
2016-05-31 16:05:33 -0500

    Add Cancel action

--- src/tsh/TP.tsh.tag/TP.tsh.tag_assistant.js
+++ src/tsh/TP.tsh.tag/TP.tsh.tag_assistant.js
@@ -64,6 +64,22 @@ function() {
 
 //  ------------------------------------------------------------------------
 
+TP.tsh.tag_assistant.Inst.defineHandler('CancelAction',
+function(anObject) {
+
+    /**
+     * @method handleCancelAction
+     * @summary
+     * @returns {TP.tsh.tag_assistant} The receiver.
+     */
+
+    this.get('tileTPElem').setAttribute('hidden', true);
+
+    return this;
+});
+
+//  ------------------------------------------------------------------------
+
 TP.tsh.tag_assistant.Inst.defineHandler('ExecuteCommand',
 function(anObject) {
 
