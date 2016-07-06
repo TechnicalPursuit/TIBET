fe556601cbe3643a9710f25a1d519174a93a2bdc
idearat <ss@technicalpursuit.com>
2016-07-06 13:59:20 -0600

    Fix closure'd this reference bug.

--- src/tibet/cli/release.js
+++ src/tibet/cli/release.js
@@ -552,11 +552,11 @@ Cmd.prototype.phaseTwo = function(source) {
         sh.exec(cmd, function(code, output) {
             if (code !== 0) {
                 release.error(output);
-                result = this.prompt.question(
+                result = release.prompt.question(
                     'tibet checkup detected errors. Continue anyway?' +
                     ' Enter \'yes\' after inspection: ');
                 if (!/^y/i.test(result)) {
-                    this.log('Release cancelled. Revert uncommitted branch changes.');
+                    release.log('Release cancelled. Revert uncommitted branch changes.');
                     return;
                 }
                 return;
