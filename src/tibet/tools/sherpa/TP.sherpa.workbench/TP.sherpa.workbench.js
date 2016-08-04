b28e13f0225bd2d2872f21e1857391500eb4ca8c
William J. Edney <bedney@technicalpursuit.com>
2016-08-03 21:48:32 -0500

    Added scroll buttons for inspector and wired them in

--- src/tibet/tools/sherpa/TP.sherpa.workbench/TP.sherpa.workbench.js
+++ src/tibet/tools/sherpa/TP.sherpa.workbench/TP.sherpa.workbench.js
@@ -17,11 +17,61 @@
 TP.sherpa.TemplatedTag.defineSubtype('workbench');
 
 //  ------------------------------------------------------------------------
+//  Type Methods
+//  ------------------------------------------------------------------------
+
+TP.sherpa.workbench.Type.defineMethod('tagAttachDOM',
+function(aRequest) {
+
+    /**
+     * @method tagAttachDOM
+     * @summary Sets up runtime machinery for the element in aRequest
+     * @param {TP.sig.Request} aRequest A request containing processing
+     *     parameters and other data.
+     */
+
+    var elem,
+        tpElem,
+
+        sherpaInspectorTPElem,
+
+        arrows;
+
+    //  this makes sure we maintain parent processing
+    this.callNextMethod();
+
+    //  Make sure that we have an Element to work from
+    if (!TP.isElement(elem = aRequest.at('node'))) {
+        //  TODO: Raise an exception.
+        return;
+    }
+
+    tpElem = TP.wrap(elem);
+
+    sherpaInspectorTPElem = TP.byId('SherpaInspector',
+                                    tpElem.getNativeWindow());
+
+    arrows = TP.byCSSPath('sherpa|scrollbutton',
+                            elem,
+                            false,
+                            true);
+
+    arrows.forEach(
+            function(anArrow) {
+                anArrow.set('scrollingContentTPElem', sherpaInspectorTPElem);
+            });
+
+    return;
+});
+
+//  ------------------------------------------------------------------------
+//  Instance Methods
+//  ------------------------------------------------------------------------
 
 TP.sherpa.workbench.Inst.defineHandler('SaveCanvas',
 function(aSignal) {
 
-	TP.info('Save the canvas content');
+    TP.info('Save the canvas content');
 
     return this;
 });
