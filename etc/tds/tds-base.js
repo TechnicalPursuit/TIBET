d51a37ac969b1a5f3af66019e9d7d8f098ca3e4a
William J. Edney <bedney@technicalpursuit.com>
2016-09-25 20:49:36 -0500

    Don’t filter out non-IPv4 or internal addresses
    Our comparison routines that use this data are now much more robust.

--- etc/tds/tds-base.js
+++ etc/tds/tds-base.js
@@ -418,11 +418,6 @@
 
         Object.keys(ifaces).forEach(function (ifname) {
           ifaces[ifname].forEach(function (iface) {
-            if ('IPv4' !== iface.family || iface.internal !== false) {
-              // skip over internal (i.e. 127.0.0.1) and non-ipv4 addresses
-              return;
-            }
-
             addresses.push(iface.address);
           });
         });
