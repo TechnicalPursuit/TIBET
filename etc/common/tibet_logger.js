9db4eec9be7661656f886b93904b4dcdc02e8d00
idearat <ss@technicalpursuit.com>
2016-10-16 06:26:55 -0600

    Allow color specs in leveled logging calls. Improve commenting.

--- etc/common/tibet_logger.js
+++ etc/common/tibet_logger.js
@@ -5,8 +5,8 @@
  *     OSI-approved Reciprocal Public License (RPL) Version 1.5. See the RPL
  *     for your rights and responsibilities. Contact TPI to purchase optional
  *     privacy waivers if you must keep your TIBET-based source code private.
- * @overview Simple server-side logging methods which match the basic API found
- *     in the client.
+ * @overview Simple server-side logging methods which support the basic API
+ *     found in the client with respect to logging levels etc.
  */
 //  ========================================================================
 
@@ -19,17 +19,21 @@
 
     Color = require('./tibet_color');
 
+
     /**
-     *
+     * Creates a new logger instance for use in logging leveled, colorized
+     * output to the system console.
+     * @param {Object} options Options for controlling the new instance. Common
+     *     values of interest to the logger include 'level', 'debug',
+     *     and those required by the 'Color' object found in tibet_color.js.
+     * @return {Logger} The primitive logger instance.
      */
     Logger = function(options) {
         var level;
 
         this.options = options || {};
 
-        if (this.options.verbose) {
-            level = Logger.TRACE;
-        } else if (this.options.debug) {
+        if (this.options.debug) {
             level = Logger.DEBUG;
         } else {
             level = Logger.INFO;
@@ -41,7 +45,8 @@
         return this;
     };
 
-    //  Standard TIBET logging levels.
+
+    //  Standard TIBET logging levels. Used to filter based on level.
     Logger.TRACE = 1;
     Logger.DEBUG = 2;
     Logger.INFO = 3;
@@ -51,6 +56,11 @@
     Logger.FATAL = 7;
     Logger.SYSTEM = 8;
 
+
+    /**
+     * Levels in string form. Used to verify a logging level is valid.
+     * @type {Array.<String>}
+     */
     Logger.LEVELS = [
         'any',
         'trace',
@@ -63,17 +73,24 @@
         'system'
     ];
 
+
     /**
-     *
+     * Returns the currently set logging level for this instance.
+     * @return {Number} The logging level as a numeric value.
      */
     Logger.prototype.getLevel = function() {
         return this.level;
     };
 
+
     /**
-     *
+     * Returns the normalized level value from a string or numeric level
+     * representation. Used to ensure level values are always numeric internally
+     * to the logger and other methods.
+     * @param {String|Number} aLevel The numeric or string level to normalize.
+     * @return {Number} The numeric level value for the input level.
      */
-    Logger.prototype.setLevel = function(aLevel) {
+    Logger.prototype.getLevelValue = function(aLevel) {
         var lvl;
 
         if (typeof aLevel === 'number') {
@@ -85,11 +102,32 @@
             }
         }
 
-        this.level = lvl;
+        return lvl;
     };
 
+
     /**
-     *
+     * Sets the current logging level for this instance.
+     * @param {String|Number} aLevel The numeric or string level to set.
+     * @return {Number} The newly set logging level as a numeric value.
+     */
+    Logger.prototype.setLevel = function(aLevel) {
+        var lvl;
+
+        lvl = this.getLevelValue(aLevel);
+        this.level = lvl;
+
+        return this;
+    };
+
+
+    /**
+     * Logs a message to the console, filtering it by the level provided and
+     * optionally colorizing it using the colorizing spec given.
+     * @param {String} msg The message to log.
+     * @param {String} spec A colorizing spec per tibet_color.js requirements.
+     * @param {Number|String} [level=Logger.INFO] The level to filter by.
+     * @return {Logger} The logger instance.
      */
     Logger.prototype.log = function(msg, spec, level) {
         var lvl;
@@ -98,45 +136,85 @@
             return;
         }
 
-        lvl = level || Logger.INFO;
+        lvl = this.getLevelValue(level || Logger.INFO);
         if (lvl < this.getLevel()) {
             return;
         }
 
         console.log(this.color.colorize(msg, spec));
+
+        return this;
     };
 
 
-    Logger.prototype.trace = function(msg) {
-        this.log(msg, 'trace', Logger.TRACE);
+    Logger.prototype.trace = function(msg, spec) {
+        var s;
+
+        s = spec ? spec + '.trace' : 'trace';
+        return this.log(msg, s, Logger.TRACE);
     };
 
-    Logger.prototype.debug = function(msg) {
-        this.log(msg, 'debug', Logger.DEBUG);
+    Logger.prototype.debug = function(msg, spec) {
+        var s;
+
+        s = spec ? spec + '.debug' : 'debug';
+        return this.log(msg, s, Logger.DEBUG);
     };
 
-    Logger.prototype.info = function(msg) {
-        this.log(msg, 'info', Logger.INFO);
+    Logger.prototype.info = function(msg, spec) {
+        var s;
+
+        s = spec ? spec + '.info' : 'info';
+        return this.log(msg, s, Logger.INFO);
     };
 
-    Logger.prototype.warn = function(msg) {
-        this.log(msg, 'warn', Logger.WARN);
+    Logger.prototype.warn = function(msg, spec) {
+        var s;
+
+        s = spec ? spec + '.warn' : 'warn';
+        return this.log(msg, s, Logger.WARN);
     };
 
-    Logger.prototype.error = function(msg) {
-        this.log(msg, 'error', Logger.ERROR);
+    Logger.prototype.error = function(msg, spec) {
+        var s;
+
+        s = spec ? spec + '.error' : 'error';
+        return this.log(msg, s, Logger.ERROR);
     };
 
-    Logger.prototype.severe = function(msg) {
-        this.log(msg, 'severe', Logger.SEVERE);
+    Logger.prototype.severe = function(msg, spec) {
+        var s;
+
+        s = spec ? spec + '.severe' : 'severe';
+        return this.log(msg, s, Logger.SEVERE);
     };
 
-    Logger.prototype.fatal = function(msg) {
-        this.log(msg, 'fatal', Logger.FATAL);
+    Logger.prototype.fatal = function(msg, spec) {
+        var s;
+
+        s = spec ? spec + '.fatal' : 'fatal';
+        return this.log(msg, s, Logger.FATAL);
     };
 
-    Logger.prototype.system = function(msg) {
-        this.log(msg, 'system', Logger.SYSTEM);
+    Logger.prototype.system = function(msg, spec) {
+        var s;
+
+        s = spec ? spec + '.system' : 'system';
+        return this.log(msg, s, Logger.SYSTEM);
+    };
+
+    /**
+     * Write verbose output at the current logging level. Note that this is not
+     * a leveled logging method, it's based purely on whether --verbose is true.
+     */
+    Logger.prototype.verbose = function(msg, spec) {
+        var s;
+
+        if (this.options.verbose !== true) {
+            return;
+        }
+
+        return this.log(msg, spec);
     };
 
     //  ---
