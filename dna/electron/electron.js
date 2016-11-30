9de16b2835ab6719e9d74b275cbb647a47eaa9c6
idearat <ss@technicalpursuit.com>
2016-11-30 11:04:41 -0700

    Fix electron DNA.

--- dna/electron/electron.js
+++ dna/electron/electron.js
@@ -1,56 +1,58 @@
 var app,
     BrowserWindow,
-    window;
+    path,
+    url,
+    win;
 
-app = require('app'); // Module to control application life.
-BrowserWindow = require('browser-window'); // Module to create native browser window.
-
-// Report crashes to our server.
-require('crash-reporter').start();
+electron = require('electron');
+app = electron.app;
+BrowserWindow = electron.BrowserWindow;
+path = require('path');
+url = require('url');
 
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the JavaScript object is GCed.
-window = null;
+win = null;
+
+// This method will be called when Electron has finished
+// initialization and is ready to create browser windows.
+app.on('ready', function() {
+
+    // Create the browser window.
+    win = new BrowserWindow({
+        width: 1024,
+        height: 768
+    });
+
+    // Load the index.html of the app. This will boot TIBET.
+    //  TODO:   read command line args for boot profile etc.
+    win.loadURL(url.format({
+        pathname: path.join(__dirname, 'index.html'),
+        protocol: 'file:',
+        slashes: true
+    }));
+
+    //  TODO: configurable
+    // Open the devtools.
+    /*
+    win.openDevTools();
+    */
+
+    // Emitted when the window is closed.
+    win.on('closed', function() {
+        // Dereference the window object, usually you would store windows
+        // in an array if your app supports multi windows, this is the time
+        // when you should delete the corresponding element.
+        win = null;
+    });
+});
 
 // Quit when all windows are closed.
 app.on('window-all-closed', function() {
     // On OS X it is common for applications and their menu bar
     // to stay active until the user quits explicitly with Cmd + Q
     //  TODO:   make this a preference.
-    if (process.platform !== 'darwin') {
+    //if (process.platform !== 'darwin') {
         app.quit();
-    }
-});
-
-// This method will be called when Electron has finished
-// initialization and is ready to create browser windows.
-app.on('ready', function() {
-    // Create the browser window.
-    // window = new BrowserWindow({width: 1024, height: 768, show: false});
-    window = new BrowserWindow({
-        width: 1024,
-        height: 768,
-        show: true // ,
-        // 'node-integration': false
-    });
-
-    window.webContents.on('did-finish-load', function() {
-        // window.webContents.executeJavaScript("alert('Hello There!');");
-    });
-
-    // and load the index.html of the app.
-    //  TODO:   read command line args for boot profile etc.
-    window.loadUrl('file://' + __dirname + '/index.html');
-
-    //  TODO: configurable
-    // Open the devtools.
-    // window.openDevTools();
-
-    // Emitted when the window is closed.
-    window.on('closed', function() {
-        // Dereference the window object, usually you would store windows
-        // in an array if your app supports multi windows, this is the time
-        // when you should delete the corresponding element.
-        window = null;
-    });
+    //}
 });
