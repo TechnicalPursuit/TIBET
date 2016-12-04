49955b2a57ef678a44ce2c449bdbe4ff9febb348
idearat <ss@technicalpursuit.com>
2016-12-04 13:14:57 -0700

    Remove lint.

--- dna/electron/electron.js
+++ dna/electron/electron.js
@@ -1,5 +1,6 @@
 var app,
     BrowserWindow,
+    electron,
     path,
     url,
     win;
@@ -52,7 +53,7 @@ app.on('window-all-closed', function() {
     // On OS X it is common for applications and their menu bar
     // to stay active until the user quits explicitly with Cmd + Q
     //  TODO:   make this a preference.
-    //if (process.platform !== 'darwin') {
+    if (process.platform !== 'darwin') {
         app.quit();
-    //}
+    }
 });
