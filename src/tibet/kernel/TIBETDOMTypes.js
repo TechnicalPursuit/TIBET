e56a7c57a8205a2dd763f03c2f967bc50508f832
William J. Edney <bedney@technicalpursuit.com>
2016-12-13 06:29:44 -0600

    Add method to determine whether to wrap ACP output
    We override this in all xctrls Elements so that expressions in their output won’t be wrapped.

--- src/tibet/kernel/TIBETDOMTypes.js
+++ src/tibet/kernel/TIBETDOMTypes.js
@@ -995,6 +995,23 @@ function(aRequest) {
 
 //  ------------------------------------------------------------------------
 
+TP.core.Node.Type.defineMethod('shouldWrapACPOutput',
+function() {
+
+    /**
+     * @method shouldWrapACPOutput
+     * @summary Whether or not we should wrap ACP expression output in an XHTML
+     *     span element. The default is true, but some subtypes that allow ACP
+     *     in their embedded templates might choose to not generate these
+     *     wrapper spans.
+     * @returns {Boolean} Whether or not to wrap it.
+     */
+
+    return true;
+});
+
+//  ------------------------------------------------------------------------
+
 TP.core.Node.Type.defineMethod('tagPrecompile',
 function(aRequest) {
 
@@ -1059,6 +1076,7 @@ function(aRequest) {
     info.atPut('$TAG', tpNode);
 
     info.atPut('shouldEcho', false);
+    info.atPut('annotateMarkup', this.shouldWrapACPOutput());
 
     //  Grab the best representation text. This may contain ACP templating
     //  expressions.
