5befca229ce5b065bbfc4f8f14aa9924fcf467e6
William J. Edney <bedney@technicalpursuit.com>
2016-12-25 12:17:18 -0600

    Kernel fixes now allow this
    These registrations should be happening at the control level.

--- src/xctrls/TP.xctrls.dialog/TP.xctrls.dialog.keybindings.js
+++ src/xctrls/TP.xctrls.dialog/TP.xctrls.dialog.keybindings.js
@@ -3,5 +3,5 @@
  */
 
 //  Dismiss dialog
-TP.core.UIElementNode.registerKeybinding('DOM_Esc_Up', 'TP.sig.DialogCancel');
+TP.xctrls.dialog.registerKeybinding('DOM_Esc_Up', 'TP.sig.DialogCancel');
 
