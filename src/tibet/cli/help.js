70e5ca375cc75afdd78f2b73f216cb50f844129c
idearat <ss@technicalpursuit.com>
2017-01-19 20:32:27 -0700

    Add support for finding missing topics and generating project-level help.

--- src/tibet/cli/help.js
+++ src/tibet/cli/help.js
@@ -22,13 +22,16 @@ var CLI,
     opener,
     path,
     sh,
+    hb,
+    nodecli,
     Cmd;
 
 CLI = require('./_cli');
 opener = require('opener');
 path = require('path');
 sh = require('shelljs');
-
+hb = require('handlebars');
+nodecli = require('shelljs-nodecli');
 
 //  ---
 //  Type Construction
@@ -63,8 +66,10 @@ Cmd.NAME = 'help';
  * The command usage string.
  * @type {string}
  */
-Cmd.prototype.USAGE = 'tibet help [command]';
+Cmd.prototype.USAGE = 'tibet help [command] [--generate] [--missing]';
 
+//  TODO    add way to list topics in man page section (which can include
+//  concepts etc. etc.)
 
 //  ---
 //  Instance Methods
@@ -114,6 +119,19 @@ Cmd.prototype.execute = function() {
             return this.executeForCommand(command);
         }
     }
+
+    //  If the missing flag is provided do a check for any commands which don't
+    //  appear to have matching markdown help files.
+    if (this.options.missing) {
+        return this.executeMissingCheck();
+    }
+
+    //  Optionally we can be asked to generate documentation for the current
+    //  context (lib or project).
+    if (this.options.generate) {
+        return this.executeGenerate();
+    }
+
 
     this.info('\nUsage: tibet <command> <options>');
 
@@ -260,19 +278,378 @@ Cmd.prototype.executeForCommand = function(command) {
         env[key] = process.env[key];
     });
 
-    //  Point the environment to our library man location.
+    //  Set up the man path. The trick here is that if someone overrides a
+    //  command in their project we want to show that first, so ensure we add
+    //  project to the front if we add it.
     manpath = path.join(CLI.expandPath('~lib'), 'doc', 'man');
     env.MANPATH = manpath;
+
+    if (CLI.inProject()) {
+        manpath = path.join(CLI.expandPath('~'), 'doc', 'man');
+        //  NOTE the addition on the front of the path here so project docs are
+        //  first in the lookup order.
+        env.MANPATH = manpath + ':' + env.MANPATH;
+    }
 
     config = {
         env: env,
         stdio: 'inherit',
         stderr: 'inherit'
     };
+
     proc = require('child_process');
-    child = proc.spawn('man', ['tibet-' + command], config);
+    child = proc.spawn('man', [
+        CLI.getProjectName().toLowerCase() + '-' + command, 'tibet-' + command],
+        config);
     child.on('close', function() {
         return 0;
+    });
+
+    return 0;
+};
+
+
+/**
+ */
+Cmd.prototype.executeGenerate = function() {
+    var thisref,
+        content,
+        footer,
+        genHtml,
+        genMan,
+        header,
+        htmlpath,
+        index,
+        indexbody,
+        indexpath,
+        list,
+        roottopic,
+        options,
+        splitter,
+        manpath,
+        rootpath,
+        srcpath,
+        version,
+        year;
+
+    thisref = this;
+
+    //  Need to be at project root for nodecli to work properly.
+    process.chdir(CLI.expandPath('~'));
+
+    //  TODO:   clean existing doc target dir?
+    //          log a message about cleansing documentation...
+
+    this.log('building documentation...');
+
+    //  ---
+    //  Verify Directories
+    //  ---
+
+    rootpath = path.join(CLI.expandPath('~'), 'doc');
+    srcpath = path.join(rootpath, 'markdown');
+
+    if (!sh.test('-d', srcpath)) {
+        throw new Error('Unable to find doc source directory.');
+    }
+
+    htmlpath = path.join(rootpath, 'html');
+    if (!sh.test('-d', htmlpath)) {
+        sh.mkdir(htmlpath);
+    }
+
+    manpath = path.join(rootpath, 'man');
+    if (!sh.test('-d', manpath)) {
+        sh.mkdir(manpath);
+    }
+
+    //  HTML generation uses common header/footer since output from the
+    //  conversion process doesn't include html/body, just "content".
+    header = sh.cat(path.join(rootpath, 'template', 'header.html'));
+    header = hb.compile(header);
+    footer = sh.cat(path.join(rootpath, 'template', 'footer.html'));
+    footer = hb.compile(footer);
+
+    //  ---
+    //  Helpers
+    //  ---
+
+    genHtml = function(file, params) {
+        var html,
+            destdir,
+            destfile,
+            result,
+            srcfile;
+
+        srcfile = path.join(srcpath, file + '.tmp');
+
+        //  Compute the HTML target file path, removing .md extension.
+        destfile = path.join(htmlpath, file);
+        destfile = destfile.slice(0, destfile.lastIndexOf('.')) + '.html';
+
+        //  Compute target directory value and make sure it exists.
+        destdir = destfile.slice(0, destfile.lastIndexOf('/'));
+        if (!sh.test('-d', destdir)) {
+            sh.mkdir(destdir);
+        }
+
+        result = nodecli.exec('marked-man', '--breaks', '--format html',
+            srcfile, {silent: true});
+
+        if (result.code !== 0) {
+            //  TODO:   oops
+            return;
+        }
+
+        html = header(params) + result.output + footer(params);
+
+        //  One last substitution is to look for variations on 'foo(n)'
+        //  and convert them into links which point to the target page.
+        html = html.replace(/([-_a-zA-Z]+)\((\d+)\)/g,
+        function(match, topic, section) {
+            if (topic === params.topic) {
+                return match;
+            }
+
+            return '<a class="crossref" href="./' + topic + '.' +
+                section + '.html">' + topic + '(' + section + ')' + '</a>';
+        });
+
+        html.to(destfile);
+    };
+
+    genMan = function(file, params) {
+        var man,
+            destdir,
+            destfile,
+            result,
+            srcfile;
+
+        srcfile = path.join(srcpath, file + '.tmp');
+
+        //  Compute the manpage target file path, removing .md extension.
+        destfile = path.join(manpath, 'man' + params.section, file);
+        destfile = destfile.slice(0, destfile.lastIndexOf('.'));
+
+        //  Compute target directory value and make sure it exists.
+        destdir = path.join(manpath, 'man' + params.section);
+        if (!sh.test('-d', destdir)) {
+            sh.mkdir(destdir);
+        }
+
+        result = nodecli.exec('marked-man', '--breaks', '--roff',
+            srcfile, {silent: true});
+
+        if (result.code !== 0) {
+            //  TODO:   oops
+            return;
+        }
+
+        man = result.output;
+        man.to(destfile);
+    };
+
+    //  ---
+    //  Process Files
+    //  ---
+
+    //  File names in markdown directory should be of the form
+    //  topic.section.md so we can extract and splice.
+    splitter = /^(.*)\.(\d)\.md$/;
+
+    //  We splice in year and version for copyright etc. so capture once.
+    year = new Date().getFullYear();
+    version = CLI.inProject() ? CLI.cfg('npm.version') : CLI.cfg('tibet.version');
+
+    //  Create an array we can keep the list of content in.
+    index = [];
+
+    //  Markdown directory should be flat but just in case do a recursive
+    //  listing. We'll filter out directories in the loop.
+    list = sh.ls('-R', srcpath);
+
+    //  Process each file, producing both a man page and HTML document.
+    list.forEach(function(file) {
+        var parts,
+            section,
+            srcfile,
+            template,
+            tempfile,
+            topic;
+
+        //  Skip directories, just process individual files.
+        srcfile = path.join(srcpath, file);
+        if (sh.test('-d', srcfile)) {
+            return;
+        }
+
+        //  Pull file name apart. Should be topic.section.md.
+        parts = splitter.exec(file);
+        if (!parts) {
+            thisref.warn('Filename ' + file + ' missing topic or section.');
+            return;
+        }
+        topic = parts[1];
+        section = parts[2];
+
+        options = {
+            topic: topic,
+            section: section,
+            version: version,
+            year: year
+        };
+
+        try {
+            tempfile = srcfile + '.tmp';
+            content = sh.cat(srcfile);
+            template = hb.compile(content);
+            content = template(options);
+            content.to(tempfile);
+
+            //  NOTE this depends on first line being the # {{topic}} line.
+            options.firstline = content.split('\n')[0];
+            index.push(JSON.parse(JSON.stringify(options)));
+
+            genMan(file, options);
+            genHtml(file, options);
+        } catch (e) {
+            thisref.error('Error processing ' + file + ': ' + e.message);
+        } finally {
+            sh.rm('-f', tempfile);
+        }
+    });
+
+    //  ---
+    //  index.html
+    //  ---
+
+    indexpath = path.join(htmlpath, 'index.html');
+
+    roottopic = CLI.inProject() ? CLI.getProjectName() : 'TIBET';
+    options = {
+        topic: roottopic,
+        section: 1,
+        version: version,
+        year: year
+    };
+
+    //  Sort alphabetically within sections.
+    index.sort(function(paramA, paramB) {
+        if (paramA.section < paramB.section) {
+            return -1;
+        } else if (paramA.section > paramB.section) {
+            return 1;
+        } else {
+            if (paramA.topic < paramB.topic) {
+                return -1;
+            } else if (paramA.topic > paramB.topic) {
+                return 1;
+            } else {
+                return 0;
+            }
+        }
+    });
+
+    //  Convert param form into markup. We use a DL to wrap below so we want
+    //  each item returned here to be a dt/dd pair with a link to topic.
+    indexbody = index.map(function(params) {
+        var parts,
+            str;
+
+        parts = params.firstline.split('--');
+        str = '<dt><a class="toc" href="./' +
+            params.topic + '.' + params.section + '.html">' +
+            parts[0] + '</a></dt><dd>-- ' + parts[1] + '</dd>';
+
+        return str;
+    });
+
+    //  Assemble the final index.html page content by using the same
+    //  header/footer as all other pages and our indexbody for content.
+    (header(options) +
+     '<dl class="toc">\n' +
+     indexbody.join('<br/>') +
+     '</dl>\n' +
+     footer(options)).to(
+        indexpath);
+
+    //  ---
+    //  manpage index
+    //  ---
+
+    //  TODO
+
+    return;
+};
+
+/**
+ * Checks the command list against the markdown directory and produces a list of
+ * commands which don't appear to have help defined.
+ */
+Cmd.prototype.executeMissingCheck = function() {
+    var thisref,
+        cmds,
+        mans,
+        missing;
+
+    thisref = this;
+
+    //  ---
+    //  Built-ins
+    //  ---
+
+    mans = sh.ls(path.join(CLI.expandPath('~lib'), 'doc/markdown')).map(function(name) {
+        return name.slice(6, -5);
+    });
+
+    cmds = sh.ls(__dirname).filter(function(name) {
+        return name.charAt(0) !== '_' && name.slice(-3) === '.js';
+    }).map(function(name) {
+        return name.slice(0, -3);
+    });
+
+    missing = cmds.filter(function(name) {
+        return mans.indexOf(name) === -1;
+    });
+
+    missing.forEach(function(name) {
+        thisref.info(name + ' (built-in command)');
+    });
+
+    //  ---
+    //  Custom commands
+    //  ---
+
+    mans = sh.ls(path.join(CLI.expandPath('~'), 'doc/markdown')).map(function(name) {
+        return name.slice(6, -5);
+    });
+
+    cmds = this.getCommands(CLI.expandPath('~app_cmd')).filter(function(name) {
+        return name.charAt(0) !== '_';
+    });
+
+    missing = cmds.filter(function(name) {
+        return mans.indexOf(name) === -1;
+    });
+
+    missing.forEach(function(name) {
+        thisref.info(name + ' (project command)');
+    });
+
+    //  ---
+    //  Makefile targets
+    //  ---
+
+    cmds = this.getMakeTargets().filter(function(name) {
+        return name.charAt(0) !== '_';
+    });
+
+    missing = cmds.filter(function(name) {
+        return mans.indexOf(name) === -1;
+    });
+
+    missing.forEach(function(name) {
+        thisref.info(name + ' (make target)');
     });
 
     return 0;
