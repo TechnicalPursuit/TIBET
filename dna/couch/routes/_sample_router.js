7c0e2955ce60951bbf40042a9f7b066872a1d3f7
idearat <ss@technicalpursuit.com>
2017-02-03 12:19:39 -0700

    Fixup comments and curl commands in sample routes.

--- dna/couch/routes/_sample_router.js
+++ dna/couch/routes/_sample_router.js
@@ -1,9 +1,9 @@
 /**
  * @overview Sample route returning an Express 'Router' which handles one or
  *     more paths below the root defined by this file's name. For example, if
- *     you rename this file 'router' and retain the post to /forminput you
- *     will be able to POST to /router/forminput. To support responding to
- *     /router simply add a get/post registration for '/' in this file.
+ *     you rename this file 'sample' and retain the post to /forminput you
+ *     will be able to POST to /sample/forminput. To support responding to
+ *     /sample simply add a get/post registration for '/' in this file.
  */
 
 (function(root) {
@@ -26,7 +26,7 @@
         router = require('express').Router();
 
         //  Meta information used by logger to identify component etc.
-        meta = {type: 'route', name: 'router'};
+        meta = {type: 'route', name: '{{filename}}'};
 
         //  Announce the loading/config of this route.
         logger.system(
@@ -38,9 +38,15 @@
         //  ---
 
         /*
-         * test via:
-         * curl -XPOST http://127.0.0.1:1407/{{filename}}/forminput --data "@../mocks/mockjson_post.json"
-         * curl -XPOST -k https://127.0.0.1:1443/{{filename}}/forminput --data "@../mocks/mockjson_post.json"
+         test via curl using variations (adjust port etc) of:
+
+            curl -XPOST http://127.0.0.1:1407/{{filename}}/forminput \
+                --header "Content-Type: application/json" \
+                --data "@../mocks/mockjson_post.json"
+
+            curl -XPOST http://127.0.0.1:1407/{{filename}}/forminput \
+                --header "Content-Type: application/json" \
+                --data "@../mocks/mockjson_post.json"
          */
         router.post('/forminput', function(req, res) {
 
