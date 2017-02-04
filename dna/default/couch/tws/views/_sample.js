2eea846941dfd698d0c8866fcbcc0ee23ca917e1
idearat <ss@technicalpursuit.com>
2017-02-04 09:23:32 -0700

    Add sample views to the TWS sections of the server DNA.

---
+++ dna/default/couch/tws/views/_sample.js
@@ -0,0 +1,16 @@
+/* global emit */
+(function() {
+    'use strict';
+
+    module.exports = {
+        map: function(doc) {
+            if (doc.type) {
+                emit(doc.type, 1);
+            }
+        },
+        reduce: function(keys, values, rereduce) {
+            return sum(values);
+        }
+    };
+
+}());
