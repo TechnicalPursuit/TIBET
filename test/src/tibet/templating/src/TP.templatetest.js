25b6af5cad3416e6e24f1a93903d253ac302207e
William J. Edney <bedney@technicalpursuit.com>
2017-02-13 15:06:48 -0600

    Remove irrelevant comments

--- test/src/tibet/templating/src/TP.templatetest.js
+++ test/src/tibet/templating/src/TP.templatetest.js
@@ -3,13 +3,7 @@
  * @summary Defines namespace-level objects and functionality for the project.
  */
 
-/**
- * Define the JavaScript namespace object which will hold application code.
- */
 TP.defineNamespace('TP.templatetest');
 
-/**
- * Define the XML namespace and prefix for any tags in the application.
- */
 TP.w3.Xmlns.registerNSInfo('urn:app:templatetest',
     TP.hc('prefix', 'templatetest'));
