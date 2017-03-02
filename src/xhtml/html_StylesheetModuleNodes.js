f052776e03000c541b8c3bddcfff7f73e8f8308b
William J. Edney <bedney@technicalpursuit.com>
2017-03-02 06:31:01 -0600

    Process any @import’ed hrefs into URI instances
    This allows the system to track CSS URIs better.

--- src/xhtml/html_StylesheetModuleNodes.js
+++ src/xhtml/html_StylesheetModuleNodes.js
@@ -78,13 +78,34 @@ function(aRequest) {
         return this.raise('TP.sig.InvalidNode');
     }
 
-    //  Register a handler function that will dispatch a TP.sig.DOMReady when
-    //  the stylesheet has finished loading.
+    //  Register a handler function that will process @import'ed URIs and
+    //  dispatch a TP.sig.DOMReady when the stylesheet has finished loading.
     handlerFunc =
         function() {
 
+            var stylesheet,
+                importHrefs;
+
             //  Remove this handler to avoid memory leaks.
             elem.removeEventListener('load', handlerFunc, false);
+
+            //  Grab the stylesheet from the element.
+            stylesheet = TP.cssElementGetStyleSheet(elem);
+
+            //  Intern the stylesheet's href as a TP.core.URI. Note that we
+            //  don't care about the return value here - we're simply interested
+            //  in having a URI object matching the href here.
+            TP.uc(stylesheet.href);
+
+            //  Grab any hrefs from @import statements in the stylesheet and
+            //  create instances of TP.core.URIs from them as well. Again, we're
+            //  not interested in the return values here. Note that this method,
+            //  by default, will recursively retrieve @import hrefs.
+            importHrefs = TP.styleSheetGetImportHrefs(stylesheet);
+            importHrefs.forEach(
+                function(anHref) {
+                    TP.uc(anHref);
+                });
 
             //  If the element doesn't have a 'dependsOn' attribute (and,
             //  therefore, doesn't have other resources (usually @imports that
