f5404027f6de3f95c590f0a305ec82de5d10b9cf
William J. Edney <bedney@technicalpursuit.com>
2017-04-25 11:30:34 -0500

    Added capability to refresh selection model by indices

--- src/xctrls/TP.xctrls.list/TP.xctrls.list.js
+++ src/xctrls/TP.xctrls.list/TP.xctrls.list.js
@@ -199,6 +199,7 @@ function(anAspect) {
      *     in the UI.
      * @description Note that the aspect can be one of the following:
      *          'value'     ->  The value of the element (the default)
+     *          'index'     ->  The index of the element
      * @param {String} anAspect The property of the elements to use to
      *      determine which elements should be selected.
      * @returns {TP.xctrls.list} The receiver.
@@ -209,7 +210,9 @@ function(anAspect) {
 
         aspect,
 
-        data;
+        data,
+
+        indexes;
 
     //  Grab the selection model.
     selectionModel = this.$getSelectionModel();
@@ -219,11 +222,8 @@ function(anAspect) {
     //  removeSelection method and it means that it needs the whole list of data
     //  (if they're all selected) so that it can individually remove items from
     //  it.
-    selectAll = this.$getSelectionModel().hasKey(TP.ALL);
+    selectAll = selectionModel.hasKey(TP.ALL);
     if (selectAll) {
-
-        //  We default the aspect to 'value'
-        aspect = TP.ifInvalid(anAspect, 'value');
 
         //  Empty the selection model in preparation for rebuilding it with
         //  individual items registered under the 'value' aspect.
@@ -235,9 +235,14 @@ function(anAspect) {
             return this;
         }
 
-        //  Remove any TP.GROUPING or TP.SPACING data rows. This is ok because
-        //  the removeSelection method works on the *values*, not the indices.
-        data = data.select(
+        //  We default the aspect to 'value'
+        aspect = TP.ifInvalid(anAspect, 'value');
+
+        switch (aspect) {
+
+            case 'value':
+                //  Remove any TP.GROUPING or TP.SPACING data rows.
+                data = data.select(
                         function(anItem) {
                             if (TP.regex.GROUPING.test(anItem) ||
                                 TP.regex.SPACING.test(anItem)) {
@@ -246,6 +251,34 @@ function(anAspect) {
 
                             return true;
                         });
+
+                break;
+
+            case 'index':
+
+                indexes = data.getIndices();
+
+                //  Remove any TP.GROUPING or TP.SPACING data rows.
+                indexes = indexes.select(
+                        function(anIndex) {
+                            if (TP.regex.GROUPING.test(data.at(anIndex)) ||
+                                TP.regex.SPACING.test(data.at(anIndex))) {
+                                return false;
+                            }
+
+                            return true;
+                        });
+
+                data = indexes;
+
+                break;
+
+            default:
+
+                //  It was an aspect that we don't know how to process.
+                data = null;
+                break;
+        }
 
         selectionModel.atPut(aspect, data);
     }
