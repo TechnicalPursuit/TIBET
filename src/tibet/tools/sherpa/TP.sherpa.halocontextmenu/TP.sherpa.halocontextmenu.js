307dd02f4f8bcb3677aec444041e08f613657dbf
William J. Edney <bedney@technicalpursuit.com>
2017-05-09 13:59:51 -0500

    Changed TP.sherpa.contextmenu to TP.sherpa.halocontextmenu
    In preparation for multiple context menus. Also, this type is now meant to run inside of an xctrls:popup.

---
+++ src/tibet/tools/sherpa/TP.sherpa.halocontextmenu/TP.sherpa.halocontextmenu.js
@@ -0,0 +1,111 @@
+//  ========================================================================
+/**
+ * @copyright Copyright (C) 1999 Technical Pursuit Inc. (TPI) All Rights
+ *     Reserved. Patents Pending, Technical Pursuit Inc. Licensed under the
+ *     OSI-approved Reciprocal Public License (RPL) Version 1.5. See the RPL
+ *     for your rights and responsibilities. Contact TPI to purchase optional
+ *     privacy waivers if you must keep your TIBET-based source code private.
+ */
+//  ========================================================================
+
+/**
+ * @type {TP.sherpa.halocontextmenu}
+ */
+
+//  ------------------------------------------------------------------------
+
+TP.sherpa.TemplatedTag.defineSubtype('halocontextmenu');
+
+TP.sherpa.halocontextmenu.Inst.defineAttribute('title',
+    TP.cpc('> .header > .title', TP.hc('shouldCollapse', true)));
+
+//  ------------------------------------------------------------------------
+//  Instance Attributes
+//  ------------------------------------------------------------------------
+
+TP.sherpa.halocontextmenu.Inst.defineAttribute('menuContent',
+    TP.cpc('> .content', TP.hc('shouldCollapse', true)));
+
+//  ------------------------------------------------------------------------
+//  Instance Methods
+//  ------------------------------------------------------------------------
+
+TP.sherpa.halocontextmenu.Inst.defineHandler('SelectMenuItem',
+function(aSignal) {
+
+    var cmdVal;
+
+    cmdVal = aSignal.getDOMTarget().getAttribute('data-cmd');
+
+    if (TP.isEmpty(cmdVal)) {
+        return this;
+    }
+
+    TP.bySystemId('SherpaConsoleService').sendConsoleRequest(cmdVal);
+
+    return this;
+});
+
+//  ------------------------------------------------------------------------
+
+TP.sherpa.halocontextmenu.Inst.defineMethod('setAttrHidden',
+function(beHidden) {
+
+    /**
+     * @method setAttrHidden
+     * @summary The setter for the receiver's hidden state.
+     * @param {Boolean} beHidden Whether or not the receiver is in a hidden
+     *     state.
+     * @returns {Boolean} Whether the receiver's state is hidden.
+     */
+
+    var wasHidden;
+
+    wasHidden = TP.bc(this.getAttribute('hidden'));
+
+    if (wasHidden === beHidden) {
+        return this;
+    }
+
+    if (!beHidden) {
+        this.render();
+    }
+
+    return this.callNextMethod();
+});
+
+//  ------------------------------------------------------------------------
+
+TP.sherpa.halocontextmenu.Inst.defineMethod('render',
+function() {
+
+    /**
+     * @method render
+     * @summary
+     * @returns
+     */
+
+    var haloTarget,
+        theContent,
+
+        menuContentTPElem;
+
+    haloTarget = TP.byId('SherpaHalo', this.getNativeDocument()).get(
+                                                    'currentTargetTPElem');
+
+    if (TP.isValid(haloTarget)) {
+
+        theContent = haloTarget.getContentForTool('contextMenu');
+        menuContentTPElem = this.get('menuContent').setContent(theContent);
+        menuContentTPElem.awaken();
+    }
+
+    //  Signal to observers that this control has rendered.
+    this.signal('TP.sig.DidRender');
+
+    return this;
+});
+
+//  ------------------------------------------------------------------------
+//  end
+//  ========================================================================
