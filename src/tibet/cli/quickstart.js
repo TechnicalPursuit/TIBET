05833b91e0230279ff107ed56168f6bae3e2f767
idearat <ss@technicalpursuit.com>
2017-07-08 10:19:00 -0600

    Bring quickstart output up to date.

--- src/tibet/cli/quickstart.js
+++ src/tibet/cli/quickstart.js
@@ -87,8 +87,9 @@ Cmd.prototype.execute = function() {
 'name so it should be a valid directory name on your platform.\n\n' +
 'Type \'tibet clone {appname}\', replacing {appname} with your project name:\n\n' +
 
-'    $ tibet clone test\n' +
-'    TIBET dna \'default\' cloned to test as app \'test\'.\n\n' +
+'    $ tibet clone hello\n' +
+'    ...\n' +
+'    Application DNA \'default\' cloned to ./hello as \'hello\'.\n\n' +
 
 'INITIALIZE THE PROJECT\n\n' +
 
@@ -96,22 +97,22 @@ Cmd.prototype.execute = function() {
 'dependencies specific to the template you cloned (we used the default here).\n' +
 'Navigate to your project and then type \'tibet init\' to initialize it:\n\n' +
 
-'    $ cd test\n' +
+'    $ cd hello\n' +
 '    $ tibet init\n' +
-'    Initializing new default project...\n' +
-'    installing project dependencies via `npm install`.\n' +
-'    Project initialized successfully.\n\n' +
+'    ...\n' +
+'    project initialized successfully.\n\n' +
 
+'START THE SERVER\n\n' +
 
-'START THE DEMO SERVER\n\n' +
-
-'The \'default\' template used by clone includes a simple Node.js-based HTTP\n' +
-'server we call the TIBET Data Server or TDS. By default the TDS will use\n' +
-'port 1407 so assuming that port isn\'t busy on your system you can start the\n' +
-'server using \'tibet start\' without any parameters:\n\n' +
+'The \'default\' template used by clone includes a Node.js-based HTTP server\n' +
+'we call the TIBET Data Server or TDS. By default the TDS will use port 1407\n' +
+'so assuming that port isn\'t busy on your system you can start the server\n' +
+'using \'tibet start\' without any parameters:\n\n' +
 
 '    $ tibet start\n' +
-'    Starting server at http://127.0.0.1:1407/index.html\n\n' +
+'    ...\n' +
+'    1498043782937 [7] TDS hello 0.1.0 @ http://127.0.0.1:1407#?boot.config=developer (sherpa)\n\n' +
+
 'Congratulations! Your new TIBET project is running. Open the web address\n' +
 'for your project in an HTML5 browser and you should see text directing you\n' +
 'on how to take the next step in your TIBET journey.\n\n' +
