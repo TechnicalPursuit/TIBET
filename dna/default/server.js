6e5e879b0e83f1b59e3fdc39818806061a8d4f4d
idearat <ss@technicalpursuit.com>
2017-07-12 13:23:01 -0600

    Update dna to use require root to access TDS.

--- dna/default/server.js
+++ dna/default/server.js
@@ -56,7 +56,7 @@
     //  Always bring in the baseline TDS, even if we don't load the 'tds' plugin
     //  (which loads any tds.plugins.tds list which might be defined). This
     //  gives access to utilities like getcfg etc.
-    TDS = require('tibet/tds/tds_base');
+    TDS = require('tibet').TDS;
 
     //  ---
     //  APP/TDS Config
