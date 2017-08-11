370ae3a120c317aa994c7bac2228d3a043edf4b4
William J. Edney <bedney@technicalpursuit.com>
2017-08-11 07:54:47 -0500

    Lint

--- src/tibet/tools/sherpa/TP.sherpa.workbench/TP.sherpa.workbench.js
+++ src/tibet/tools/sherpa/TP.sherpa.workbench/TP.sherpa.workbench.js
@@ -539,7 +539,7 @@ function(aSignal) {
     //  Grab the profile name and slice off everything after the '#'. That will
     //  be what we default the 'ShipIt environment name'.
     profileName = TP.sys.getcfg('boot.profile');
-    profileName = profileName.slice(0, profileName.indexOf('#'))
+    profileName = profileName.slice(0, profileName.indexOf('#'));
 
     TP.prompt('If you\'re using ShipIt to deploy, enter environment name.' +
                     ' Otherwise, this should be blank.',
