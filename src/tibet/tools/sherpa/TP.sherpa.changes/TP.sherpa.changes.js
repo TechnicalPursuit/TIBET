3da0666bcb4f45b49011e95f6e953459a1f6324d
William J. Edney <bedney@technicalpursuit.com>
2017-08-17 07:30:00 -0500

    Added the core of the visuals to the sherpa:changes panel
    The current visuals work - need to add logic to push/pull

--- src/tibet/tools/sherpa/TP.sherpa.changes/TP.sherpa.changes.js
+++ src/tibet/tools/sherpa/TP.sherpa.changes/TP.sherpa.changes.js
@@ -17,6 +17,165 @@
 TP.sherpa.TemplatedTag.defineSubtype('changes');
 
 //  ------------------------------------------------------------------------
+//  Type Methods
+//  ------------------------------------------------------------------------
+
+TP.sherpa.changes.Type.defineMethod('tagAttachDOM',
+function(aRequest) {
+
+    /**
+     * @method tagAttachDOM
+     * @summary Sets up runtime machinery for the element in aRequest
+     * @param {TP.sig.Request} aRequest A request containing processing
+     *     parameters and other data.
+     */
+
+    var elem,
+        tpElem;
+
+    //  this makes sure we maintain parent processing
+    this.callNextMethod();
+
+    //  Make sure that we have an Element to work from
+    if (!TP.isElement(elem = aRequest.at('node'))) {
+        //  TODO: Raise an exception.
+        return;
+    }
+
+    tpElem = TP.wrap(elem);
+    tpElem.setup();
+
+    return;
+});
+
+//  ------------------------------------------------------------------------
+
+TP.sherpa.changes.Type.defineMethod('tagAttachComplete',
+function(aRequest) {
+
+    /**
+     * @method tagAttachComplete
+     * @summary Executes once the tag has been fully processed and its
+     *     attachment phases are fully complete.
+     * @description Because tibet:data tag content drives binds and we need to
+     *     notify even without a full page load, we notify from here once the
+     *     attachment is complete (instead of during tagAttachData).
+     * @param {TP.sig.Request} aRequest A request containing processing
+     *     parameters and other data.
+     */
+
+    var dataURI;
+
+    //  this makes sure we maintain parent processing
+    this.callNextMethod();
+
+    //  set the server-side URIs list to an empty Array so that it draw an
+    //  'empty full row' list.
+    dataURI = TP.uc('urn:tibet:changedServerURIs');
+    dataURI.setResource(TP.ac());
+
+    //  set the client-side URIs list to an empty Array so that it draw an
+    //  'empty full row' list.
+    dataURI = TP.uc('urn:tibet:changedClientURIs');
+    dataURI.setResource(TP.ac());
+
+    return;
+});
+
+//  ------------------------------------------------------------------------
+//  Instance Attributes
+//  ------------------------------------------------------------------------
+
+TP.sherpa.changes.Inst.defineAttribute('countWithName',
+    TP.cpc('> .header > sherpa|count[name="{{0}}"]',
+                            TP.hc('shouldCollapse', true)));
+
+TP.sherpa.changes.Inst.defineAttribute('serverCount',
+    TP.cpc('> .header > sherpa|count[name="server"]',
+                            TP.hc('shouldCollapse', true)));
+
+TP.sherpa.changes.Inst.defineAttribute('clientCount',
+    TP.cpc('> .header > sherpa|count[name="client"]',
+                            TP.hc('shouldCollapse', true)));
+
+TP.sherpa.changes.Inst.defineAttribute('lists',
+    TP.cpc('> .content xctrls|list',
+                            TP.hc('shouldCollapse', false)));
+
+//  ------------------------------------------------------------------------
+//  Instance Methods
+//  ------------------------------------------------------------------------
+
+TP.sherpa.changes.Inst.defineHandler('ClosedChange',
+function(aSignal) {
+
+    /**
+     * @method handleClosedChange
+     * @summary Handles when the HUD's 'closed' state changes.
+     * @param {TP.sig.ClosedChange} aSignal The TIBET signal which triggered
+     *     this method.
+     * @returns {TP.sherpa.changes} The receiver.
+     */
+
+    var hud,
+        hudIsHidden;
+
+    hud = TP.byId('SherpaHUD', this.getNativeWindow());
+
+    hudIsHidden = TP.bc(hud.getAttribute('closed'));
+
+    if (!hudIsHidden) {
+        this.observe(TP.ANY, TP.ac('DirtyChange', 'RemoteResourceDirty'));
+    } else {
+        this.ignore(TP.ANY, TP.ac('DirtyChange', 'RemoteResourceDirty'));
+    }
+
+    return this;
+}, {
+    origin: 'SherpaHUD'
+});
+
+//  ------------------------------------------------------------------------
+
+TP.sherpa.changes.Inst.defineHandler('DirtyChange',
+function(aSignal) {
+
+    /**
+     * @method handleDirtyChange
+     * @summary Handles when any URI's client-side 'dirty' state changes.
+     * @param {TP.sig.DirtyChange} aSignal The TIBET signal which triggered
+     *     this method.
+     * @returns {TP.sherpa.changes} The receiver.
+     */
+
+    if (TP.isKindOf(aSignal.getOrigin(), TP.core.URL)) {
+        this.updateURIInfo();
+    }
+
+    return this;
+});
+
+//  ------------------------------------------------------------------------
+
+TP.sherpa.changes.Inst.defineHandler('RemoteResourceDirty',
+function(aSignal) {
+
+    /**
+     * @method handleResourceDirty
+     * @summary Handles when any URI's server-side 'dirty' state changes.
+     * @param {TP.sig.RemoteResourceDirty} aSignal The TIBET signal which
+     *     triggered this method.
+     * @returns {TP.sherpa.changes} The receiver.
+     */
+
+    if (TP.isKindOf(aSignal.getOrigin(), TP.core.URL)) {
+        this.updateURIInfo();
+    }
+
+    return this;
+});
+
+//  ------------------------------------------------------------------------
 
 TP.sherpa.changes.Inst.defineHandler('ShowClientChanges',
 function(aSignal) {
@@ -30,8 +189,7 @@ function(aSignal) {
      * @returns {TP.sherpa.changes} The receiver.
      */
 
-    TP.uc('urn:tibet:current_changes_tab#tibet(selection)').setResource(
-                                                                    'client');
+    this.setSelectedPanel('client');
 
     return this;
 });
@@ -50,8 +208,208 @@ function(aSignal) {
      * @returns {TP.sherpa.changes} The receiver.
      */
 
-    TP.uc('urn:tibet:current_changes_tab#tibet(selection)').setResource(
-                                                                    'server');
+    this.setSelectedPanel('server');
+
+    return this;
+});
+
+//  ------------------------------------------------------------------------
+
+TP.sherpa.changes.Inst.defineHandler('UISelect',
+function(aSignal) {
+
+    /**
+     * @method handleUISelect
+     * @summary Handles notifications of when the user has selected to switch to
+     *     the currently non-selected panel of URIs that have changed.
+     * @param {TP.sig.UISelect} aSignal The TIBET signal which triggered this
+     *     method.
+     * @returns {TP.sherpa.changes} The receiver.
+     */
+
+    this.get('lists').forEach(
+        function(aList) {
+            aList.render();
+        });
+
+    aSignal.stopPropagation();
+
+    return this;
+});
+
+//  ------------------------------------------------------------------------
+
+TP.sherpa.changes.Inst.defineMethod('setSelectedPanel',
+function(panelName) {
+
+    /**
+     * @method setSelectedPanel
+     * @summary Sets the currently selected panel to the panel with a 'name'
+     *     attribute matching the supplied panel name.
+     * @param {String} panelName The name of the panel to switch to.
+     * @returns {TP.sherpa.changes} The receiver.
+     */
+
+    var selectionURI,
+
+        currentName,
+
+        countTPElem;
+
+    //  Grab the model value for the currently selected tab.
+    selectionURI = TP.uc('urn:tibet:current_changes_tab#tibet(selection)');
+    currentName = selectionURI.getResource().get('result');
+
+    //  If that value is different than the supplied name, then switch.
+    if (currentName !== panelName) {
+
+        //  Grab the current panel and make it not selected.
+        countTPElem = this.get('countWithName', currentName);
+        countTPElem.setAttribute('selected', false);
+
+        //  Grab the desired panel and make it selected.
+        countTPElem = this.get('countWithName', panelName);
+        countTPElem.setAttribute('selected', true);
+
+        //  Update the currently selected tab model value.
+        selectionURI.setResource(panelName);
+    }
+
+    return this;
+});
+
+//  ------------------------------------------------------------------------
+
+TP.sherpa.changes.Inst.defineMethod('setup',
+function() {
+
+    /**
+     * @method setup
+     * @summary Perform the initial setup for the receiver.
+     * @returns {TP.sherpa.changes} The receiver.
+     */
+
+    this.observe(TP.byId('SherpaHUD', this.getNativeWindow()), 'ClosedChange');
+
+    return this;
+});
+
+//  ------------------------------------------------------------------------
+
+TP.sherpa.changes.Inst.defineMethod('updateURIInfo',
+function() {
+
+    /**
+     * @method updateURIInfo
+     * @summary Updates the URI information in the client and server panels.
+     * @returns {TP.sherpa.changes} The receiver.
+     */
+
+    var serverData,
+        serverCountTPElem,
+
+        oldServerCount,
+        newServerCount,
+        serverURI,
+
+        clientData,
+        clientCountTPElem,
+
+        oldClientCount,
+        newClientCount,
+        clientURI,
+
+        countPulseFinishedFunc;
+
+    //  Grab the server data by getting the keys of TP.core.URI's 'remote change
+    //  list'. These will be the URI locations.
+    serverData = TP.core.URI.get('remoteChangeList').getKeys();
+
+    //  Virtualize these URIs to match the format in the client panel.
+    serverData = serverData.collect(
+                    function(aLocation) {
+                        return TP.uriInTIBETFormat(aLocation);
+                    });
+
+    //  Set the data of the server URIs model to the data we computed. This will
+    //  cause dependents (like the list) to update.
+    serverURI = TP.uc('urn:tibet:changedServerURIs');
+    serverURI.setResource(serverData, TP.hc('signalChange', true));
+
+    //  Update the count for the server side, if required.
+    serverCountTPElem = this.get('serverCount');
+
+    oldServerCount = serverCountTPElem.getTextContent().asNumber();
+    newServerCount = TP.notEmpty(serverData) ? serverData.getSize() : 0;
+
+    //  If the old and new server counts don't match, then update the server
+    //  count.
+    if (oldServerCount !== newServerCount) {
+
+        //  Set the text of the server count to the new count and add a class
+        //  that will cause the count to animate.
+        serverCountTPElem.setTextContent(newServerCount);
+        serverCountTPElem.addClass('contentrefreshed');
+
+        //  Set up a signal handler that waits for the animation to finish. When
+        //  it does, then remove the class that caused the animation.
+        (countPulseFinishedFunc = function(aSignal) {
+
+            countPulseFinishedFunc.ignore(
+                serverCountTPElem, 'TP.sig.DOMAnimationEnd');
+
+            serverCountTPElem.removeClass('contentrefreshed');
+        }).observe(serverCountTPElem, 'TP.sig.DOMAnimationEnd');
+    }
+
+    //  ---
+
+    //  Grab the lcient data by getting TP.core.URI's 'local change list'. These
+    //  will be hash of the URI locations as keys and the URIs as values.
+    clientData = TP.core.URI.getLocalChangeList();
+
+    //  Select out only URIs that are really URLs.
+    clientData = clientData.select(
+                    function(kvPair) {
+                        return TP.isKindOf(kvPair.last(), TP.core.URL);
+                    });
+
+    //  Collect out the URL locations.
+    clientData = clientData.collect(
+                    function(kvPair) {
+                        return kvPair.first();
+                    });
+
+    //  Set the data of the client URIs model to the data we computed. This will
+    //  cause dependents (like the list) to update.
+    clientURI = TP.uc('urn:tibet:changedClientURIs');
+    clientURI.setResource(clientData, TP.hc('signalChange', true));
+
+    //  Update the count for the client side, if required.
+    clientCountTPElem = this.get('clientCount');
+
+    oldClientCount = clientCountTPElem.getTextContent().asNumber();
+    newClientCount = TP.notEmpty(clientData) ? clientData.getSize() : 0;
+
+    //  If the old and new client counts don't match, then update the client
+    //  count.
+    if (oldClientCount !== newClientCount) {
+
+        //  Set the text of the client count to the new count and add a class
+        //  that will cause the count to animate.
+        clientCountTPElem.setTextContent(newClientCount);
+        clientCountTPElem.addClass('contentrefreshed');
+
+        //  Set up a signal handler that waits for the animation to finish. When
+        //  it does, then remove the class that caused the animation.
+        (countPulseFinishedFunc = function(aSignal) {
+
+            countPulseFinishedFunc.ignore(
+                clientCountTPElem, 'TP.sig.DOMAnimationEnd');
+
+            clientCountTPElem.removeClass('contentrefreshed');
+        }).observe(clientCountTPElem, 'TP.sig.DOMAnimationEnd');
+    }
 
     return this;
 });
