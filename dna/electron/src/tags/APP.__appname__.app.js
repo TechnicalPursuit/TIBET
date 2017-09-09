6d0d274a5d721eafd019abd3f4c3163fc2368ad2
idearat <ss@technicalpursuit.com>
2017-09-09 13:33:46 -0600

    Update all references to former wiki-hosted docs.

--- dna/electron/src/tags/APP.__appname__.app.js
+++ dna/electron/src/tags/APP.__appname__.app.js
@@ -10,5 +10,5 @@ TP.core.TemplatedTag.defineSubtype('APP.{{appname}}:app');
 /*
  * For information on how to expand the functionality in this type visit:
  *
- * https://github.com/TechnicalPursuit/TIBET/wiki/TIBET-Getting-Started
+ * https://www.technicalpursuit.com/docs/index.xhtml
  */
