df0ea2d45156e57f098b06035a6930027836c1f6
William J. Edney <bedney@technicalpursuit.com>
2017-09-17 21:36:48 -0500

    Adjust tag DNA to define the tag’s theme URI to have TP.NO_RESULT

--- src/tibet/cli/dna/type/templatedtag/__nsroot__.__nsname__.__typename__.js
+++ src/tibet/cli/dna/type/templatedtag/__nsroot__.__nsname__.__typename__.js
@@ -7,6 +7,11 @@
 
 {{supertype}}.defineSubtype('{{nsroot}}.{{nsname}}:{{typename}}');
 
+//  This tag is not a themed tag. By default, since the body element has a
+//  'data-theme' attribute, we have to explicity say so.
+//  Note how this property is TYPE_LOCAL, by design.
+{{nsroot}}.{{nsname}}.{{typename}}.defineAttribute('themeURI', TP.NO_RESULT);
+
 //  ------------------------------------------------------------------------
 //  end
 //  ========================================================================
