c186255165e046d1ae38cae1e477d4ba25f987b8
William J. Edney <bedney@technicalpursuit.com>
2017-10-04 02:55:07 -0500

    Put numeric count in its own span and manage it like the content for the whole control

--- src/tibet/tools/sherpa/TP.sherpa.count/TP.sherpa.count.js
+++ src/tibet/tools/sherpa/TP.sherpa.count/TP.sherpa.count.js
@@ -17,5 +17,55 @@
 TP.sherpa.Element.defineSubtype('count');
 
 //  ------------------------------------------------------------------------
+//  Instance Attributes
+//  ------------------------------------------------------------------------
+
+TP.sherpa.count.Inst.defineAttribute('countNumber',
+    TP.cpc('> .value', TP.hc('shouldCollapse', true)));
+
+//  ------------------------------------------------------------------------
+//  Instance Methods
+//  ------------------------------------------------------------------------
+
+TP.sherpa.count.Inst.defineMethod('getContent',
+function(aRequest) {
+
+    /**
+     * @method getContent
+     * @summary Returns the receiver's content.
+     * @param {TP.sig.Request|TP.core.Hash} aRequest Optional control
+     *     parameters.
+     * @returns {String} The text content of the native node.
+     */
+
+    return this.get('countNumber').getContent(aRequest);
+});
+
+//  ------------------------------------------------------------------------
+
+TP.sherpa.count.Inst.defineMethod('setContent',
+function(newContent, aRequest, stdinContent) {
+
+    /**
+     * @method setContent
+     * @summary Sets the content of the receiver's native DOM counterpart to
+     *     the content supplied.
+     * @param {Object} newContent The content to write into the receiver. This
+     *     can be a String, a Node, or an Object capable of being converted into
+     *     one of those forms.
+     * @param {TP.sig.Request} aRequest An optional request object which defines
+     *     further parameters.
+     * @param {Object} stdinContent Content to set as the 'stdin' when executing
+     *     the supplied content. Note that if this parameter is supplied, the
+     *     content is 'executed', as well as processed, by the shell.
+     * @returns {TP.core.Node} The result of setting the content of the
+     *     receiver.
+     */
+
+    return this.get('countNumber').setContent(
+                                    newContent, aRequest, stdinContent);
+});
+
+//  ------------------------------------------------------------------------
 //  end
 //  ========================================================================
