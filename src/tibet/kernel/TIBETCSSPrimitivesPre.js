2e5b3f649c2d4cea09f67b822559e2bfd2a05284
William J. Edney <bedney@technicalpursuit.com>
2017-10-20 08:34:02 -0500

    Added method to calculate CSS selector specificity

--- src/tibet/kernel/TIBETCSSPrimitivesPre.js
+++ src/tibet/kernel/TIBETCSSPrimitivesPre.js
@@ -226,6 +226,243 @@ function(aNode) {
 
 //  ------------------------------------------------------------------------
 
+TP.definePrimitive('calculateSingleCSSSelectorSpecificity',
+function(singleSelectorText) {
+
+    /**
+     * @method calculateSingleSelectorSpecificity
+     * @summary Calculates the specificity of CSS selectors per the algorithms
+     *     at: http://www.w3.org/TR/css3-selectors/#specificity
+     * @description This code is a TIBETized version of:
+     *     https://github.com/keeganstreet/specificity
+     *     The MIT License (MIT)
+     *     Copyright (c) 2016 Keegan Street and others
+     * @param {String} singleSelectorText The 'single selector' to compute the
+     *     specificity for. Note that this means that the selector shouldn't be
+     *     a comma-separated (',') selector.
+     * @returns {Object} Returns an object with the following properties:
+     *     selector: the input
+     *     specificity: e.g. 0,1,0,0
+     *     parts: array with details about each part of the selector that counts
+     *     towards the specificity
+     *     specificityArray: e.g. [0, 1, 0, 0]
+     */
+
+    var selector,
+        findMatch,
+        typeCount,
+        parts,
+        attributeRegex,
+        idRegex,
+        classRegex,
+        pseudoElementRegex,
+        pseudoClassWithBracketsRegex,
+        pseudoClassRegex,
+        elementRegex;
+
+    selector = singleSelectorText;
+
+    typeCount = {
+        a: 0,
+        b: 0,
+        c: 0
+    };
+
+    parts = TP.ac();
+
+    /* eslint-disable no-useless-escape */
+
+    //  The following regular expressions assume that selectors matching the
+    //  preceding regular expressions have been removed.
+    attributeRegex = /(\[[^\]]+\])/g;
+    idRegex = /(#[^\s\+>~\.\[:]+)/g;
+    classRegex = /(\.[^\s\+>~\.\[:]+)/g;
+    pseudoElementRegex =
+        /(::[^\s\+>~\.\[:]+|:first-line|:first-letter|:before|:after)/gi;
+
+    //  A regex for pseudo classes with brackets - :nth-child(),
+    //  :nth-last-child(), :nth-of-type(), :nth-last-type(), :lang()
+    pseudoClassWithBracketsRegex = /(:[\w-]+\([^\)]*\))/gi;
+
+    //  A regex for other pseudo classes, which don't have brackets
+    pseudoClassRegex = /(:[^\s\+>~\.\[:]+)/g;
+
+    elementRegex = /([^\s\+>~\.\[:]+)/g;
+
+    //  Find matches for a regular expression in a string and push their
+    //  details to parts
+    //  Type is "a" for IDs, "b" for classes, attributes and pseudo-classes
+    //  and "c" for elements and pseudo-elements
+    findMatch = function(regex, type) {
+
+        var matches,
+            i,
+            len,
+            match,
+            index,
+            length;
+
+        if (regex.test(selector)) {
+
+            matches = selector.match(regex);
+
+            len = matches.getSize();
+
+            for (i = 0; i < len; i++) {
+
+                typeCount[type] += 1;
+                match = matches[i];
+                index = selector.indexOf(match);
+                length = match.length;
+
+                parts.push(
+                    {
+                        selector: singleSelectorText.substr(index, length),
+                        type: type,
+                        index: index,
+                        length: length
+                    });
+
+                //  Replace this simple selector with whitespace so it won't
+                //  be counted in further simple selectors
+                selector = selector.replace(
+                                match, Array(length + 1).join(' '));
+            }
+        }
+    };
+
+    //  Replace escaped characters with plain text, using the "A" character
+    //  https://www.w3.org/TR/CSS21/syndata.html#characters
+    (function() {
+        var replaceWithPlainText,
+            escapeHexadecimalRegex,
+            escapeHexadecimalRegex2,
+            escapeSpecialCharacter;
+
+        replaceWithPlainText = function(regex) {
+            var matches,
+                i,
+                len,
+                match;
+
+            if (regex.test(selector)) {
+                matches = selector.match(regex);
+                for (i = 0, len = matches.length; i < len; i += 1) {
+                    match = matches[i];
+                    selector = selector.replace(
+                                match, Array(match.length + 1).join('A'));
+                }
+            }
+        };
+
+        //  Matches a backslash followed by six hexadecimal digits
+        //  followed by an optional single whitespace character
+        escapeHexadecimalRegex = /\\[0-9A-Fa-f]{6}\s?/g;
+
+        //  Matches a backslash followed by fewer than six hexadecimal
+        //  digits followed by a mandatory single whitespace character
+        escapeHexadecimalRegex2 = /\\[0-9A-Fa-f]{1,5}\s/g;
+
+        //  Matches a backslash followed by any character
+        escapeSpecialCharacter = /\\./g;
+
+        replaceWithPlainText(escapeHexadecimalRegex);
+        replaceWithPlainText(escapeHexadecimalRegex2);
+        replaceWithPlainText(escapeSpecialCharacter);
+    }());
+
+    //  Remove the negation psuedo-class (:not) but leave its argument
+    //  because specificity is calculated on its argument
+    (function() {
+        var regex;
+
+        regex = /:not\(([^\)]*)\)/g;
+
+        if (regex.test(selector)) {
+            selector = selector.replace(regex, '     $1 ');
+        }
+    }());
+
+    //  Remove anything after a left brace in case a user has pasted in a
+    //  rule, not just a selector
+    (function() {
+        var regex,
+            matches,
+            i,
+            len,
+            match;
+
+        regex = /{[^]*/gm;
+
+        if (regex.test(selector)) {
+
+            matches = selector.match(regex);
+
+            len = matches.getSize();
+
+            for (i = 0; i < len; i++) {
+                match = matches.at(i);
+                selector = selector.replace(
+                            match, Array(match.length + 1).join(' '));
+            }
+        }
+    }());
+
+    //  Add attribute selectors to parts collection (type b)
+    findMatch(attributeRegex, 'b');
+
+    //  Add ID selectors to parts collection (type a)
+    findMatch(idRegex, 'a');
+
+    //  Add class selectors to parts collection (type b)
+    findMatch(classRegex, 'b');
+
+    //  Add pseudo-element selectors to parts collection (type c)
+    findMatch(pseudoElementRegex, 'c');
+
+    //  Add pseudo-class selectors to parts collection (type b)
+    findMatch(pseudoClassWithBracketsRegex, 'b');
+    findMatch(pseudoClassRegex, 'b');
+
+    //  Remove universal selector and separator characters
+    selector = selector.replace(/[\*\s\+>~]/g, ' ');
+
+    //  Remove any stray dots or hashes which aren't attached to words
+    //  These may be present if the user is live-editing this selector
+    selector = selector.replace(/[#\.]/g, ' ');
+
+    //  The only things left should be element selectors (type c)
+    findMatch(elementRegex, 'c');
+
+    //  Order the parts in the order they appear in the original selector
+    //  This is neater for external apps to deal with
+    parts.sort(function(a, b) {
+        return a.index - b.index;
+    });
+
+    /* eslint-disable no-extra-parens,no-implicit-coercion */
+    return {
+        selector: singleSelectorText,
+        specificity: '0,' +
+                    typeCount.a.toString() + ',' +
+                    typeCount.b.toString() + ',' +
+                    typeCount.c.toString(),
+        specificityArray: TP.ac(0, typeCount.a, typeCount.b, typeCount.c),
+        specificityScore:
+                //  Increased these by an order of magnitude over the original
+                //  version of this code since, yes, we've seen selectors with
+                //  10+ class names.
+                (typeCount.a * 1000) + (typeCount.b * 100) + (typeCount.c * 10),
+
+        parts: parts
+    };
+    /* eslint-enable no-extra-parens,no-implicit-coercion */
+
+    /* eslint-enable no-useless-escape */
+});
+
+//  ------------------------------------------------------------------------
+
 TP.definePrimitive('$elementCSSFlush',
 function(anElement) {
 
