b4b932aad12e2d7f109f31c6f8be7a71d2559471
William J. Edney <bedney@technicalpursuit.com>
2017-10-30 16:49:59 -0500

    Added a client side version of the tibet ‘package’ command
    And added functionality to it to list/fix missing script paths

---
+++ src/tsh/TP.tsh.package.js
@@ -0,0 +1,200 @@
+//  ========================================================================
+/**
+ * @copyright Copyright (C) 1999 Technical Pursuit Inc. (TPI) All Rights
+ *     Reserved. Patents Pending, Technical Pursuit Inc. Licensed under the
+ *     OSI-approved Reciprocal Public License (RPL) Version 1.5. See the RPL
+ *     for your rights and responsibilities. Contact TPI to purchase optional
+ *     privacy waivers if you must keep your TIBET-based source code private.
+ */
+//  ------------------------------------------------------------------------
+
+/**
+ * @type {TP.tsh.package}
+ * @summary A subtype of TP.core.ActionTag that knows how to
+ *     conditionally process its child actions based on a binding expression.
+ */
+
+//  ------------------------------------------------------------------------
+
+TP.core.ActionTag.defineSubtype('tsh:package');
+
+TP.tsh.package.addTraits(TP.tsh.Element);
+
+//  ------------------------------------------------------------------------
+//  Type Methods
+//  ------------------------------------------------------------------------
+
+TP.tsh.package.Type.defineMethod('tshExecute',
+function(aRequest) {
+
+    /**
+     * @method tshExecute
+     * @summary Runs the receiver, effectively invoking its action.
+     * @param {TP.sig.Request} aRequest The request containing command input for
+     *     the shell.
+     * @returns {Object} A value which controls how the outer TSH processing
+     *     loop should continue. Common values are TP.CONTINUE, TP.DESCEND, and
+     *     TP.BREAK.
+     */
+
+    var shell,
+
+        unlisted,
+
+        profile,
+        profileParts,
+
+        results,
+        shouldFix,
+
+        newArgs,
+
+        str,
+
+        uri;
+
+    shell = aRequest.at('cmdShell');
+
+    //  If either one of the debugging flags is turned on, then echo the
+    //  debugging information.
+    if (shell.getArgument(aRequest, 'tsh:debug', null, false)) {
+        return this.printDebug(aRequest, true, false);
+    }
+
+    if (shell.getArgument(aRequest, 'tsh:debugresolve', null, false)) {
+        return this.printDebug(aRequest, true, true);
+    }
+
+    //  No arguments means we dump usage.
+    if (!shell.hasArguments(aRequest)) {
+        return this.printUsage(aRequest);
+    }
+
+    //  Are we looking for unlisted paths?
+    unlisted = shell.getArgument(aRequest, 'tsh:unlisted', null, false);
+
+    //  NB: We don't default the value here, because we default it to different
+    //  values depending on other flags.
+    profile = shell.getArgument(aRequest, 'tsh:profile', null, false);
+
+    if (TP.isString(profile)) {
+        profile = profile.unquoted();
+    } else if (unlisted) {
+        profile = TP.sys.cfg('project.packaging.profile', 'main@base');
+    } else {
+        profile = TP.sys.cfg('boot.profile', 'main@base');
+    }
+
+    profileParts = profile.split('@');
+
+    //  If we're looking for unlisted paths, then we have to ask the system for
+    //  script paths that belong to methods that were actually invoked.
+    if (unlisted) {
+        results = TP.sys.getMissingScriptPaths(
+                                profileParts.first(), profileParts.last());
+
+        //  Iterate over all of the paths and convert them into their
+        //  virtual path form.
+        results = results.collect(
+                    function(aPath) {
+                        return TP.uriInTIBETFormat(aPath);
+                    });
+
+        //  If the user is asking us to fix this situation, then we invoke a
+        //  remote command to actually patch these into the config matching the
+        //  profile.
+        shouldFix = shell.getArgument(aRequest, 'tsh:fix', null, false);
+
+        if (shouldFix) {
+
+            //  Iterate over all of the paths and escape spaces, since we're
+            //  going to join them all together on a space.
+            results = results.collect(
+                        function(aPath) {
+                            return aPath.replace(/ /g, '\\ ');
+                        });
+
+            //  Join all of the paths together with spaces.
+            results = results.join(' ');
+
+            //  Build a new set of arguments, leaving out the 'unlisted' and
+            //  'fix' flags and adding the 'add' flag and the paths to be added
+            newArgs = TP.hc(
+                        'ARGV', TP.ac(),
+                        'tsh:profile', TP.ac(profile, profile),
+                        'tsh:add', TP.ac(results, results));
+
+            aRequest.set('ARGUMENTS', newArgs);
+
+            TP.signal(null, 'RemoteConsoleCommand',
+                TP.hc(
+                    'originalRequest', aRequest,
+                    'timeout', 60000,
+                    TP.ONSUCCESS, function(aResponse) {
+
+                        var successOutputReq;
+
+                        successOutputReq =
+                            TP.sig.UserOutputRequest.construct(
+                                TP.hc(
+                                'output', 'Profile \'' + profile + '\' updated.',
+                                'async', true,
+                                'cmdAsIs', true));
+
+                        successOutputReq.fire(shell);
+                    },
+                    TP.ONFAIL, function(aResponse) {
+                        //  empty
+                    }
+                ));
+
+            aRequest.complete('delegated to :cli');
+        } else {
+            //  Iterate over all of the paths and compose a chunk of markup that
+            //  could be copied and pasted into a cfg file.
+            str = '<ul>';
+            results.forEach(
+                function(aPath) {
+                    str += '<li>' +
+                            '&lt;script src="' + aPath + '"/&gt;' +
+                            '</li>';
+                });
+            str += '</ul>';
+
+            //  Make sure to mark the request 'as is' since we're outputting
+            //  markup that we don't want processed.
+            aRequest.atPut('cmdAsIs', true);
+            aRequest.complete(str);
+        }
+    } else {
+
+        //  Otherwise, we just list the package assets
+
+        uri = TP.uriExpandPath(profileParts.first());
+        if (!TP.isURIString(uri)) {
+            uri = TP.uriJoinPaths('~app_cfg', uri);
+        }
+
+        if (TP.isEmpty(TP.uriExtension(uri))) {
+            uri += '.xml';
+        }
+
+        results = TP.boot.$listPackageAssets(uri, profileParts.last());
+
+        aRequest.complete(results);
+    }
+
+    return;
+});
+
+//  ------------------------------------------------------------------------
+
+TP.core.TSH.addHelpTopic('package',
+    TP.tsh.package.Type.getMethod('tshExecute'),
+    'List package assets either as asset paths or nodes.',
+    ':package [--profile profilename] [--unlisted] [--fix]',
+    'Coming Soon');
+
+//  ------------------------------------------------------------------------
+//  end
+//  ========================================================================
