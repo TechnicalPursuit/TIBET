9f008482b2bb26465a149cb12437decfaeed25c4
William J. Edney <bedney@technicalpursuit.com>
2017-11-05 19:39:30 -0600

    Remove obsolete CodeMirror ‘search’ and ‘hint’ plugins

--- cmd/make/_rollup_codemirror.js
+++ cmd/make/_rollup_codemirror.js
@@ -21,19 +21,9 @@
 
         make.sh.exec('mkdir ../../deps/codemirror/addon');
 
-        make.sh.exec('mkdir ../../deps/codemirror/addon/search');
-        make.sh.exec('cp -f -R addon/search/searchcursor.js ' +
-                '../../deps/codemirror/addon/search');
-
         make.sh.exec('mkdir ../../deps/codemirror/addon/runmode');
         make.sh.exec('cp -f -R addon/runmode/runmode.js ' +
                 '../../deps/codemirror/addon/runmode');
-
-        make.sh.exec('mkdir ../../deps/codemirror/addon/hint');
-        make.sh.exec('cp -f -R addon/hint/show-hint.js ' +
-                '../../deps/codemirror/addon/hint');
-        make.sh.exec('cp -f -R addon/hint/show-hint.css ' +
-                '../../deps/codemirror/addon/hint');
 
         resolve();
     };
