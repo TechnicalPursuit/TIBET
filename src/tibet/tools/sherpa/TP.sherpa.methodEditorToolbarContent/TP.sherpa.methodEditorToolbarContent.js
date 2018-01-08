171651719e4bf3cbdaae42ed44e2fa1def60ef15
William J. Edney <bedney@technicalpursuit.com>
2018-01-08 17:54:56 -0600

    Remove the ‘refresh’ button and override the updating method to not try to reference it
    ‘Refreshing’ the URI that methods belong to is frought with issues and so is disabled for now.

--- src/tibet/tools/sherpa/TP.sherpa.methodEditorToolbarContent/TP.sherpa.methodEditorToolbarContent.js
+++ src/tibet/tools/sherpa/TP.sherpa.methodEditorToolbarContent/TP.sherpa.methodEditorToolbarContent.js
@@ -17,5 +17,46 @@
 TP.sherpa.uriEditorToolbarContent.defineSubtype('methodEditorToolbarContent');
 
 //  ------------------------------------------------------------------------
+
+TP.sherpa.methodEditorToolbarContent.Inst.defineMethod('refreshControls',
+function(editorIsDirty, uriIsDirty) {
+
+    /**
+     * @method refreshControls
+     * @summary Refreshes the toolbar buttons based on the supplied dirty flags.
+     * @param {Boolean} [editorIsDirty] Whether or not the editor is dirty. If
+     *     not supplied, this defaults by querying the editor directly.
+     * @param {Boolean} [uriIsDirty] Whether or not the URI the editor is
+     *     editing is dirty. If not supplied, this defaults by querying the URI
+     *     directly.
+     * @returns {TP.sherpa.uriEditorToolbarContent} The receiver.
+     */
+
+    var editorTPElem,
+        isDirty;
+
+    editorTPElem = TP.byId(this.getAttribute('tibet:ctrl'),
+                            this.getNativeDocument());
+
+    isDirty = editorTPElem.isDirty();
+    if (isDirty) {
+        this.get('applyButton').removeAttribute('disabled');
+        this.get('revertButton').removeAttribute('disabled');
+    } else {
+        this.get('applyButton').setAttribute('disabled', true);
+        this.get('revertButton').setAttribute('disabled', true);
+    }
+
+    isDirty = editorTPElem.isSourceDirty();
+    if (isDirty && TP.isKindOf(this.get('$editorURI'), TP.core.URL)) {
+        this.get('pushButton').removeAttribute('disabled');
+    } else {
+        this.get('pushButton').setAttribute('disabled', true);
+    }
+
+    return this;
+});
+
+//  ------------------------------------------------------------------------
 //  end
 //  ========================================================================
