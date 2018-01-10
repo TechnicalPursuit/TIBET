b4bed4be0dd95f785de614a3e0d4c77590edb930
William J. Edney <bedney@technicalpursuit.com>
2018-01-10 08:52:00 -0600

    Possible patch to fix build

--- src/tibet/kernel/TIBETDOMPrimitivesPost.js
+++ src/tibet/kernel/TIBETDOMPrimitivesPost.js
@@ -6082,8 +6082,8 @@ function(aNode, joinChar, stopAncestor, onlyElements) {
     //  non-Attribute, non-Element kind of node), then we need to unshift the
     //  node's index to its parent to get a complete path.
     if (usingParent) {
-        index = TP.nodeGetIndexInParent(aNode, onlyElements);
-        path.unshift(index);
+        //index = TP.nodeGetIndexInParent(aNode, onlyElements);
+        //path.unshift(index);
     }
 
     return path.reverse().join(joinChar || '.');
