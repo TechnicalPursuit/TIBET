ca9d92f85c72bcea9f38fb6e8b5cfc2696da68c1
idearat <ss@technicalpursuit.com>
2018-01-20 13:59:18 -0700

    Add support to :import for package@config references.

--- src/tsh/TP.tsh.import.js
+++ src/tsh/TP.tsh.import.js
@@ -45,9 +45,6 @@ function(aRequest) {
 
         input,
 
-        len,
-        i,
-
         ref,
         options,
 
@@ -91,94 +88,107 @@ function(aRequest) {
     input = shell.getArgument(aRequest, 'tsh:href', null, true);
     if (TP.notValid(input)) {
         if (TP.isEmpty(input = aRequest.stdin())) {
-            return aRequest.fail(
-                'Unable to find reference for ' + TP.str(ref));
+            return aRequest.fail('Invalid package/file reference.');
         }
     } else {
         input = TP.ac(input);
     }
 
-    len = input.getSize();
-    for (i = 0; i < len; i++) {
-        ref = input.at(i);
+    if (input.getSize() > 1) {
+        aRequest.fail('Import limited to a single target.');
+        return;
+    }
 
+    ref = input.at(0);
+
+    if (/@/.test(ref)) {
+
+        /* eslint-disable no-loop-func */
+        TP.sys.importPackage(ref).then(function(result) {
+            aRequest.complete(ref);
+        }).catch(function(err) {
+            aRequest.fail(
+                'tsh:import failed to load ' + ref);
+        });
+        /* eslint-enable no-loop-func */
+
+        //  NOTE we have to return here and let promise do the rest.
+        return;
+
+    } else {
         ref = TP.uriExpandPath(ref);
+    }
 
-        options = TP.boot.$$loadpaths.grep(ref);
-        if (TP.isEmpty(options)) {
-            //  a file we've never seen before
-            options = TP.ac(ref);
-        }
+    options = TP.boot.$$loadpaths.grep(ref);
+    if (TP.isEmpty(options)) {
+        //  a file we've never seen before
+        options = TP.ac(ref);
+    }
 
-        //  ambiguous...
-        if (options.getSize() > 1) {
+    //  ambiguous...
+    if (options.getSize() > 1) {
+        aRequest.fail(
+            'Multiple choices: ' + TP.src(options));
+        return;
+    }
+
+    file = options.collapse();
+    try {
+        flag = TP.sys.shouldLogCodeChanges();
+        TP.sys.shouldLogCodeChanges(false);
+
+        /*
+        root.stdout('tsh:import loading source from ' + file,
+                        aRequest);
+        */
+
+        url = TP.uc(file);
+        if (TP.notValid(url)) {
             aRequest.fail(
-                'Multiple choices: ' + TP.src(options));
-
-            continue;
+                'tsh:import failed to load ' + file);
+            return;
         }
 
-        file = options.collapse();
-        try {
-            flag = TP.sys.shouldLogCodeChanges();
-            TP.sys.shouldLogCodeChanges(false);
-
-            /*
-            root.stdout('tsh:import loading source from ' + file,
-                            aRequest);
-            */
-
-            url = TP.uc(file);
-            if (TP.notValid(url)) {
-                aRequest.fail(
-                    'tsh:import failed to load ' + file);
-
-                continue;
-            }
-
-            resp = url.getResource(TP.hc('refresh', true, 'async', false));
-            if (TP.notValid(src = resp.get('result'))) {
-                aRequest.fail(
-                    'tsh:import failed to load ' + file);
-
-                continue;
-            }
-
-            debug = TP.sys.shouldUseDebugger();
-            TP.sys.shouldUseDebugger(false);
-
-            TP.boot.$sourceImport(src, null, TP.str(url), true);
-
-            //  Grab the location of the URL, trim it down so that its only
-            //  the name of the file with no extension and see if there's
-            //  now a 'type' with that name. If so, call its 'initialize()'
-            //  method.
-            urlLoc = url.getLocation();
-            urlName = TP.uriName(urlLoc);
-            urlTypeName = urlName.slice(0, urlName.lastIndexOf('.'));
-            urlTypeName = urlTypeName.replace(/_/, ':');
-
-            if (TP.isType(urlType = urlTypeName.asType()) &&
-                !urlType.isInitialized() &&
-                urlType.ownsMethod('initialize')) {
-                try {
-                    urlType.initialize();
-                    urlType.isInitialized(true);
-                } catch (e) {
-                    this.raise(
-                        'TP.sig.InitializationException',
-                        'Unable to initialize ' + urlType.getName());
-                }
-            }
-        } catch (e) {
+        resp = url.getResource(TP.hc('refresh', true, 'async', false));
+        if (TP.notValid(src = resp.get('result'))) {
             aRequest.fail(
-                TP.ec(e, 'tsh:import failed to load ' + file));
-
-            continue;
-        } finally {
-            TP.sys.shouldLogCodeChanges(flag);
-            TP.sys.shouldUseDebugger(debug);
+                'tsh:import failed to load ' + file);
+            return;
         }
+
+        debug = TP.sys.shouldUseDebugger();
+        TP.sys.shouldUseDebugger(false);
+
+        TP.boot.$sourceImport(src, null, TP.str(url), true);
+
+        //  Grab the location of the URL, trim it down so that its only
+        //  the name of the file with no extension and see if there's
+        //  now a 'type' with that name. If so, call its 'initialize()'
+        //  method.
+        urlLoc = url.getLocation();
+        urlName = TP.uriName(urlLoc);
+        urlTypeName = urlName.slice(0, urlName.lastIndexOf('.'));
+        urlTypeName = urlTypeName.replace(/_/, ':');
+
+        if (TP.isType(urlType = urlTypeName.asType()) &&
+            !urlType.isInitialized() &&
+            urlType.ownsMethod('initialize')) {
+            try {
+                urlType.initialize();
+                urlType.isInitialized(true);
+            } catch (e) {
+                this.raise(
+                    'TP.sig.InitializationException',
+                    'Unable to initialize ' + urlType.getName());
+            }
+        }
+    } catch (e) {
+        aRequest.fail(
+            TP.ec(e, 'tsh:import failed to load ' + file));
+        return;
+    } finally {
+        TP.sys.shouldLogCodeChanges(flag);
+        TP.sys.shouldUseDebugger(debug);
     }
 
     aRequest.complete(url.getLocation());
