3dc5a9b10c39e874ffdca8d9189ae4d0257ad598
William J. Edney <bedney@technicalpursuit.com>
2018-03-10 15:48:40 -0600

    Added HTML ‘template’ element

--- src/xhtml/html_MetainformationModuleNodes.js
+++ src/xhtml/html_MetainformationModuleNodes.js
@@ -27,6 +27,84 @@ TP.html.meta.Inst.resolveTraits(
         TP.ac('getContent', 'setContent'),
         TP.core.EmptyElementNode);
 
+//  ========================================================================
+//  TP.html.template
+//  ========================================================================
+
+/**
+ * @type {TP.html.template}
+ * @summary 'template' tag.
+ */
+
+//  ------------------------------------------------------------------------
+
+TP.html.Attrs.defineSubtype('template');
+
+//  ------------------------------------------------------------------------
+//  Instance Methods
+//  ------------------------------------------------------------------------
+
+TP.html.template.Inst.defineMethod('getContent',
+function(aRequest) {
+
+    /**
+     * @method getContent
+     * @summary Returns the receiver's content.
+     * @description At this level, this method returns its 'inner content',
+     *     which is a serialization of the receiver's '.content' property.
+     * @param {TP.sig.Request|TP.core.Hash} aRequest Optional control
+     *     parameters.
+     * @returns {String} The text content of the native node.
+     */
+
+    var nativeNode,
+        frag;
+
+    nativeNode = this.getNativeNode();
+
+    //  Note here that force making a copy of the '.content' fragment -
+    //  otherwise, we'll throw an error.
+    frag = TP.copy(nativeNode.content);
+    if (!TP.isFragment(frag)) {
+        return '';
+    }
+
+    return TP.wrap(frag).asString();
+});
+
+//  ------------------------------------------------------------------------
+
+TP.html.template.Inst.defineMethod('getContentNode',
+function(aRequest) {
+
+    /**
+     * @method getContentNode
+     * @summary Returns the receiver's content node(s). This method is provided
+     *     for API compatibility with other types.
+     * @description At this level, this method returns its 'inner content' node,
+     *     which is a TP.core.DocumentFragment, which will contain a *clone* of
+     *     the receiver's '.content'. property.
+     * @param {TP.sig.Request|TP.core.Hash} aRequest Optional control
+     *     parameters.
+     * @returns {TP.core.DocumentFragment} A TP.core.DocumentFragment containing
+     *     a *clone* of the receiver's '.content' DocumentFragment.
+     */
+
+    var nativeNode,
+        frag;
+
+    nativeNode = this.getNativeNode();
+
+    //  Note here that force making a copy of the '.content' fragment -
+    //  otherwise, we'll throw an error.
+    frag = TP.copy(nativeNode.content);
+    if (!TP.isFragment(frag)) {
+        return null;
+    }
+
+    return TP.wrap(frag);
+});
+
 //  ------------------------------------------------------------------------
 //  end
 //  ========================================================================
