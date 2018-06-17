761099f972622fb1981691fd668594aa7822c7c9
William J. Edney <bedney@technicalpursuit.com>
2018-06-17 13:50:27 -0500

    Allow drag up as well as drag move to pass the curtain trap
    We now allow the curtain to also be used for ‘smooth drag tracking’ when we want a ‘window-wide topmost’ layer to do so.

--- src/xctrls/TP.xctrls.curtain/TP.xctrls.curtain.js
+++ src/xctrls/TP.xctrls.curtain/TP.xctrls.curtain.js
@@ -117,9 +117,28 @@ function(aSignal) {
      * @returns {TP.xctrls.curtain} The receiver.
      */
 
-    //  NB: Unlike the trap for all of the other UI signals, we allow 'drag
-    //  move' to pass, since otherwise drag will be jittery for things like
-    //  dialog panels that overlay the curtain.
+    //  NB: Unlike the trap for almost all of the other UI signals, we allow
+    //  'drag move' to pass, since we use curtain for smooth drag tracking.
+    return this;
+}, {
+    phase: TP.CAPTURING
+});
+
+//  ------------------------------------------------------------------------
+
+TP.xctrls.curtain.Inst.defineHandler('DOMDragUp',
+function(aSignal) {
+
+    /**
+     * @method handleDOMDragUp
+     * @summary Handles notifications of 'drag up' signals.
+     * @param {TP.sig.DOMDragMove} aSignal The TIBET signal which triggered
+     *     this method.
+     * @returns {TP.xctrls.curtain} The receiver.
+     */
+
+    //  NB: Unlike the trap for almost all of the other UI signals, we allow
+    //  'drag up' to pass, since we use curtain for smooth drag tracking.
     return this;
 }, {
     phase: TP.CAPTURING
