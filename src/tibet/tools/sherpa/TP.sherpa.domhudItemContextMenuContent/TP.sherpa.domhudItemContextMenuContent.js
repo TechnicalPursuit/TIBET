c39e0008b9a55082b2864582a394a81af84a01ec
William J. Edney <bedney@technicalpursuit.com>
2018-06-27 16:19:28 -0500

    Added context menu capability and tags to the Sherpa dom hud

---
+++ src/tibet/tools/sherpa/TP.sherpa.domhudItemContextMenuContent/TP.sherpa.domhudItemContextMenuContent.js
@@ -0,0 +1,21 @@
+//  ========================================================================
+/**
+ * @copyright Copyright (C) 1999 Technical Pursuit Inc. (TPI) All Rights
+ *     Reserved. Patents Pending, Technical Pursuit Inc. Licensed under the
+ *     OSI-approved Reciprocal Public License (RPL) Version 1.5. See the RPL
+ *     for your rights and responsibilities. Contact TPI to purchase optional
+ *     privacy waivers if you must keep your TIBET-based source code private.
+ */
+//  ========================================================================
+
+/**
+ * @type {TP.sherpa.domhudItemContextMenuContent}
+ */
+
+//  ------------------------------------------------------------------------
+
+TP.sherpa.TemplatedTag.defineSubtype('domhudItemContextMenuContent');
+
+//  ------------------------------------------------------------------------
+//  end
+//  ========================================================================
