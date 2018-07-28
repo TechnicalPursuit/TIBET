2ae4107a12fbc2dfc6dfcc25545a2bfd9045057c
William J. Edney <bedney@technicalpursuit.com>
2018-07-28 15:19:12 -0500

    Comment updates

--- src/tibet/tools/sherpa/TP.sherpa.inspector/couch/TP.sherpa.CouchDBSubInspectorSources.js
+++ src/tibet/tools/sherpa/TP.sherpa.inspector/couch/TP.sherpa.CouchDBSubInspectorSources.js
@@ -1,5 +1,17 @@
-//  ------------------------------------------------------------------------
-//  Sherpa Couch Tool API
+//  ========================================================================
+/**
+ * @copyright Copyright (C) 1999 Technical Pursuit Inc. (TPI) All Rights
+ *     Reserved. Patents Pending, Technical Pursuit Inc. Licensed under the
+ *     OSI-approved Reciprocal Public License (RPL) Version 1.5. See the RPL
+ *     for your rights and responsibilities. Contact TPI to purchase optional
+ *     privacy waivers if you must keep your TIBET-based source code private.
+ */
+//  ========================================================================
+
+/**
+ * @type {TP.sherpa.CouchDBSubInspectorSources}
+ */
+
 //  ------------------------------------------------------------------------
 
 TP.sherpa.InspectorPathSource.defineSubtype('sherpa.CouchDBSubInspectorSources');
