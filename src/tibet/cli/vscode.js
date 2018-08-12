bc04700243bfc7896175554be45adb07c98dbc4e
idearat <ss@technicalpursuit.com>
2018-08-12 13:55:07 -0600

    Add vscode command for opening a file:line:char in Visual Studio Code
    Expected to be invoked remotely via the CLI command to allow the Sherpa to open files in VSCode.

---
+++ src/tibet/cli/vscode.js
@@ -0,0 +1,95 @@
+//  ========================================================================
+/**
+ * @copyright Copyright (C) 1999 Technical Pursuit Inc. (TPI) All Rights
+ *     Reserved. Patents Pending, Technical Pursuit Inc. Licensed under the
+ *     OSI-approved Reciprocal Public License (RPL) Version 1.5. See the RPL
+ *     for your rights and responsibilities. Contact TPI to purchase optional
+ *     privacy waivers if you must keep your TIBET-based source code private.
+ * @overview The 'tibet vscode' command. Allows client-side triggering of VSCode
+ *     to let you navigate VSCode to a specific file:line from the Sherpa.
+ */
+//  ========================================================================
+
+/* eslint indent:0 */
+
+(function() {
+
+'use strict';
+
+var CLI,
+    Cmd;
+
+CLI = require('./_cli');
+
+//  ---
+//  Type Construction
+//  ---
+
+Cmd = function() {
+    //  empty
+};
+Cmd.Parent = require('./_cmd');
+Cmd.prototype = new Cmd.Parent();
+
+
+//  ---
+//  Type Attributes
+//  ---
+
+/**
+ * The command execution context.
+ * @type {Cmd.CONTEXTS}
+ */
+Cmd.CONTEXT = CLI.CONTEXTS.ANY;
+
+/**
+ * The command name for this type.
+ * @type {string}
+ */
+Cmd.NAME = 'vscode';
+
+//  ---
+//  Instance Attributes
+//  ---
+
+/**
+ * The command usage string.
+ * @type {string}
+ */
+Cmd.prototype.USAGE = 'tibet vscode [path[:line]]';
+
+
+//  ---
+//  Instance Methods
+//  ---
+
+/**
+ * Perform the actual command processing logic.
+ * @returns {Number} A return code. Non-zero indicates an error.
+ */
+Cmd.prototype.execute = function() {
+    var cp,
+        child,
+        target,
+        args;
+
+    args = [];
+    target = process.argv[process.argv.length - 1];
+    if (CLI.notEmpty(target) && target !== 'vscode') {
+        if (target.indexOf(':') !== -1) {
+            args.push('--goto');
+        }
+        args.push(target);
+    }
+
+    cp = require('child_process');
+    child = cp.spawn('code', args, {detached: true, stdio: ['ignore', 'ignore', 'ignore']});
+    child.unref();
+
+    return 0;
+};
+
+
+module.exports = Cmd;
+
+}());
