d92264557349bd31edcfb891fb33665ad307fcbb
William J. Edney <bedney@technicalpursuit.com>
2018-11-01 15:02:16 -0500

    Major update to use new authentication capability of latest TIBET AWS capability
    Also uses externally provided login UI panel.

--- src/tibet/tools/sherpa/TP.sherpa.inspector/aws/TP.sherpa.AWSSubInspectorSources.js
+++ src/tibet/tools/sherpa/TP.sherpa.inspector/aws/TP.sherpa.AWSSubInspectorSources.js
@@ -109,7 +109,96 @@ function() {
 
 TP.sherpa.AWSSubInspectorSources.Inst.defineMethod('checkAuthentication',
 function() {
-    return true;
+
+    /**
+     * @method checkAuthentication
+     * @summary Returns whether or not this object is authenticated with AWS to
+     *     perform its required operations.
+     * @returns {Boolean} Whether or not we're authenticated.
+     */
+
+    var isAuthenticated;
+
+    isAuthenticated = TP.aws.AWSService.isAuthenticated('AWS_PASSTHROUGH');
+
+    return isAuthenticated;
+});
+
+//  ------------------------------------------------------------------------
+
+TP.sherpa.AWSSubInspectorSources.Inst.defineMethod('getAuthenticationContent',
+function(options) {
+
+    /**
+     * @method getAuthenticationContent
+     * @summary Returns the Element containing user interface content that
+     *     allows the user to authenticate.
+     * @param {TP.core.Hash} options A hash of data available to this source to
+     *     generate the content. This will have the following keys, amongst
+     *     others:
+     *          'targetObject':     The object being queried using the
+     *                              targetAspect to produce the object being
+     *                              displayed.
+     *          'targetAspect':     The property of the target object currently
+     *                              being displayed.
+     *          'pathParts':        The Array of parts that make up the
+     *                              currently selected path.
+     *          'bindLoc':          The URI location where the data for the
+     *                              content can be found.
+     * @returns {Element} The content used for authentication GUI for the user.
+     */
+
+    var authPanelLoc,
+        authPanelReq,
+        authPanelResp,
+
+        win,
+
+        bayNum;
+
+    //  Grab the authenication panel content.
+    authPanelLoc =
+        TP.uc('~ide_root/TP.sherpa.inspector/aws/auth_panel.xhtml').
+        getLocation();
+
+    authPanelReq = TP.request('uri', authPanelLoc, 'async', false);
+    authPanelResp = TP.httpGet(authPanelLoc, authPanelReq);
+
+    win = TP.sys.getUIRoot().getNativeWindow();
+
+    //  The bay that the login panel will be drawn into is one more than what is
+    //  currently there and can be computed by the total number of bays.
+    bayNum = options.at('pathParts').getSize();
+
+    //  After we repaint, and the 'bay inspector item' is real, then set up a
+    //  UIDidFocus handler on that item to focus the username field.
+    (function() {
+
+        var inspector,
+            bayTPElem,
+            usernameTPElem;
+
+        inspector = TP.byId('SherpaInspector', win);
+        bayTPElem = inspector.getBayFromSlotPosition(bayNum);
+
+        //  Set up the UIDidFocus handler directly on the bay inspector item.
+        bayTPElem.defineHandler('UIDidFocus',
+            function(aSignal) {
+
+                //  Make sure to remove this handler because this bay inspector
+                //  item will be reused.
+                delete bayTPElem[TP.composeHandlerName('UIDidFocus')];
+
+                //  Grab the username field and focus it.
+                usernameTPElem = bayTPElem.get('.username');
+                usernameTPElem.focus();
+            });
+
+    }).queueForNextRepaint(win);
+
+    //  Grab the result of the response and return it's documentElement (i.e.
+    //  root element).
+    return authPanelResp.get('result').documentElement;
 });
 
 //  ------------------------------------------------------------------------
@@ -175,7 +264,7 @@ function(options) {
      */
 
     if (!this.checkAuthentication()) {
-        return this.requestAuthentication();
+        return this.getAuthenticationContent(options);
     }
 
     return this.callNextMethod();
@@ -242,6 +331,82 @@ function(options) {
     }
 
     return this.callNextMethod();
+});
+
+//  ------------------------------------------------------------------------
+
+TP.sherpa.AWSSubInspectorSources.Inst.defineHandler('AuthenticateConnection',
+function() {
+
+    /**
+     * @method handleAuthenticateConnection
+     * @summary Handles when the user has completed authentication information
+     *     in the UI in an attempt to authenticate an AWS connection.
+     * @param {TP.sig.AuthenticateConnection} aSignal The signal indicating that
+     *     the user has dismisse
+     * @returns {TP.sherpa.AWSSubInspectorSources} The receiver.
+     */
+
+    var inspector,
+        content,
+
+        username,
+        password,
+
+        message,
+
+        serverURI,
+
+        authRequest;
+
+    //  Grab the current bay content.
+    inspector = TP.byId('SherpaInspector', TP.sys.getUIRoot());
+    content = inspector.getInspectorBayContentItem();
+
+    //  Grab the username and password fields from that content.
+    username = TP.byCSSPath('.username', content, true);
+    password = TP.byCSSPath('.password', content, true);
+
+    //  Grab the message output to the user from that content, show it and a
+    //  message that we're authenticating.
+    message = TP.byCSSPath('.login_user_message', content, true);
+    message.setRawContent(TP.sc('Attempting authentication...'));
+    message.setAttribute('hidden', false);
+
+    serverURI = TP.uc(this.get('serverAddress'));
+
+    //  Authenticate the server URI that we're using with the
+    //  AWSPassthroughService using the supplied username and password. Note that
+    //  this object will handle the request itself, such that we won't need to
+    //  fire it.
+    authRequest = TP.aws.AWSPassthroughService.authenticate(
+                        'AWS_PASSTHROUGH',
+                        username.getValue(),
+                        password.getValue());
+
+    //  The authentication succeeded - repopulate the bay we were using to
+    //  display the authentication UI and size the inspector to the new content.
+    authRequest.defineHandler('RequestSucceeded',
+        function(aResponse) {
+            inspector.repopulateBay();
+            inspector.sizeBays();
+            inspector.scrollBaysToEnd();
+        });
+
+    //  The authentication failed - let the user know by setting the message
+    //  output. Reset the username and password to empty values and focus the
+    //  username field.
+    authRequest.defineHandler('401',
+        function(aResponse) {
+            message.setRawContent('Login failed');
+
+            username.setValue('');
+            password.setValue('');
+
+            username.focus();
+        });
+
+    return this;
 });
 
 //  ------------------------------------------------------------------------
@@ -368,8 +533,12 @@ function(options) {
 
     dataURI = TP.uc(options.at('bindLoc'));
 
-    requestParams = TP.hc('action', 'listBuckets');
-    s3Request = TP.sig.AWSS3Request.construct(requestParams);
+    requestParams = TP.hc(
+                        'action', 'listBuckets',
+                        'params',
+                            TP.hc('remoteService', 'S3'));
+
+    s3Request = TP.sig.AWSPassthroughRequest.construct(requestParams);
     s3Request.defineHandler('RequestSucceeded',
                             function(aResponse) {
                                 var result,
@@ -413,8 +582,12 @@ function(options) {
 
     dataURI = TP.uc(options.at('bindLoc'));
 
-    requestParams = TP.hc('action', 'listFunctions');
-    lambdaRequest = TP.sig.AWSLambdaRequest.construct(requestParams);
+    requestParams = TP.hc(
+                        'action', 'listFunctions',
+                        'params',
+                            TP.hc('remoteService', 'Lambda'));
+
+    lambdaRequest = TP.sig.AWSPassthroughRequest.construct(requestParams);
     lambdaRequest.defineHandler('RequestSucceeded',
                             function(aResponse) {
                                 var result,
