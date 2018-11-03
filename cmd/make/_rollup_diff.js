971416926dfd5d495a6a9c16d202541b26b4ef38
William J. Edney <bedney@technicalpursuit.com>
2018-11-03 17:23:39 -0500

    Convert from using TPI-owned diff fork to using mainline repository
    This resulted in no version change of diff used by TIBET.
    
    The _rollup_diff command was updated to remove commands for minification that are no longer necessary (minified packages come prebuilt now).
    
    Also, the prebuilt minified package was added to the manifest.

--- cmd/make/_rollup_diff.js
+++ cmd/make/_rollup_diff.js
@@ -9,12 +9,11 @@
         npmdir = make.CLI.expandPath('~npm_dir');
         make.sh.cd(make.path.join(npmdir, 'diff'));
 
-        make.sh.exec('npm install');
-
-        make.sh.exec('grunt build --force');
         make.sh.sed('-i', /\/\/# sourceMappingURL/g,
             '\n//**no source maps!**', './dist/diff.js');
+
         make.sh.exec('cp -f dist/diff.js  ../../deps/diff-tpi.js');
+        make.sh.exec('cp -f dist/diff.min.js  ../../deps/diff-tpi.min.js');
 
         resolve();
     };
