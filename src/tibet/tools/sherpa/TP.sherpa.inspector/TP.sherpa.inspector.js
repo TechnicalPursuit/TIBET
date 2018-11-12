15b01497040f09516a6778502b03692785346ab8
William J. Edney <bedney@technicalpursuit.com>
2018-11-11 21:21:31 -0600

    No need to use the getter again - we already have the items

--- src/tibet/tools/sherpa/TP.sherpa.inspector/TP.sherpa.inspector.js
+++ src/tibet/tools/sherpa/TP.sherpa.inspector/TP.sherpa.inspector.js
@@ -3002,7 +3002,7 @@ function(aBayNum) {
     data = TP.getDataForTool(
                     resolver,
                     'inspector',
-                    TP.hc('pathParts', this.get('selectedItems')));
+                    TP.hc('pathParts', selectedItems));
 
     //  Set the resource of the URI holding the bound data for that bay to the
     //  refreshed data that we just obtained.
