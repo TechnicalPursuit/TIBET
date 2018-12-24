13a0cf5640b7c9a1aa3b12b984ca8f1b4ce445a9
William J. Edney <bedney@technicalpursuit.com>
2018-12-24 06:27:25 -0600

    Added list for supertypes to type info.

--- src/tibet/tools/sherpa/src/TP.lang.RootObject_Additions.js
+++ src/tibet/tools/sherpa/src/TP.lang.RootObject_Additions.js
@@ -310,6 +310,7 @@ function(options) {
             TP.ac('Type Methods', 'Type Methods'),
             TP.ac('Local Attributes', 'Local Attributes'),
             TP.ac('Local Methods', 'Local Methods'),
+            TP.ac('Supertypes', 'Supertypes'),
             TP.ac('Subtypes', 'Subtypes'),
             TP.ac('Tests', 'Tests')
         );
@@ -405,6 +406,13 @@ function(aSourceName) {
         case 'Local Methods':
 
             source = TP.sherpa.LocalMethodsInspectorSource.construct();
+            source.addEntry('primary', this);
+
+            break;
+
+        case 'Supertypes':
+
+            source = TP.sherpa.SupertypesInspectorSource.construct();
             source.addEntry('primary', this);
 
             break;
@@ -1678,6 +1686,77 @@ function(options) {
 //  ------------------------------------------------------------------------
 
 TP.sherpa.SubtypesInspectorSource.Inst.defineMethod(
+    'resolveAspectForInspector',
+function(anAspect, options) {
+
+    /**
+     * @method resolveAspectForInspector
+     * @summary Returns the object that is produced when resolving the aspect
+     *     against the receiver.
+     * @param {String} anAspect The aspect to resolve against the receiver to
+     *     produce the return value.
+     * @param {TP.core.Hash} options A hash of data available to this source to
+     *     generate the configuration data. This will have the following keys,
+     *     amongst others:
+     *          'pathParts':        The Array of parts that make up the
+     *                              currently selected path.
+     * @returns {Object} The object produced when resolving the aspect against
+     *     the receiver.
+     */
+
+    return TP.sys.getTypeByName(anAspect);
+});
+
+//  ========================================================================
+//  TP.sherpa.SupertypesInspectorSource
+//  ========================================================================
+
+TP.sherpa.InspectorSource.defineSubtype('SupertypesInspectorSource');
+
+//  ------------------------------------------------------------------------
+
+TP.sherpa.SupertypesInspectorSource.Inst.defineMethod(
+    'getDataForInspector',
+function(options) {
+
+    /**
+     * @method getDataForInspector
+     * @summary Returns the source's data that will be supplied to the content
+     *     hosted in an inspector bay. In most cases, this data will be bound to
+     *     the content using TIBET data binding. Therefore, when this data
+     *     changes, the content will be refreshed to reflect that.
+     * @param {TP.core.Hash} options A hash of data available to this source to
+     *     generate the data. This will have the following keys, amongst others:
+     *          'targetObject':     The object being queried using the
+     *                              targetAspect to produce the object being
+     *                              displayed.
+     *          'targetAspect':     The property of the target object currently
+     *                              being displayed.
+     *          'pathParts':        The Array of parts that make up the
+     *                              currently selected path.
+     *          'bindLoc':          The URI location where the data for the
+     *                              content can be found.
+     * @returns {Object} The data that will be supplied to the content hosted in
+     *     a bay.
+     */
+
+    var result;
+
+    result = this.getEntryAt('primary').getSupertypeNames();
+
+    result = result.collect(
+                function(entry) {
+                    return TP.ac(
+                            entry,
+                            this.getEntryLabel(entry));
+                }.bind(this));
+
+    return result;
+});
+
+//  ------------------------------------------------------------------------
+
+TP.sherpa.SupertypesInspectorSource.Inst.defineMethod(
     'resolveAspectForInspector',
 function(anAspect, options) {
 
