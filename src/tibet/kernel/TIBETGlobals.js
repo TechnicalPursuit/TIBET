f1f40b22023aa1f372f2a171668443b442f2fdef
William J. Edney <bedney@technicalpursuit.com>
2019-01-12 06:55:42 -0600

    Add regex that matches any CSS url(…) values.

--- src/tibet/kernel/TIBETGlobals.js
+++ src/tibet/kernel/TIBETGlobals.js
@@ -3015,7 +3015,8 @@ TP.regex.CSS_IMPORT_RULE = /@import\s*(url\()?['"]?(.*?)['"]?(\))?;/g;
 TP.regex.CSS_URL_PROPERTY = /:\s+(url\(){1}['"]?([^)]*?)['"]?(\)){1};/g;
 
 //  The URL value of the property can be found in group 2. (needs reset)
-TP.regex.CSS_URL_VALUE = /(url\(){1}['"]?(~[^)]*?)['"]?(\)){1}/g;
+TP.regex.CSS_URL_VALUE = /(url\(){1}['"]?([^)]*?)['"]?(\)){1}/g;
+
 //  The URL value of the property can be found in group 2. (needs reset)
 TP.regex.CSS_VIRTUAL_URL_VALUE = /(url\(){1}['"]?(~[^)]*?)['"]?(\)){1}/g;
 
