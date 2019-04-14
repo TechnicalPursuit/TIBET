9dbb330696b097987aac88ac463873bc7f540e72
William J. Edney <bedney@technicalpursuit.com>
2019-04-14 18:43:21 -0500

    Since this RegExp no longer takes ‘$_’ statement into account, we need to do a separate check here for ‘$_’

--- src/tibet/kernel/TIBETBinding.js
+++ src/tibet/kernel/TIBETBinding.js
@@ -5683,10 +5683,12 @@ function(aspect, exprs, outerScopeValue, updatedAspects, aFacet, transformFunc, 
                 //  no path type was supplied.
                 pathType = aPathType;
 
-                //  If the expression contains ACP variables, then set the
-                //  pathType to null and let the machinery that looks at the
-                //  value compute the expression value.
-                if (TP.regex.ACP_PATH_CONTAINS_VARIABLES.test(expr)) {
+                //  If the expression contains ACP variables or a '$_' stdin
+                //  indicator, then set the pathType to null and let the
+                //  machinery that looks at the value compute the expression
+                //  value.
+                if (TP.regex.ACP_PATH_CONTAINS_VARIABLES.test(expr) ||
+                    expr.contains('$_')) {
                     pathType = null;
                 }
 
