66d8b3789c3d7149dd68bd77a19279c423ff3fa8
William J. Edney <bedney@technicalpursuit.com>
2019-07-24 21:58:30 -0500

    Add tooltip hints to the Sherpa inspector URI and Signal Map lists

--- src/tibet/tools/sherpa/TP.sherpa.inspector/TP.sherpa.TIBETSignalMapListInspectorSource.js
+++ src/tibet/tools/sherpa/TP.sherpa.inspector/TP.sherpa.TIBETSignalMapListInspectorSource.js
@@ -28,6 +28,42 @@ TP.sherpa.InspectorSource.defineSubtype(
 //  ------------------------------------------------------------------------
 
 TP.sherpa.TIBETSignalMapListInspectorSource.Inst.defineMethod(
+'getContentForInspector',
+function(options) {
+
+    /**
+     * @method getContentForInspector
+     * @summary Returns the source's content that will be hosted in an inspector
+     *     bay.
+     * @param {TP.core.Hash} options A hash of data available to this source to
+     *     generate the content. This will have the following keys, amongst
+     *     others:
+     *          'targetObject':     The object being queried using the
+     *                              targetAspect to produce the object being
+     *                              displayed.
+     *          'targetAspect':     The property of the target object currently
+     *                              being displayed.
+     *          'pathParts':        The Array of parts that make up the
+     *                              currently selected path.
+     *          'bindLoc':          The URI location where the data for the
+     *                              content can be found.
+     * @returns {Element} The Element that will be used as the content for the
+     *     bay.
+     */
+
+    var dataURI;
+
+    dataURI = TP.uc(options.at('bindLoc'));
+
+    return TP.elem(
+            '<xctrls:list bind:in="{data: ' +
+            dataURI.asString() +
+            '}" filter="true" alwaysSignalChange="true" toggleItems="false" tooltips="true"/>');
+});
+
+//  ------------------------------------------------------------------------
+
+TP.sherpa.TIBETSignalMapListInspectorSource.Inst.defineMethod(
 'getDataForInspector',
 function(options) {
 
--- src/tibet/tools/sherpa/TP.sherpa.inspector/TP.sherpa.TIBETURIListInspectorSource.js
+++ src/tibet/tools/sherpa/TP.sherpa.inspector/TP.sherpa.TIBETURIListInspectorSource.js
@@ -124,7 +124,7 @@ function(options) {
     return TP.elem(
             '<xctrls:list bind:in="{data: ' +
             dataURI.asString() +
-            '}" filter="true" alwaysSignalChange="true" toggleItems="false"/>');
+            '}" filter="true" alwaysSignalChange="true" toggleItems="false" tooltips="true"/>');
 });
 
 //  ------------------------------------------------------------------------
