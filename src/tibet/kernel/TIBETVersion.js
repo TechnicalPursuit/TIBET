0d61582979f053d162b3e6e76d3a267e6afbeb69
William J. Edney <bedney@technicalpursuit.com>
2019-10-02 16:16:30 -0500

    Release-ready build 5.0.0-develop

--- src/tibet/kernel/TIBETVersion.js
+++ src/tibet/kernel/TIBETVersion.js
@@ -16,18 +16,18 @@
     /* eslint-disable quote-props,quotes */
     release({
         //  new release
-        "semver": "5.0.0-rc.6+g1103c9888e.121.1568308718087",
+        "semver": "5.0.0+gc063bc760f.123.1570050478939",
         "major": "5",
         "minor": "0",
         "patch": "0",
-        "suffix": "rc",
-        "increment": "6",
-        "time": "1568308718087",
+        "suffix": "",
+        "increment": "7",
+        "time": "1570050478939",
         //  prior semver
-        "describe": "5.0.0-rc.5-121-g1103c9888e",
-        "ptag": "5.0.0-rc.5",
-        "commits": "121",
-        "phash": "1103c9888e"
+        "describe": "5.0.0-rc.6-123-gc063bc760f",
+        "ptag": "5.0.0-rc.6",
+        "commits": "123",
+        "phash": "c063bc760f"
     });
     /* eslint-enable quote-props,quotes */
     //  --- latest.js end ---
