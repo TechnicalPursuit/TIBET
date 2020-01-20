0e1de5f3d65c1871312002e8d0cfbca108a536db
William J. Edney <bedney@technicalpursuit.com>
2020-01-20 14:02:22 -0600

    Recognize when we’re building an app release and switch on compression (just like for the lib)

--- dna/electron/cmd/make/build.js
+++ dna/electron/cmd/make/build.js
@@ -2,7 +2,20 @@
     'use strict';
 
     module.exports = function(make, resolve, reject) {
+        var options,
+            config;
+
         make.log('building app...');
+
+        //  If --release was specified then we want to force certain options for
+        //  downstream rollup processing.
+        options = make.reparse({boolean: ['release']});
+
+        if (options.release) {
+            config = make.cfg('make.compression.parse_options');
+            config.default.zip = true;
+            config.default.brotli = true;
+        }
 
         make.chain('clean',
             'build_docs',
