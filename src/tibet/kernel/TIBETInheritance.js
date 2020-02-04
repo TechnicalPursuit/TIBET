c52f3f47a8ca6f5bd0aa74a7aefb192e997ec4d2
William J. Edney <bedney@technicalpursuit.com>
2020-02-04 14:59:57 -0600

    Lint

--- src/tibet/kernel/TIBETInheritance.js
+++ src/tibet/kernel/TIBETInheritance.js
@@ -10987,9 +10987,9 @@ function() {
     TP.boot.$stdout('Preparing to initialize: ' + own.getValues().join('\n'),
         TP.TRACE);
 
-    if (TP.isValid(own)) {
+    thisref = this;
 
-        thisref = this;
+    if (TP.isValid(own)) {
 
         //  Loop over each one of those owners (each of which should be a
         //  type), and if it hasn't already been initialized, send it an
