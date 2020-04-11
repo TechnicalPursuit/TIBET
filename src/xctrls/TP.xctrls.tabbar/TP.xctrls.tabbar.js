d145f58170110cf0cdddfb404aef21da20140dcf
William J. Edney <bedney@technicalpursuit.com>
2020-04-11 16:10:25 -0500

    Diagnostics to debug GitLab - attempt #2

--- src/xctrls/TP.xctrls.tabbar/TP.xctrls.tabbar.js
+++ src/xctrls/TP.xctrls.tabbar/TP.xctrls.tabbar.js
@@ -49,6 +49,128 @@ TP.xctrls.tabbar.Type.defineAttribute('defaultItemTagName',
  */
 TP.xctrls.tabbar.Type.defineAttribute('wantsCloseMarks', true);
 
+//  ------------------------------------------------------------------------
+
+TP.xctrls.tabbar.Inst.defineMethod('render',
+function() {
+
+    /**
+     * @method render
+     * @summary Renders the receiver.
+     * @returns {TP.dom.D3Tag} The receiver.
+     */
+
+    var containerSelection,
+        rootUpdateSelection;
+
+    console.log('GOT TO TABBAR RENDER #1');
+
+    //  Note that this is a strict check for the value of 'false' (the Boolean
+    //  value of false). This can't just be a 'falsey' value.
+    if (TP.isFalse(this.get('shouldRender'))) {
+        return this;
+    }
+
+    console.log('GOT TO TABBAR RENDER #2');
+
+    this.d3SelectContainer();
+
+    console.log('GOT TO TABBAR RENDER #3');
+
+    //  If the data is not valid, then empty the root selection (keeping the
+    //  root itself intact for future updates).
+    if (TP.notValid(this.get('data'))) {
+
+    console.log('GOT TO TABBAR RENDER #4');
+
+        containerSelection = this.get('containerSelection');
+
+    console.log('GOT TO TABBAR RENDER #5');
+
+        //  No valid container selection? Return here.
+        if (TP.notValid(containerSelection)) {
+
+    console.log('GOT TO TABBAR RENDER #6');
+
+            //  Signal to observers that this control has rendered.
+            this.signal('TP.sig.DidRender');
+
+            return this;
+        }
+
+    console.log('GOT TO TABBAR RENDER #7');
+
+        containerSelection.selectAll('*').remove();
+
+    console.log('GOT TO TABBAR RENDER #8');
+
+        //  Signal to observers that this control has rendered.
+        this.signal('TP.sig.DidRender');
+
+    } else {
+
+    console.log('GOT TO TABBAR RENDER #9');
+
+        //  Select any nodes under the 'selection root'
+        rootUpdateSelection = this.d3Select();
+
+    console.log('GOT TO TABBAR RENDER #10');
+
+        //  No valid root update selection? Return here.
+        if (TP.notValid(rootUpdateSelection)) {
+
+    console.log('GOT TO TABBAR RENDER #11');
+
+            //  Signal to observers that this control has rendered.
+            this.signal('TP.sig.DidRender');
+
+            return this;
+        }
+
+    console.log('GOT TO TABBAR RENDER #12');
+
+        //  Associate (or 'bind') the data to the root update selection.
+        this.d3Data(rootUpdateSelection);
+
+    console.log('GOT TO TABBAR RENDER #13');
+
+        //  Update any existing update selection
+        this.d3Update();
+        this.d3UpdateTransition();
+
+    console.log('GOT TO TABBAR RENDER #14');
+
+        //  Add any content to the enter selection
+        this.d3Enter();
+        this.d3EnterTransition();
+
+    console.log('GOT TO TABBAR RENDER #15');
+
+        //  Remove any content from the exit selection
+        this.d3Exit();
+        this.d3ExitTransition();
+
+    console.log('GOT TO TABBAR RENDER #16');
+
+        //  If we're supposed to maintain the order between data and DOM, then
+        //  do so here.
+        if (TP.notFalse(this.getType().get('shouldOrder'))) {
+    console.log('GOT TO TABBAR RENDER #17');
+
+            this.get('updateSelection').order();
+        }
+
+    console.log('GOT TO TABBAR RENDER #18');
+
+        //  Signal to observers that this control has rendered its data.
+        this.signal('TP.sig.DidRenderData');
+    }
+
+    console.log('GOT TO TABBAR RENDER #19');
+
+    return this;
+});
+
 //  ========================================================================
 //  TP.xctrls.tabitem
 //  ========================================================================
