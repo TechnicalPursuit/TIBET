81fc4b6e9412382ea4423c45ad0708fb2b58f8c2
William J. Edney <bedney@technicalpursuit.com>
2020-09-19 22:23:09 -0500

    Comment updates

--- src/tibet/helpers/tibet_service_worker.js
+++ src/tibet/helpers/tibet_service_worker.js
@@ -1,3 +1,13 @@
+//  ========================================================================
+/**
+ * @copyright Copyright (C) 1999 Technical Pursuit Inc. (TPI) All Rights
+ *     Reserved. Patents Pending, Technical Pursuit Inc. Licensed under the
+ *     OSI-approved Reciprocal Public License (RPL) Version 1.5. See the RPL
+ *     for your rights and responsibilities. Contact TPI to purchase optional
+ *     privacy waivers if you must keep your TIBET-based source code private.
+ */
+//  ------------------------------------------------------------------------
+
 var msgHandler,
     bootSystemPort,
     channelPromiseCBs;
@@ -352,3 +362,6 @@ self.sendMessageToPage = function(client, msgObjContent) {
     return listenerPromise;
 };
 
+//  ------------------------------------------------------------------------
+//  end
+//  ========================================================================
