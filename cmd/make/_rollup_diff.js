45bb94069c340f847b30d02f0d2a5c12a92f1655
William J. Edney <bedney@technicalpursuit.com>
2020-12-04 17:21:33 -0600

    The ‘diff’ package no longer comes with a pre-minified file.

--- cmd/make/_rollup_diff.js
+++ cmd/make/_rollup_diff.js
@@ -16,10 +16,6 @@
             make.CLI.joinPaths('.', 'dist', 'diff.js'),
             make.CLI.joinPaths('..', '..', 'deps', 'diff-tpi.js'));
 
-        make.sh.cp(
-            make.CLI.joinPaths('.', 'dist', 'diff.min.js'),
-            make.CLI.joinPaths('..', '..', 'deps', 'diff-tpi.min.js'));
-
         resolve();
     };
 
