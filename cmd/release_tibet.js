a923392305363b1e095b3773beaecd6a1f3ff2aa
William J. Edney <bedney@technicalpursuit.com>
2020-12-16 17:46:50 -0600

    Update the release_tibet command to use the new format required by the dockerhub deploy subcommand for ‘extra_tag_entries’
    Also, supply the DockerHub account name.

--- cmd/release_tibet.js
+++ cmd/release_tibet.js
@@ -170,9 +170,9 @@ Cmd.prototype.execute = async function() {
      *          "nodockercache": true,
      *          "dockerfile": "Dockerfile_DEPLOY",
      *          "extra_tag_entries": [
-     *              "technicalpursuit/tibet:latest technicalpursuit/tibet:" + meta.source.major,
-     *              "technicalpursuit/tibet:latest technicalpursuit/tibet:" + meta.source.major + '.' + meta.source.minor,
-     *              "technicalpursuit/tibet:latest technicalpursuit/tibet:" + meta.source.major + '.' + meta.source.minor + '.' + meta.source.patch,
+     *              ["technicalpursuit/tibet:latest","technicalpursuit/tibet:<meta.source.major>"],
+     *              ["technicalpursuit/tibet:latest","technicalpursuit/tibet:<meta.source.major>.<meta.source.minor>"],
+     *              ["technicalpursuit/tibet:latest","technicalpursuit/tibet:<meta.source.major>.<meta.source.minor>.<meta.source.patch>"],
      *          ],
      *          "extra_push_entries": [
      *              "technicalpursuit/tibet:" + meta.source.major
@@ -429,20 +429,28 @@ Cmd.prototype.execute = async function() {
 
      execArgs.push(
         '{' +
+            '"account": "technicalpursuit", ' +
             '"projectname": "tibet", ' +
             '"projectversion": "latest", ' +
             '"nodockercache": true, ' +
             '"dockerfile": "Dockerfile_DEPLOY", ' +
 
             '"extra_tag_entries": [' +
-                '"technicalpursuit/tibet:latest technicalpursuit/tibet:' +
-                meta.source.major + '", ' +
-                '"technicalpursuit/tibet:latest technicalpursuit/tibet:' +
-                meta.source.major + '.' + meta.source.minor + '", ' +
-                '"technicalpursuit/tibet:latest technicalpursuit/tibet:' +
-                meta.source.major + '.' + meta.source.minor + '.' + meta.source.patch +
-            '"], ' +
-
+                '[' +
+                    '"technicalpursuit/tibet:latest"' + ',' +
+                    '"technicalpursuit/tibet:' + meta.source.major + '"' +
+                ']' +
+                ',' +
+                '[' +
+                    '"technicalpursuit/tibet:latest"' + ',' +
+                    '"technicalpursuit/tibet:' + meta.source.major + '.' + meta.source.minor + '"' +
+                ']' +
+                ',' +
+                '[' +
+                    '"technicalpursuit/tibet:latest"' + ',' +
+                    '"technicalpursuit/tibet:' + meta.source.major + '.' + meta.source.minor + '.' + meta.source.patch + '"' +
+                ']' +
+            '],' +
             '"extra_push_entries": [' +
                 '"technicalpursuit/tibet:' + meta.source.major + '", ' +
                 '"technicalpursuit/tibet:' + meta.source.major + '.' + meta.source.minor + '", ' +
