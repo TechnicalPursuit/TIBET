b93aa7f4a2bccf88b08c7f79198a1a157a4b80e3
William J. Edney <bedney@technicalpursuit.com>
2021-03-27 11:27:59 -0500

    Added Electron signals to track window move / resize

--- src/tibet/kernel/TIBETSourceTypes.js
+++ src/tibet/kernel/TIBETSourceTypes.js
@@ -1477,6 +1477,9 @@ TP.sig.ElectronSignal.defineSubtype('UpdateAvailable');
 TP.sig.ElectronSignal.defineSubtype('UpdateNotAvailable');
 TP.sig.ElectronSignal.defineSubtype('UpdateDownloaded');
 
+TP.sig.ElectronSignal.defineSubtype('WindowMoved');
+TP.sig.ElectronSignal.defineSubtype('WindowResized');
+
 TP.sig.ElectronSignal.defineSubtype('ApplicationWillExit');
 TP.sig.ElectronSignal.defineSubtype('ApplicationDidExit');
 
