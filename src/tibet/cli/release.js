8fa77bd16c72ae69ee3096104f5f9996da1c8c99
William J. Edney <bedney@technicalpursuit.com>
2021-04-27 21:29:23 -0500

    release 5.3.2-develop

--- src/tibet/cli/release.js
+++ src/tibet/cli/release.js
@@ -10,7 +10,7 @@
  */
 //  ========================================================================
 
-/* eslint indent:0, consistent-this:0 */
+/* eslint indent:0, consistent-this:0, no-debugger:0 */
 
 (function() {
 
@@ -421,6 +421,7 @@ Cmd.prototype.phaseThree = function(meta) {
             release.warn('dry-run. bypassing ' + cmd);
         } else {
             release.log('executing ' + cmd);
+            debugger;
             try {
                 res = release.shexec(cmd);
             } catch (e) {
