0baf3898d45d02d068850e5e7edb8ee2e2b43a48
William J. Edney <bedney@technicalpursuit.com>
2021-06-10 06:24:31 -0500

    Capture top-level window path and use as the TDS.home.uri.
    This is more robust way of doing this than previous per-project hardcoding.

--- dna/couch/public/login.js
+++ dna/couch/public/login.js
@@ -3,6 +3,18 @@ document.getElementById('login').addEventListener(
 'submit',
 function(evt) {
 
+    var path;
+
+    //  Grab the top-level pathname and, if it has real pathname content, use
+    //  that as the TDS's home URI.
+    path = top.location.pathname;
+    if (path) {
+        if (path.endsWith('/')) {
+            path = path.slice(0, path.lastIndexOf('/'));
+        }
+        top.sessionStorage.setItem('TDS.home.uri', path);
+    }
+
     evt.preventDefault();
     login();
 },
