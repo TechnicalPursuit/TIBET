3aa8d9a818715aae2020cfc08a387159e155aab9
William J. Edney <bedney@technicalpursuit.com>
2021-08-30 19:39:37 -0500

    "Update the version to: tibet@5.4.0+gca7ddff277.337.1630370362838"

--- src/tibet/kernel/version.js
+++ src/tibet/kernel/version.js
@@ -19,18 +19,18 @@
 
     release({
         //  new release
-        semver: '5.3.7+g9e44065640.63.1623617117935',
+        semver: '5.4.0+gca7ddff277.337.1630370362838',
         major: '5',
-        minor: '3',
-        patch: '7',
+        minor: '4',
+        patch: '0',
         suffix: '',
         increment: '0',
-        time: '1623617117935',
+        time: '1630370362838',
         //  prior semver
-        describe: '5.3.6-63-g9e44065640',
-        ptag: '5.3.6',
-        commits: '63',
-        phash: '9e44065640'
+        describe: '5.3.7-337-gca7ddff277',
+        ptag: '5.3.7',
+        commits: '337',
+        phash: 'ca7ddff277'
     },
     'lib');
 
