489188313941bdcfde84935dbbc5b42e95f4a9a3
William J. Edney <bedney@technicalpursuit.com>
2021-09-19 10:21:30 -0500

    Added / updated ECMA module tests.

--- test/src/tibet/modules/NativeModules_Tests.js
+++ test/src/tibet/modules/NativeModules_Tests.js
@@ -28,19 +28,110 @@ TP.test.NativeModuleTester = TP.lang.Object.construct();
 TP.test.NativeModuleTester.describe('Use Native Class from Module',
 function() {
 
-    this.it('Load native class', function(test, options) {
+    this.it('Load native class with no dependencies', function(test, options) {
 
         return TP.sys.importModule(
                 TP.uc('~lib_test/src/tibet/modules/ECMAModuleNoImport.js')).then(
                     function(module) {
                         const {MyObject} = module;
 
-                        console.log('GOT HERE: ', MyObject);
-
-                        test.assert.isValid(MyObject);
+                        test.assert.isNativeType(MyObject);
                     });
     });
-}).skip(!TP.sys.isHTTPBased()); //  ECMA modules don't work with file:// URLs
+
+    //  ---
+
+    this.it('Load native class with single URL dependency', function(test, options) {
+
+        return TP.sys.importModule(
+                TP.uc('~lib_test/src/tibet/modules/ECMAModuleStandardImport.js')).then(
+                    function(module) {
+                        const {AnotherObject} = module;
+
+                        test.assert.isNativeType(AnotherObject);
+                    });
+    });
+
+    //  ---
+
+    this.it('Load native class with single bare specifier dependency', function(test, options) {
+
+        return TP.sys.importModule(
+                TP.uc('~lib_test/src/tibet/modules/ECMAModuleBareSpecifierImport.js')).then(
+                    function(module) {
+                        const {AnotherBareSpecifierObject} = module;
+
+                        test.assert.isNativeType(AnotherBareSpecifierObject);
+                    });
+    });
+});
+
+//  ------------------------------------------------------------------------
+
+TP.test.NativeModuleTester.describe('Use TIBET-derived Class from Module',
+function() {
+
+    this.it('Load TIBET-derived class from a whole namespace import', function(test, options) {
+
+        return TP.sys.importModule(
+                TP.uc('~lib_test/src/tibet/modules/ECMAModuleWholeTIBETNSImport.js')).then(
+                    function(module) {
+                        const {AnotherPoint, YetAnotherPoint} = module;
+
+                        test.assert.isNativeType(AnotherPoint);
+                        test.assert.isNativeType(YetAnotherPoint);
+                    });
+    });
+
+    //  ---
+
+    this.it('Load TIBET-derived class from a whole namespace and individual type import', function(test, options) {
+
+        return TP.sys.importModule(
+                TP.uc('~lib_test/src/tibet/modules/ECMAModuleMixedTIBETNSAndTypeImport.js')).then(
+                    function(module) {
+                        const {Rect, AnotherRect, YetAnotherRect, AThirdRect} = module;
+
+                        test.assert.isNativeType(Rect);
+                        test.assert.isNativeType(AnotherRect);
+                        test.assert.isNativeType(YetAnotherRect);
+                        test.assert.isNativeType(AThirdRect);
+                    });
+    });
+
+    //  ---
+
+    this.it('Load TIBET-derived class from a whole namespace and individual type import', function(test, options) {
+
+        return TP.sys.importModule(
+                TP.uc('~lib_test/src/tibet/modules/ECMAModuleMixedTIBETNSAndTypeImport.js')).then(
+                    function(module) {
+                        const {Rect, AnotherRect, YetAnotherRect, AThirdRect} = module;
+
+                        test.assert.isNativeType(Rect);
+                        test.assert.isNativeType(AnotherRect);
+                        test.assert.isNativeType(YetAnotherRect);
+                        test.assert.isNativeType(AThirdRect);
+                    });
+    });
+
+    //  ---
+
+    this.it('Load TIBET-derived class from a whole namespace and individual type import with an alias', function(test, options) {
+
+        return TP.sys.importModule(
+                TP.uc('~lib_test/src/tibet/modules/ECMAModuleMixedTIBETNSAndTypeImportWithAlias.js')).then(
+                    function(module) {
+                        const {FooFoo, AnotherMatrix, YetAnotherMatrix, AThirdMatrix} = module;
+
+                        test.assert.isNativeType(FooFoo);
+                        test.assert.isNativeType(AnotherMatrix);
+                        test.assert.isNativeType(YetAnotherMatrix);
+                        test.assert.isNativeType(AThirdMatrix);
+                    });
+    });
+
+});
 
 /* eslint-enable class-methods-use-this */
 
