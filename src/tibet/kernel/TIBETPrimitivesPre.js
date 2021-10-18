f1c714b5a05816e96f18f949c635b657043a06d5
William J. Edney <bedney@technicalpursuit.com>
2021-10-18 10:52:57 -0500

    Put lint patch back to what idearat checked in.
    Mistakenly resolved the conflict the wrong way in the prior commit.

--- src/tibet/kernel/TIBETPrimitivesPre.js
+++ src/tibet/kernel/TIBETPrimitivesPre.js
@@ -487,7 +487,9 @@ TP.makeStartUpper = function(anObj) {
      */
 
     //  NOTE we test for equality not identity (String vs. string "types)
-    return anObj === '' ? '' : anObj[0].toUpperCase() + anObj.slice(1);
+    /* eslint-disable eqeqeq */
+    return anObj == '' ? '' : anObj[0].toUpperCase() + anObj.slice(1);
+    /* eslint-enable eqeqeq */
 };
 
 //  Manual setup
@@ -11198,7 +11200,9 @@ function() {
      */
 
     //  NOTE we test for equality not identity (String vs. string "types)
-    return this === '' ? '' : this[0].toLowerCase() + this.substring(1);
+    /* eslint-disable eqeqeq */
+    return this == '' ? '' : this[0].toLowerCase() + this.substring(1);
+    /* eslint-enable eqeqeq */
 });
 
 //  ------------------------------------------------------------------------
