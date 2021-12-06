f703a01ef31feb3f22208438f89410796f16eeb0
idearat <ss@technicalpursuit.com>
2021-12-06 05:27:31 -0700

    Start on some element-level API tests for binding methods.

---
+++ test/src/bind/TP.bind_Object_Tests.js
@@ -0,0 +1,185 @@
+//  ========================================================================
+/**
+ * @copyright Copyright (C) 1999 Technical Pursuit Inc. (TPI) All Rights
+ *     Reserved. Patents Pending, Technical Pursuit Inc. Licensed under the
+ *     OSI-approved Reciprocal Public License (RPL) Version 1.5. See the RPL
+ *     for your rights and responsibilities. Contact TPI to purchase optional
+ *     privacy waivers if you must keep your TIBET-based source code private.
+ */
+//  ========================================================================
+
+//  ------------------------------------------------------------------------
+//  Test Tag Types
+//  ------------------------------------------------------------------------
+
+TP.tag.CustomTag.defineSubtype('test:elem');
+TP.test.elem.defineAttribute('styleURI', TP.NO_RESULT);
+TP.test.elem.defineAttribute('themeURI', TP.NO_RESULT);
+
+//  ------------------------------------------------------------------------
+//  Inbound Binds
+//  ------------------------------------------------------------------------
+
+TP.bind.XMLNS.Type.describe('inbound bind info',
+function() {
+
+    this.it('uses explicit attribute value before bind:in', function(test, options) {
+        var tpElem,
+            info;
+
+        tpElem = TP.tpelem(`
+<test:elem xmlns:bind="http://www.technicalpursuit.com/2005/binding"
+    foo="[[attr]]"
+    bind:in="{foo: bindin, bar: bar}"
+/>
+`);
+        test.assert.isValid(tpElem, 'element not valid');
+
+        info = tpElem.getInboundBindingEntryFor('foo');
+        test.assert.isValid(info, 'info not valid');
+
+        test.assert.isEqualTo(info.at('dataExprs').first(), 'foo');
+    });
+
+
+    this.it('uses explicit attribute value before bind:io', function(test, options) {
+        var tpElem,
+            info;
+
+        tpElem = TP.tpelem(`
+<test:elem xmlns:bind="http://www.technicalpursuit.com/2005/binding"
+    foo="{value: foo}"
+    bind:io="{foo: bindio, bar: bar}"
+/>
+`);
+        test.assert.isValid(tpElem, 'element not valid');
+
+        info = tpElem.getInboundBindingEntryFor('foo');
+        test.assert.isValid(info, 'info not valid');
+
+        test.assert.isEqualTo(info.at('dataExprs').first(), 'foo');
+    });
+
+
+    this.it('uses bind:in attribute value before bind:io', function(test, options) {
+        var tpElem,
+            info;
+
+        tpElem = TP.tpelem(`
+<test:elem xmlns:bind="http://www.technicalpursuit.com/2005/binding"
+    bind:in="{foo: bindin, bar: bar}"
+    bind:io="{foo: bindio, bar: bar}"
+/>
+`);
+        test.assert.isValid(tpElem, 'element not valid');
+
+        info = tpElem.getInboundBindingEntryFor('foo');
+        test.assert.isValid(info, 'info not valid');
+
+        test.assert.isEqualTo(info.at('dataExprs').first(), 'bindin');
+    });
+
+
+    this.it('uses bind:io if nothing else overrides it', function(test, options) {
+        var tpElem,
+            info;
+
+        tpElem = TP.tpelem(`
+<test:elem xmlns:bind="http://www.technicalpursuit.com/2005/binding"
+    bind:io="{foo: bindio, bar: bar}"
+/>
+`);
+        test.assert.isValid(tpElem, 'element not valid');
+
+        info = tpElem.getInboundBindingEntryFor('foo');
+        test.assert.isValid(info, 'info not valid');
+
+        test.assert.isEqualTo(info.at('dataExprs').first(), 'bindio');
+    });
+});
+
+//  ------------------------------------------------------------------------
+//  Outbound Binds
+//  ------------------------------------------------------------------------
+
+TP.bind.XMLNS.Type.describe('outbound bind info',
+function() {
+
+    this.it('uses explicit attribute value before bind:out', function(test, options) {
+        var tpElem,
+            info;
+
+        tpElem = TP.tpelem(`
+<test:elem xmlns:bind="http://www.technicalpursuit.com/2005/binding"
+    foo="{value: foo}"
+    bind:out="{foo: bindout, bar: bar}"
+/>
+`);
+        test.assert.isValid(tpElem, 'element not valid');
+
+        info = tpElem.getOutboundBindingEntryFor('foo');
+        test.assert.isValid(info, 'info not valid');
+
+        test.assert.isEqualTo(info.at('dataExprs').first(), 'foo');
+    });
+
+
+    this.it('uses explicit attribute value before bind:io', function(test, options) {
+        var tpElem,
+            info;
+
+        tpElem = TP.tpelem(`
+<test:elem xmlns:bind="http://www.technicalpursuit.com/2005/binding"
+    foo="{value: foo}"
+    bind:io="{foo: bindio, bar: bar}"
+/>
+`);
+        test.assert.isValid(tpElem, 'element not valid');
+
+        info = tpElem.getOutboundBindingEntryFor('foo');
+        test.assert.isValid(info, 'info not valid');
+
+        test.assert.isEqualTo(info.at('dataExprs').first(), 'foo');
+    });
+
+
+    this.it('uses bind:out attribute value before bind:io', function(test, options) {
+        var tpElem,
+            info;
+
+        tpElem = TP.tpelem(`
+<test:elem xmlns:bind="http://www.technicalpursuit.com/2005/binding"
+    bind:out="{foo: bindin, bar: bar}"
+    bind:io="{foo: bindio, bar: bar}"
+/>
+`);
+        test.assert.isValid(tpElem, 'element not valid');
+
+        info = tpElem.getOutboundBindingEntryFor('foo');
+        test.assert.isValid(info, 'info not valid');
+
+        test.assert.isEqualTo(info.at('dataExprs').first(), 'bindin');
+    });
+
+
+    this.it('uses bind:io if nothing else overrides it', function(test, options) {
+        var tpElem,
+            info;
+
+        tpElem = TP.tpelem(`
+<test:elem xmlns:bind="http://www.technicalpursuit.com/2005/binding"
+    bind:io="{foo: bindio, bar: bar}"
+/>
+`);
+        test.assert.isValid(tpElem, 'element not valid');
+
+        info = tpElem.getOutboundBindingEntryFor('foo');
+        test.assert.isValid(info, 'info not valid');
+
+        test.assert.isEqualTo(info.at('dataExprs').first(), 'bindio');
+    });
+});
+
+//  ------------------------------------------------------------------------
+//  end
+//  ========================================================================
