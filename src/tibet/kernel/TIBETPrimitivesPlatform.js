f0b4939190fa07bfa3f09a9796476a280eed0626
William J. Edney <bedney@technicalpursuit.com>
2021-12-22 14:14:15 -0600

    Change toString reporting for type prototypes ('Type' and 'Inst') to report 'Function'

--- src/tibet/kernel/TIBETPrimitivesPlatform.js
+++ src/tibet/kernel/TIBETPrimitivesPlatform.js
@@ -1138,6 +1138,10 @@ TP.hc(
             return '[object CSSStyleDeclaration]';
         }
 
+        if (TP.isPrototype(anObject)) {
+            return '[object Function]';
+        }
+
         if (TP.isProxy(anObject)) {
             return '[object Proxy]';
         }
